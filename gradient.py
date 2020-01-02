48af4795c7606b7283e24b87ae41a38fbfc84169
cyy <cyyever@outlook.com>
2020-01-02 14:59:48 +0800

    fix api

---
+++ gradient.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import torch.autograd as autograd
+
+from .util import parameters_to_vector
+
+
+def get_gradient(model, loss_fun):
+    return parameters_to_vector(autograd.grad(loss_fun, model.parameters()))
