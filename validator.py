fd258eb7fd6bf6091ee5611525d9b065b211254a
cyy <cyyever@outlook.com>
2020-01-14 14:49:33 +0800

    fix code

--- validator.py
+++ validator.py
@@ -15,35 +15,36 @@ class validator:
         self.loss_fun = loss_fun
         self.validation_dataset = validation_dataset
 
-    def validate(self, batch_size, after_batch_callback=None):
+    def validate(self, batch_size, use_grad=False, after_batch_callback=None):
         validation_data_loader = torch.utils.data.DataLoader(
             self.validation_dataset, batch_size=batch_size
         )
-        num_correct = 0
-        num_examples = 0
-        device = get_device()
-        self.model.eval()
-        self.model.to(device)
-        validation_loss = torch.zeros(1)
-        validation_loss = validation_loss.to(device)
-        for batch in validation_data_loader:
-            inputs, targets = batch
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            outputs = self.model(inputs)
-            batch_loss = self.loss_fun(outputs, targets)
-            if self.loss_fun.reduction == "mean":
-                batch_loss *= len(outputs)
-            if after_batch_callback:
-                after_batch_callback(self.model, batch_loss)
-            validation_loss += batch_loss
-            correct = torch.eq(
-                torch.max(F.softmax(outputs, dim=1), dim=1)[1], targets
-            ).view(-1)
-            num_correct += torch.sum(correct).item()
-            num_examples += correct.shape[0]
-        validation_loss /= len(self.validation_dataset)
-        return (validation_loss, num_correct / num_examples)
+        with torch.set_grad_enabled(use_grad):
+            num_correct = 0
+            num_examples = 0
+            device = get_device()
+            self.model.eval()
+            self.model.to(device)
+            validation_loss = torch.zeros(1)
+            validation_loss = validation_loss.to(device)
+            for batch in validation_data_loader:
+                inputs, targets = batch
+                inputs = inputs.to(device)
+                targets = targets.to(device)
+                outputs = self.model(inputs)
+                batch_loss = self.loss_fun(outputs, targets)
+                if self.loss_fun.reduction == "mean":
+                    batch_loss *= len(outputs)
+                if after_batch_callback:
+                    after_batch_callback(self.model, batch_loss)
+                validation_loss += batch_loss
+                correct = torch.eq(
+                    torch.max(F.softmax(outputs, dim=1), dim=1)[1], targets
+                ).view(-1)
+                num_correct += torch.sum(correct).item()
+                num_examples += correct.shape[0]
+            validation_loss /= len(self.validation_dataset)
+            return (validation_loss, num_correct / num_examples)
 
     def get_gradient(self):
         gradient = None
@@ -55,7 +56,7 @@ class validator:
             else:
                 gradient += _get_gradient(model, batch_loss)
 
-        self.validate(64, after_batch_callback)
+        self.validate(64, True, after_batch_callback)
         return gradient / len(self.validation_dataset)
 
     def hessian_vector_product(self, v, damping=0):
@@ -68,7 +69,7 @@ class validator:
             else:
                 res += _hessian_vector_product(model, batch_loss, v)
 
-        self.validate(64, after_batch_callback)
+        self.validate(64, True, after_batch_callback)
         res /= len(self.validation_dataset)
         if damping != 0:
             res += damping * v
