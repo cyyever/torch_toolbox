d501e4a05b7ec1197a0993ece1317a87407f89f9
cyy <cyyever@outlook.com>
2020-02-19 19:08:04 +0800

    fix code

--- trainer.py
+++ trainer.py
@@ -6,22 +6,16 @@ import torch.optim as optim
 
 from .device import get_cpu_device
 from .device import get_device
+from .util import model_gradients_to_vector
 
 
 class trainer:
     def __init__(
-        self, model, loss_funs, training_datasets, name="",
+        self, model, loss_fun, training_dataset, name="",
     ):
         self.model = copy.deepcopy(model)
-        if not isinstance(loss_funs, list):
-            loss_funs = [loss_funs]
-        if not isinstance(training_datasets, list):
-            training_datasets = [training_datasets]
-        if len(loss_funs) != len(training_datasets):
-            raise ValueError(
-                "loss_funs shape does not match training_datasets shape")
-        self.loss_funs = loss_funs
-        self.training_datasets = training_datasets
+        self.loss_fun = loss_fun
+        self.training_dataset = training_dataset
         self.name = name
         self.min_training_loss = None
         self.min_training_loss_model = None
@@ -31,50 +25,71 @@ class trainer:
         self.optimizer_fun = optimizer_fun
 
     def train(self, epochs, batch_size, learning_rate, **kwargs):
-        training_data_loaders = [
-            torch.utils.data.DataLoader(
-                training_dataset, batch_size=batch_size, shuffle=True
-            )
-            for training_dataset in self.training_datasets
-        ]
+        training_data_loader = torch.utils.data.DataLoader(
+            self.training_dataset, batch_size=batch_size, shuffle=True
+        )
 
         device = get_device()
         self.model.to(device)
         optimizer = self.optimizer_fun(
             self.model.parameters(), lr=learning_rate)
+        example_size = len(self.training_dataset)
 
         for epoch in range(epochs):
             self.model.train()
             training_loss = 0.0
-            for training_data_loader, loss_fun in zip(
-                training_data_loaders, self.loss_funs
-            ):
-                for batch in training_data_loader:
-                    if "pre_batch_callback" in kwargs:
-                        kwargs["pre_batch_callback"](
-                            self.model, batch, learning_rate)
-                    self.model.to(device)
-                    optimizer.zero_grad()
-                    inputs = batch[0]
-                    targets = batch[1]
-                    inputs = inputs.to(device)
-                    targets = targets.to(device)
-                    outputs = self.model(inputs)
-                    loss = loss_fun(outputs, targets)
-                    loss.backward()
-                    optimizer.step()
-                    batch_loss = loss.data.item()
+            batch_index = 0
+            for batch in training_data_loader:
+                if "pre_batch_callback" in kwargs:
+                    kwargs["pre_batch_callback"](
+                        self.model, batch, learning_rate)
+                self.model.to(device)
+                optimizer.zero_grad()
+                batch_loss = 0
+                if "per_example_gradient_callback" in kwargs:
+                    prev_accumulated_gradient = None
+                    example_inputs, example_targets, example_indices = batch
+                    real_batch_size = len(example_indices)
+                    for i, example_index in enumerate(example_indices):
+                        example_index = example_indices[i]
+                        example_input = example_inputs[i].to(device)
+                        example_target = example_targets[i].to(device)
+                        output = self.model(torch.stack([example_input]))
+                        loss = self.loss_fun(
+                            output, torch.stack([example_target]))
+                        batch_loss += loss.data.item() / real_batch_size
+                        loss.backward()
+                        cur_accumulated_gradient = model_gradients_to_vector(
+                            self.model)
+                        example_gradient = None
+                        if prev_accumulated_gradient is None:
+                            example_gradient = cur_accumulated_gradient
+                        else:
+                            example_gradient = cur_accumulated_gradient - prev_accumulated_gradient
 
-                    if hasattr(loss_fun, "reduction") and (
-                        loss_fun.reduction == "mean"
-                        or loss_fun.reduction == "elementwise_mean"
-                    ):
-                        batch_loss *= len(outputs)
-                        batch_loss /= len(training_data_loader.dataset)
-                    training_loss += batch_loss
+                        if "per_example_gradient_callback" in kwargs:
+                            kwargs["per_example_gradient_callback"](
+                                self.model, example_index, example_gradient, learning_rate)
+                else:
+                    inputs = batch[0].to(device)
+                    targets = batch[1].to(device)
+                    outputs = self.model(inputs)
+                    loss = self.loss_fun(outputs, targets)
+                    batch_loss = loss.data.item()
+                    loss.backward()
+
+                optimizer.step()
+                print(
+                    "trainer:{}, epoch: {}, batch: {}, batch training loss: {}".format(
+                        self.name, epoch, batch_index, batch_loss))
+                batch_index += 1
+                training_loss += (batch_loss * real_batch_size / example_size)
+                if "after_batch_callback" in kwargs:
+                    kwargs["after_batch_callback"](
+                        self.model, batch, learning_rate)
 
             print(
-                "trainer:{}, epoch: {}, training loss: {}".format(
+                "trainer:{}, epoch: {}, epoch training loss: {}".format(
                     self.name, epoch, training_loss
                 )
             )
@@ -101,7 +116,7 @@ class trainer:
         if not os.path.isdir(save_dir):
             os.makedirs(save_dir)
         open(os.path.join(save_dir, "training_dataset"), "wb").write(
-            pickle.dumps(self.training_datasets)
+            pickle.dumps(self.training_dataset)
         )
 
     def parameters(self, use_best_model=False):
--- util.py
+++ util.py
@@ -5,3 +5,17 @@ def parameters_to_vector(parameters):
     return nn.utils.parameters_to_vector(
         [parameter.reshape(-1) for parameter in parameters]
     )
+
+
+def model_parameters_to_vector(model):
+    parameters = model.parameters()
+    return nn.utils.parameters_to_vector(
+        [parameter.reshape(-1) for parameter in parameters]
+    )
+
+
+def model_gradients_to_vector(model):
+    parameters = model.parameters()
+    return nn.utils.parameters_to_vector(
+        [parameter.grad.reshape(-1) for parameter in parameters]
+    )
