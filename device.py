e046760d404daf2b4dc4b47479e701e7ad682879
cyy <cyyever@outlook.com>
2020-02-19 19:08:04 +0800

    fix code

--- device.py
+++ device.py
@@ -4,10 +4,10 @@ import torch
 
 
 def get_cpu_device():
-    return torch.device("cuda")
+    return torch.device("cpu")
 
 
 def get_device():
     if torch.cuda.is_available():
-        return get_cpu_device()
+        return torch.device("cuda")
     return torch.device("cpu")
