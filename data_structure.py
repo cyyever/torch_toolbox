364382f3e57a064e357017781ecedd49ac29d779
cyy <cyyever@outlook.com>
2020-02-20 22:09:32 +0800

    fix ds

--- data_structure.py
+++ data_structure.py
@@ -1,4 +1,5 @@
 import uuid
+import shutil
 import pickle
 import os
 
@@ -7,7 +8,7 @@ class LargeDict(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.storage_dir = None
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.in_memory_key_number = 128
         self.all_keys = set()
 
@@ -16,6 +17,7 @@ class LargeDict(dict):
 
     def set_in_memory_key_number(self, num):
         self.in_memory_key_number = num
+        os.makedirs(os.path.join(self.storage_dir, self.id))
 
     def __getitem__(self, key):
         if key in self.all_keys:
@@ -25,28 +27,31 @@ class LargeDict(dict):
     def __setitem__(self, key, val):
         self.all_keys.add(key)
         while super().__len__() > self.in_memory_key_number:
-            for other_key in super().keys():
+            for other_key in list(super().keys()):
                 if other_key == key:
                     continue
                 self.__save_item(other_key)
-                super().__delitem__(other_key)
-        return super().__setitem__(self, key, val)
+        return super().__setitem__(key, val)
 
     def __delitem__(self, key):
         self.all_keys.remove(key)
         return super().__delitem__(key)
 
+    def __del__(self):
+        if self.storage_dir is not None:
+            shutil.rmtree(os.path.join(self.storage_dir, self.id))
+
     def __get_key_storage_path(self, key):
         if self.storage_dir is None:
             raise RuntimeError("no storage_dir")
-        return os.path.join(self.storage_dir, self.id + "_" + str(key))
+        return os.path.join(self.storage_dir, self.id, str(key))
 
     def __save_item(self, key):
         if not super().__contains__(key):
             raise RuntimeError("no key " + str(key))
 
         with open(os.path.join(self.__get_key_storage_path(key)), "wb") as f:
-            pickle.dump(f, super().__getitem__(key))
+            pickle.dump(super().__getitem__(key), f)
         super().__delitem__(key)
 
     def __load_item(self, key):
