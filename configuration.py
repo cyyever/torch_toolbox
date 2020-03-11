a0ffb54df6e1e2783b2684a4352a00dcbbb26f6d
cyy <cyyever@outlook.com>
2020-03-11 16:08:48 +0800

    fix code

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
 import math
 import torch.nn as nn
 import torch.optim as optim
+import torchvision
 
 from .hyper_parameter import HyperParameter
 from .trainer import Trainer
@@ -33,6 +34,36 @@ def get_task_configuration(task_name, for_training):
                 lambda optimizer: optim.lr_scheduler.LambdaLR(
                     optimizer, lr_lambda=lambda epoch: math.pow(
                         0.5, math.floor(
+                            epoch / 10)), ))
+
+            trainer.set_hyper_parameter(hyper_parameter)
+            trainer.set_validation_dataset(validation_dataset)
+            return trainer
+        validator = Validator(model, loss_fun, validation_dataset)
+        return validator
+    if task_name == "CIFAR10":
+        training_dataset = get_dataset(task_name, True)
+        validation_dataset = get_dataset(task_name, False)
+        model = torchvision.models.mobilenet_v2(num_classes=10)
+        loss_fun = nn.CrossEntropyLoss()
+        if for_training:
+            trainer = Trainer(model, loss_fun, training_dataset)
+            hyper_parameter = HyperParameter(
+                epoches=350, batch_size=64, learning_rate=0.1
+            )
+
+            hyper_parameter.set_optimizer_factory(
+                lambda params, learning_rate, weight_decay: optim.SGD(
+                    params,
+                    lr=learning_rate,
+                    weight_decay=(weight_decay / len(training_dataset)),
+                )
+            )
+
+            hyper_parameter.set_lr_scheduler_factory(
+                lambda optimizer: optim.lr_scheduler.LambdaLR(
+                    optimizer, lr_lambda=lambda epoch: math.pow(
+                        0.9, math.floor(
                             epoch / 10)), ))
 
             trainer.set_hyper_parameter(hyper_parameter)
