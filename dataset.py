f7a0e0a66e34a2c8d8d6db30be01dbceadbd33a7
cyy <cyyever@outlook.com>
2020-03-13 17:04:06 +0800

    add data argument

--- dataset.py
+++ dataset.py
@@ -97,7 +97,8 @@ def get_dataset(name, for_train):
             download=True,
             transform=transforms.Compose(
                 [
-                    transforms.Resize((32, 32)),
+                    transforms.RandomCrop(32, padding=4),
+                    transforms.RandomHorizontalFlip(),
                     transforms.ToTensor(),
                     transforms.Normalize(
                         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
