c2bff761708d90cc0fea4b31234d20cc19a35088
cyy <cyyever@outlook.com>
2020-03-14 17:55:31 +0800

    fix loss

--- loss.py
+++ loss.py
@@ -3,26 +3,31 @@ import torch.nn as nn
 
 
 class CrossEntropyLossWithCoefficents(nn.CrossEntropyLoss):
-    def __init__(self, coefficent_init_value=1, reduction="mean"):
+    def __init__(self, coefficent_init_value=1, final_reduction="mean"):
         super(
             CrossEntropyLossWithCoefficents,
             self).__init__(
             reduction="none",
         )
-        self.coefficent_init_value = coefficent_init_value
-        self.final_reduction = reduction
+        self.__coefficent_init_value = coefficent_init_value
+        self.__final_reduction = final_reduction
+        self.device = None
 
     def forward(self, input, target):
+        self.reduction = "none"
         loss = super().forward(input, target)
         loss_cnt = loss.shape[0]
         coefficents = []
         for _ in range(loss_cnt):
-            coefficent = torch.Tensor(
-                [self.coefficent_init_value], requires_grad=True)
+            coefficent = torch.Tensor([self.__coefficent_init_value])
+            coefficent.requires_grad_(True)
+            if self.device is not None:
+                coefficent = coefficent.to(self.device)
             coefficents.append(coefficent)
-        if self.final_reduction == "none":
+        if self.__final_reduction == "none":
             return torch.mul(coefficent, loss)
-        result = loss @ coefficents
-        if self.final_reduction == "mean":
+        result = loss @ torch.stack(coefficents)
+        if self.__final_reduction == "mean":
             result /= loss_cnt
+        self.reduction = self.__final_reduction
         return result
