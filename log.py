e2d784bdc15520074cf6ad71ba175596627123a0
cyy <cyyever@outlook.com>
2020-04-08 21:53:12 +0800

    fix log

--- log.py
+++ log.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import logging
+import os
 
 __logger_format = "%(asctime)s %(levelname)s {thd:%(thread)d} [%(filename)s => %(lineno)d] : %(message)s"
+logging.basicConfig(format=__logger_format, level=logging.INFO)
 
 
-def set_logger_file(filename, name="ML_exp"):
-    logger = logging.getLogger(name)
-    logger.addHandler(logging.FileHandler(filename))
+def set_logger_file(filename):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    logger = logging.getLogger()
+    handler = logging.FileHandler(filename)
+    handler.setFormatter(logging.Formatter(__logger_format))
+    logger.addHandler(handler)
 
 
-def get_logger(name="ML_exp"):
-    logging.basicConfig(format=__logger_format)
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+def get_logger():
+    logger = logging.getLogger()
     return logger
