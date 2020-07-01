fdc8eea065ac461bd14b83b5b6ff62ba55db8139
cyyever <cyyever@outlook.com>
2020-07-01 19:01:54 +0800

    fix code

--- device.py
+++ device.py
@@ -6,10 +6,21 @@ import torch
 def get_cpu_device():
     return torch.device("cpu")
 
+
+def get_cuda_devices():
+    device_count = torch.cuda.device_count()
+    assert device_count > 0
+    devices = []
+    for device_id in range(device_count):
+        devices.append(torch.device("cuda:" + str(device_id)))
+    return devices
+
+
 def get_cuda_device(device_id=None):
     if device_id:
-        return torch.device("cuda:"+str(device_id))
+        return torch.device("cuda:" + str(device_id))
     return torch.device("cuda")
+
 
 def get_device():
     if torch.cuda.is_available():
