ac91120c5e300b37b1e4b49bc3414d353ebbcb28
cyy <cyyever@outlook.com>
2020-07-03 11:42:52 +0800

    fix code

--- hessian_vector_product2.py
+++ hessian_vector_product2.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
 
+import threading
 import copy
 import numpy as np
 import torch
 import torch.autograd as autograd
-import threading
 
-from device import get_device
+from cyy_naive_lib.thread_pool import ThreadPool
+
+from device import get_cuda_devices
 from util import (
     parameters_to_vector,
     model_parameters_to_vector,
     set_model_attr,
     del_model_attr,
+    split_list_to_chunks,
 )
+
+
+device_threads = ThreadPool()
 
 
 class ModelSnapshot:
     data = dict()
+    lock = threading.Lock()
 
     @staticmethod
     def clear(model_class):
-        ModelSnapshot.data.pop(model_class, None)
+        with ModelSnapshot.lock:
+            ModelSnapshot.data.pop(model_class, None)
 
     @staticmethod
     def get(model_class, device):
         device_str = str(device)
-        if model_class not in ModelSnapshot.data:
-            ModelSnapshot.data[model_class] = dict()
-        if device_str not in ModelSnapshot.data[model_class]:
-            ModelSnapshot.data[model_class][device_str] = []
-        return ModelSnapshot.data[model_class][device_str]
+        with ModelSnapshot.lock:
+            if model_class not in ModelSnapshot.data:
+                ModelSnapshot.data[model_class] = dict()
+            if device_str not in ModelSnapshot.data[model_class]:
+                ModelSnapshot.data[model_class][device_str] = []
+            return ModelSnapshot.data[model_class][device_str]
 
     @staticmethod
     def resize(model, device, new_number):
@@ -52,6 +61,7 @@ def load_model_parameters(model, parameters, param_shape_dict, device):
 
 
 def get_hessian_vector_product_func(model, batch, loss_fun, for_train):
+
     # get all parameters and names
     params = []
     param_shape_dict = dict()
@@ -64,65 +74,83 @@ def get_hessian_vector_product_func(model, batch, loss_fun, for_train):
     for name in param_shape_dict:
         del_model_attr(model_snapshot, name.split("."))
 
-    # device = torch.device("cuda:2")
-    # device2 = torch.device("cuda:2")
-    device = torch.device("cpu")
-    device2 = torch.device("cpu")
-    assert device == device2
-    # device = get_device()
-    inputs = batch[0].to(device)
-    targets = batch[1].to(device)
     parameter_snapshot = parameters_to_vector(params)
 
-    def f(*args):
-        nonlocal inputs, targets, device, loss_fun, for_train, param_shape_dict, model_snapshot
-        model_class = model_snapshot.__class__.__name__
-        ModelSnapshot.resize(model_snapshot, device, len(args))
-        model_snapshots = ModelSnapshot.get(model_class, device)
-        assert len(model_snapshots) >= len(args)
-        loss = None
-        for i, arg in enumerate(args):
-            cur_model_snapshot = model_snapshots[i]
-            load_model_parameters(
-                cur_model_snapshot, arg, param_shape_dict, device)
-            cur_model_snapshot.zero_grad()
+    devices = get_cuda_devices()
+    inputs_dict = dict()
+    targets_dict = dict()
+    parameter_dict = dict()
 
-            if for_train:
-                cur_model_snapshot.train()
-            else:
-                cur_model_snapshot.eval()
-            cur_model_snapshot.to(device)
-            if loss is None:
-                loss = loss_fun(cur_model_snapshot(inputs), targets)
-            else:
-                loss += loss_fun(cur_model_snapshot(inputs), targets)
-        return loss
+    for idx, device in enumerate(devices):
+        inputs_dict[idx] = copy.deepcopy(batch[0]).to(device)
+        targets_dict[idx] = copy.deepcopy(batch[1]).to(device)
+        parameter_dict[idx] = copy.deepcopy(parameter_snapshot).to(device)
+
+
+    def get_f(device, inputs, targets):
+        def f(*args):
+            nonlocal inputs, targets, loss_fun, for_train, param_shape_dict, model_snapshot, device
+            model_class = model_snapshot.__class__.__name__
+            ModelSnapshot.resize(model_snapshot, device, len(args))
+            model_snapshots = ModelSnapshot.get(model_class, device)
+            assert len(model_snapshots) >= len(args)
+            loss = None
+            for i, arg in enumerate(args):
+                cur_model_snapshot = model_snapshots[i]
+                load_model_parameters(
+                    cur_model_snapshot, arg, param_shape_dict, device)
+                if for_train:
+                    cur_model_snapshot.train()
+                else:
+                    cur_model_snapshot.eval()
+                if loss is None:
+                    loss = loss_fun(cur_model_snapshot(inputs), targets)
+                else:
+                    loss += loss_fun(cur_model_snapshot(inputs), targets)
+            return loss
+
+        return f
 
     def vhp_func(v):
-        nonlocal f, device
-
         v_is_tensor = False
         if isinstance(v, list):
-            vector_num = len(v)
             vectors = v
         elif isinstance(v, tuple):
-            vector_num = len(v)
             vectors = list(v)
         else:
             v_is_tensor = True
-            vector_num = 1
             vectors = [v]
 
-        for index, vector in enumerate(vectors):
-            vectors[index] = vector.to(device)
-        vectors = tuple(vectors)
+        vector_chunks = split_list_to_chunks(
+            vectors, (len(vectors) + len(devices) - 1) // len(devices)
+        )
+        assert len(vector_chunks) <= len(devices)
 
-        products = autograd.functional.vhp(
-            f, tuple([parameter_snapshot] * vector_num), vectors, strict=True
-        )[1]
+        total_products = dict()
+        product_lock = threading.Lock()
+        for idx, vector_chunk in enumerate(vector_chunks):
+            def thread_func(idx, thread_device, vector_chunk):
+                nonlocal parameter_snapshot
+                for index, vector in enumerate(vector_chunk):
+                    vector_chunk[index] = vector.to(thread_device)
+                vector_chunk = tuple(vector_chunk)
+                products = autograd.functional.vhp(
+                    get_f(thread_device, inputs_dict[idx], targets_dict[idx]),
+                    tuple([parameter_dict[idx]] * len(vector_chunk)),
+                    vector_chunk,
+                    strict=True,
+                )[1]
+                with product_lock:
+                    total_products[idx] = products
+
+            device_threads.exec(thread_func, idx, devices[idx], vector_chunk)
+        device_threads.stop()
+        products = []
+        for idx in sorted(total_products.keys()):
+            products += total_products[idx]
         if v_is_tensor:
             return products[0]
-        return products
+        return [p.to(devices[0]) for p in products]
 
     return vhp_func
 
