d6ebf8924a86f47748c26a45d4173d2e7e1fdd2e
cyyever <cyyever@outlook.com>
2020-07-04 12:06:07 +0800

    use new code

--- hessian_vector_product.py
+++ hessian_vector_product.py
@@ -1,136 +1,166 @@
 #!/usr/bin/env python3
 
+import threading
 import copy
-import torch
 import numpy as np
+import torch
 import torch.autograd as autograd
 
-from device import get_device
-from util import parameters_to_vector, model_parameters_to_vector
+from cyy_naive_lib.thread_pool import ThreadPool
+
+from device import get_cuda_devices
+from util import (
+    parameters_to_vector,
+    model_parameters_to_vector,
+    set_model_attr,
+    del_model_attr,
+    split_list_to_chunks,
+)
 
 
-__cached_model_snapshots = dict()
+device_threads = ThreadPool()
 
 
-def clear_cached_model_snapshots():
-    global __cached_model_snapshots
-    __cached_model_snapshots = dict()
+class ModelSnapshot:
+    data = dict()
+    lock = threading.Lock()
+
+    @staticmethod
+    def clear(model_class):
+        with ModelSnapshot.lock:
+            ModelSnapshot.data.pop(model_class, None)
+
+    @staticmethod
+    def get(model_class, device):
+        device_str = str(device)
+        with ModelSnapshot.lock:
+            if model_class not in ModelSnapshot.data:
+                ModelSnapshot.data[model_class] = dict()
+            if device_str not in ModelSnapshot.data[model_class]:
+                ModelSnapshot.data[model_class][device_str] = []
+            return ModelSnapshot.data[model_class][device_str]
+
+    @staticmethod
+    def resize(model, device, new_number):
+        model_class = model.__class__.__name__
+        snapshots = ModelSnapshot.get(model_class, device)
+        while len(snapshots) < new_number:
+            snapshots.append(copy.deepcopy(model))
 
 
-def set_attr(obj, names, val):
-    if len(names) == 1:
-        delattr(obj, names[0])
-        setattr(obj, names[0], val)
-    else:
-        set_attr(getattr(obj, names[0]), names[1:], val)
+def load_model_parameters(model, parameters, param_shape_dict, device):
+    bias = 0
+    for name, shape in param_shape_dict.items():
+        param_element_num = np.prod(shape)
+        param = parameters.narrow(
+            0, bias, param_element_num).view(
+            *shape).to(device)
+        set_model_attr(model, name.split("."), param, as_parameter=False)
+        bias += param_element_num
+    assert bias == len(parameters)
 
 
 def get_hessian_vector_product_func(model, batch, loss_fun, for_train):
 
+    # get all parameters and names
+    params = []
+    param_shape_dict = dict()
     model_snapshot = copy.deepcopy(model)
 
-    # get all parameters and names
-    names = []
-    params = []
-    param_shapes = []
     for name, param in model_snapshot.named_parameters():
-        names.append(name)
         params.append(param.detach())
-        param_shapes.append(param.shape)
+        param_shape_dict[name] = param.shape
 
-    device = torch.device("cuda:2")
-    device2 = torch.device("cuda:2")
-    assert device == device2
-    # device = get_device()
-    inputs = batch[0].to(device)
-    targets = batch[1].to(device)
+    for name in param_shape_dict:
+        del_model_attr(model_snapshot, name.split("."))
+
     parameter_snapshot = parameters_to_vector(params)
 
-    def load_model_parameters(model, parameters, device):
-        global set_attr
-        nonlocal names, param_shapes
-        bias = 0
-        for name, shape in zip(names, param_shapes):
-            param_element_num = np.prod(shape)
-            param = (
-                parameters.narrow(
-                    0, bias, param_element_num).view(
-                    *shape).to(device))
-            set_attr(model, name.split("."), param)
-            bias += param_element_num
-        assert bias == len(parameters)
+    devices = get_cuda_devices()
+    inputs_dict = dict()
+    targets_dict = dict()
+    parameter_dict = dict()
 
-    def f(*args):
-        nonlocal inputs, targets, device, loss_fun, for_train, load_model_parameters
-        global __cached_model_snapshots
+    for idx, device in enumerate(devices):
+        inputs_dict[idx] = copy.deepcopy(batch[0]).to(device)
+        targets_dict[idx] = copy.deepcopy(batch[1]).to(device)
+        parameter_dict[idx] = copy.deepcopy(parameter_snapshot).to(device)
 
-        model_snapshots = __cached_model_snapshots[model_snapshot.__class__.__name__]
-        loss = None
-        for i, arg in enumerate(args):
-            cur_model_snapshot = model_snapshots[i]
-            load_model_parameters(cur_model_snapshot, arg, device)
-            cur_model_snapshot.zero_grad()
 
-            if for_train:
-                cur_model_snapshot.train()
-            else:
-                cur_model_snapshot.eval()
-            cur_model_snapshot.to(device)
-            if loss is None:
-                loss = loss_fun(cur_model_snapshot(inputs), targets)
-            else:
-                loss += loss_fun(cur_model_snapshot(inputs), targets)
-        return loss
+    def get_f(device, inputs, targets):
+        def f(*args):
+            nonlocal inputs, targets, loss_fun, for_train, param_shape_dict, model_snapshot, device
+            model_class = model_snapshot.__class__.__name__
+            ModelSnapshot.resize(model_snapshot, device, len(args))
+            model_snapshots = ModelSnapshot.get(model_class, device)
+            assert len(model_snapshots) >= len(args)
+            loss = None
+            for i, arg in enumerate(args):
+                cur_model_snapshot = model_snapshots[i]
+                load_model_parameters(
+                    cur_model_snapshot, arg, param_shape_dict, device)
+                if for_train:
+                    cur_model_snapshot.train()
+                else:
+                    cur_model_snapshot.eval()
+                if loss is None:
+                    loss = loss_fun(cur_model_snapshot(inputs), targets)
+                else:
+                    loss += loss_fun(cur_model_snapshot(inputs), targets)
+            return loss
+
+        return f
 
     def vhp_func(v):
-        nonlocal f, device
-        global __cached_model_snapshots
-
-        vector_num = 1
-        vectors = v
         v_is_tensor = False
         if isinstance(v, list):
-            vector_num = len(v)
             vectors = v
         elif isinstance(v, tuple):
-            vector_num = len(v)
             vectors = list(v)
         else:
             v_is_tensor = True
+            vectors = [v]
 
-        for index, vector in enumerate(vectors):
-            vectors[index] = vector.to(device)
-        vectors = tuple(vectors)
+        vector_chunks = split_list_to_chunks(
+            vectors, (len(vectors) + len(devices) - 1) // len(devices)
+        )
+        assert len(vector_chunks) <= len(devices)
 
-        parameter_snapshots = list()
-        while len(parameter_snapshots) < vector_num:
-            parameter_snapshots.append(
-                copy.deepcopy(parameter_snapshot).to(device).requires_grad_()
-            )
+        total_products = dict()
+        product_lock = threading.Lock()
+        for idx, vector_chunk in enumerate(vector_chunks):
+            def thread_func(idx, thread_device, vector_chunk):
+                nonlocal parameter_snapshot
+                for index, vector in enumerate(vector_chunk):
+                    vector_chunk[index] = vector.to(thread_device)
+                vector_chunk = tuple(vector_chunk)
+                products = autograd.functional.vhp(
+                    get_f(thread_device, inputs_dict[idx], targets_dict[idx]),
+                    tuple([parameter_dict[idx]] * len(vector_chunk)),
+                    vector_chunk,
+                    strict=True,
+                )[1]
+                with product_lock:
+                    total_products[idx] = products
 
-        model_class = model_snapshot.__class__.__name__
-        if model_class not in __cached_model_snapshots:
-            __cached_model_snapshots[model_class] = list()
-        model_snapshots = __cached_model_snapshots[model_class]
-
-        while len(model_snapshots) < vector_num:
-            model_snapshots.append(copy.deepcopy(model_snapshot))
-        assert len(model_snapshots) >= vector_num
-        assert len(parameter_snapshots) >= vector_num
-
-        products = autograd.functional.vhp(
-            f, tuple(parameter_snapshots[:vector_num]), vectors, strict=True
-        )[1]
+            device_threads.exec(thread_func, idx, devices[idx], vector_chunk)
+        device_threads.stop()
+        products = []
+        for idx in sorted(total_products.keys()):
+            products += total_products[idx]
         if v_is_tensor:
             return products[0]
-        return products
+        return [p.to(devices[0]) for p in products]
 
     return vhp_func
 
 
 if __name__ == "__main__":
+    import cProfile, pstats, io
+    from pstats import SortKey
     from configuration import get_task_configuration
     from cyy_naive_lib.time_counter import TimeCounter
+    pr = cProfile.Profile()
 
     trainer = get_task_configuration("MNIST", True)
     training_data_loader = torch.utils.data.DataLoader(
@@ -143,12 +173,14 @@ if __name__ == "__main__":
             trainer.model, batch, trainer.loss_fun, True
         )
         with TimeCounter() as c:
-            a = hvp_function([v, 2 * v])
+            pr.enable()
+            a = hvp_function(v)
             print("one use time ", c.elapsed_milliseconds())
             print(a)
             c.reset_start_time()
-            a = hvp_function([v, v])
-            print("one use time ", c.elapsed_milliseconds())
+            a = hvp_function([v, 2 * v])
+            print("two use time ", c.elapsed_milliseconds())
+            print(a)
             c.reset_start_time()
             a = hvp_function([v, v])
             print("two use time ", c.elapsed_milliseconds())
@@ -173,4 +205,10 @@ if __name__ == "__main__":
             c.reset_start_time()
             a = hvp_function([v] * 100)
             print("100 use time ", c.elapsed_milliseconds())
+            pr.disable()
+            s = io.StringIO()
+            sortby = SortKey.CUMULATIVE
+            ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+            ps.print_stats()
+            print(s.getvalue())
         break
