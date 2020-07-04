c0cdc6b125215588cb92182804fb965575a88bd6
cyy <cyyever@outlook.com>
2020-07-04 15:07:50 +0800

    fix code

--- hessian_vector_product.py
+++ hessian_vector_product.py
@@ -18,9 +18,6 @@ from util import (
 )
 
 
-device_task_queue = None
-
-
 class ModelSnapshot:
     data = dict()
     lock = threading.Lock()
@@ -60,8 +57,70 @@ def load_model_parameters(model, parameters, param_shape_dict, device):
     assert bias == len(parameters)
 
 
-def get_hessian_vector_product_func(model, batch, loss_fun, for_train):
+def __get_f(
+        device,
+        inputs,
+        targets,
+        loss_fun,
+        model_snapshot,
+        param_shape_dict):
+    def f(*args):
+        nonlocal inputs, targets, loss_fun, param_shape_dict, model_snapshot, device
+        model_class = model_snapshot.__class__.__name__
+        ModelSnapshot.resize(model_snapshot, device, len(args))
+        model_snapshots = ModelSnapshot.get(model_class, device)
+        assert len(model_snapshots) >= len(args)
+        loss = None
+        for i, arg in enumerate(args):
+            cur_model_snapshot = model_snapshots[i]
+            load_model_parameters(
+                cur_model_snapshot, arg, param_shape_dict, device)
+            if loss is None:
+                loss = loss_fun(cur_model_snapshot(inputs), targets)
+            else:
+                loss += loss_fun(cur_model_snapshot(inputs), targets)
+        return loss
 
+    return f
+
+
+def __thread_func(task):
+    (
+        idx,
+        thread_device,
+        vector_chunk,
+        total_products,
+        product_lock,
+        loss_fun,
+        model_snapshot,
+        parameter_vector,
+        inputs,
+        targets,
+        param_shape_dict,
+    ) = task
+    for index, vector in enumerate(vector_chunk):
+        vector_chunk[index] = vector.to(thread_device)
+    vector_chunk = tuple(vector_chunk)
+    products = autograd.functional.vhp(
+        __get_f(
+            thread_device,
+            inputs,
+            targets,
+            loss_fun,
+            model_snapshot,
+            param_shape_dict,
+        ),
+        tuple(
+            [parameter_vector] *
+            len(vector_chunk)),
+        vector_chunk,
+        strict=True,
+    )[1]
+    with product_lock:
+        total_products[idx] = products
+
+
+def get_hessian_vector_product_func(model, batch, loss_fun):
     # get all parameters and names
     params = []
     param_shape_dict = dict()
@@ -86,32 +145,7 @@ def get_hessian_vector_product_func(model, batch, loss_fun, for_train):
         targets_dict[idx] = copy.deepcopy(batch[1]).to(device)
         parameter_dict[idx] = copy.deepcopy(parameter_snapshot).to(device)
 
-    def get_f(device, inputs, targets):
-        def f(*args):
-            nonlocal inputs, targets, loss_fun, for_train, param_shape_dict, model_snapshot, device
-            model_class = model_snapshot.__class__.__name__
-            ModelSnapshot.resize(model_snapshot, device, len(args))
-            model_snapshots = ModelSnapshot.get(model_class, device)
-            assert len(model_snapshots) >= len(args)
-            loss = None
-            for i, arg in enumerate(args):
-                cur_model_snapshot = model_snapshots[i]
-                load_model_parameters(
-                    cur_model_snapshot, arg, param_shape_dict, device)
-                if for_train:
-                    cur_model_snapshot.train()
-                else:
-                    cur_model_snapshot.eval()
-                if loss is None:
-                    loss = loss_fun(cur_model_snapshot(inputs), targets)
-                else:
-                    loss += loss_fun(cur_model_snapshot(inputs), targets)
-            return loss
-
-        return f
-
     def vhp_func(v):
-        global device_task_queue
         v_is_tensor = False
         if isinstance(v, list):
             vectors = v
@@ -126,33 +160,30 @@ def get_hessian_vector_product_func(model, batch, loss_fun, for_train):
         )
         assert len(vector_chunks) <= len(devices)
 
-        def thread_func(task):
-            idx, thread_device, vector_chunk, total_products, product_lock = task
-            for index, vector in enumerate(vector_chunk):
-                vector_chunk[index] = vector.to(thread_device)
-            vector_chunk = tuple(vector_chunk)
-            products = autograd.functional.vhp(
-                get_f(thread_device, inputs_dict[idx], targets_dict[idx]),
-                tuple([parameter_dict[idx]] * len(vector_chunk)),
-                vector_chunk,
-                strict=True,
-            )[1]
-            with product_lock:
-                total_products[idx] = products
-
-        if device_task_queue is None:
-            device_task_queue = TaskQueue(
-                processor=thread_func, worker_num=len(devices)
-            )
-            device_task_queue.start()
+        device_task_queue = TaskQueue(
+            processor=__thread_func,
+            worker_num=len(devices))
+        device_task_queue.start()
         total_products = dict()
         product_lock = threading.Lock()
         for idx, vector_chunk in enumerate(vector_chunks):
             device_task_queue.add_task(
-                (idx, devices[idx], vector_chunk, total_products, product_lock)
+                (
+                    idx,
+                    devices[idx],
+                    vector_chunk,
+                    total_products,
+                    product_lock,
+                    loss_fun,
+                    model_snapshot,
+                    parameter_dict[idx],
+                    inputs_dict[idx],
+                    targets_dict[idx],
+                    param_shape_dict,
+                )
             )
 
-        device_task_queue.join()
+        device_task_queue.stop()
         products = []
         for idx in sorted(total_products.keys()):
             products += total_products[idx]
@@ -172,8 +203,6 @@ if __name__ == "__main__":
     from configuration import get_task_configuration
     from cyy_naive_lib.time_counter import TimeCounter
 
-    pr = cProfile.Profile()
-
     trainer = get_task_configuration("MNIST", True)
     training_data_loader = torch.utils.data.DataLoader(
         trainer.training_dataset, batch_size=16, shuffle=True,
@@ -182,10 +211,18 @@ if __name__ == "__main__":
     v = torch.ones(parameter_vector.shape)
     for batch in training_data_loader:
         hvp_function = get_hessian_vector_product_func(
-            trainer.model, batch, trainer.loss_fun, True
+            trainer.model, batch, trainer.loss_fun
         )
+        a = hvp_function([v, 2 * v])
+        print(a)
+        trainer = get_task_configuration("MNIST", True)
+        hvp_function = get_hessian_vector_product_func(
+            trainer.model, batch, trainer.loss_fun
+        )
+        a = hvp_function([v, 2 * v])
+        print(a)
+
         with TimeCounter() as c:
-            pr.enable()
             a = hvp_function(v)
             print("one use time ", c.elapsed_milliseconds())
             print(a)
@@ -211,17 +248,12 @@ if __name__ == "__main__":
             c.reset_start_time()
             a = hvp_function([v] * 10)
             print("10 use time ", c.elapsed_milliseconds())
-            while True:
-                c.reset_start_time()
-                a = hvp_function([v] * 100)
-                print("100 use time ", c.elapsed_milliseconds())
             c.reset_start_time()
             a = hvp_function([v] * 100)
             print("100 use time ", c.elapsed_milliseconds())
-            pr.disable()
-            s = io.StringIO()
-            sortby = SortKey.CUMULATIVE
-            ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-            ps.print_stats()
-            print(s.getvalue())
+            with cProfile.Profile() as pr:
+                c.reset_start_time()
+                a = hvp_function([v] * 100)
+                print("100 use time ", c.elapsed_milliseconds())
+            pstats.Stats(pr).sort_stats(SortKey.CUMULATIVE).print_stats()
         break
