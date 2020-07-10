22655e505b5708a5d5889904248ddda230b3f373
cyy <cyyever@outlook.com>
2020-07-10 16:05:18 +0800

    refactoring code

--- visualization.py
+++ visualization.py
@@ -4,23 +4,15 @@ import torch
 
 class Window:
     cur_env = "main"
-    envs = {cur_env: dict()}
+    # envs = {cur_env: dict()}
 
     @staticmethod
     def set_env(env):
         Window.cur_env = env
-        if Window.cur_env not in Window.envs:
-            Window.envs[Window.cur_env] = dict()
 
-    @staticmethod
-    def get(title):
-        instances = Window.envs.get(Window.cur_env)
-        if title not in instances:
-            instance = Window(title, env=Window.cur_env)
-            instances[title] = instance
-        return instances[title]
-
-    def __init__(self, title, env, x_label="", y_label=""):
+    def __init__(self, title, env=None, x_label="", y_label=""):
+        if env is None:
+            env = Window.cur_env
         self.vis = visdom.Visdom(env=env)
         self.env = env
         self.win = None
@@ -28,31 +20,23 @@ class Window:
         self.x_label = x_label
         self.y_label = y_label
 
-    def plot_learning_rate(self, epoch, learning_rate):
-        self.__plot_line(
-            torch.LongTensor([epoch]),
-            torch.Tensor([learning_rate]),
-            x_label="Epoch",
-            y_label="Learning Rate",
-        )
+    # def plot_scalar_by_epoch(self, epoch, scalar, y_label=None, name=None):
+    #     if y_label is None:
+    #         y_label = self.y_label
 
-    def plot_scalar_by_epoch(self, epoch, scalar, y_label=None, name=None):
-        if y_label is None:
-            y_label = self.y_label
+    #     self.__plot_line(
+    #         torch.LongTensor([epoch]),
+    #         torch.Tensor([scalar]),
+    #         x_label="Epoch",
+    #         y_label=y_label,
+    #         name=name,
+    #     )
 
-        self.__plot_line(
-            torch.LongTensor([epoch]),
-            torch.Tensor([scalar]),
-            x_label="Epoch",
-            y_label=y_label,
-            name=name,
-        )
+    # def plot_loss(self, epoch, loss, name=None):
+    #     return self.plot_scalar_by_epoch(epoch, loss, "Loss", name)
 
-    def plot_loss(self, epoch, loss, name=None):
-        return self.plot_scalar_by_epoch(epoch, loss, "Loss", name)
-
-    def plot_accuracy(self, epoch, accuracy, name=None):
-        return self.plot_scalar_by_epoch(epoch, accuracy, "Accuracy", name)
+    # def plot_accuracy(self, epoch, accuracy, name=None):
+    #     return self.plot_scalar_by_epoch(epoch, accuracy, "Accuracy", name)
 
     def __plot_line(self, x, y, x_label=None, y_label=None, name=None):
         if self.win is not None and not self.vis.win_exists(self.win):
@@ -110,3 +94,28 @@ class Window:
 
     def save(self):
         self.vis.save([self.env])
+
+
+class EpochWindow(Window):
+    def __init__(self, title, env=None, y_label=""):
+        super().__init__(title, env=env, x_label="Epoch", y_label=y_label)
+
+    def plot_learning_rate(self, epoch, learning_rate):
+        self.plot_scalar(epoch, learning_rate, y_label="Learning Rate")
+
+    def plot_loss(self, epoch, loss, name=None):
+        return self.plot_scalar(epoch, loss, y_label="Loss", name=name)
+
+    def plot_accuracy(self, epoch, accuracy, name=None):
+        return self.plot_scalar(epoch, accuracy, y_label="Accuracy", name=name)
+
+    def plot_scalar(self, epoch, scalar, y_label=None, name=None):
+        if y_label is None:
+            y_label = self.y_label
+
+        self.__plot_line(
+            torch.LongTensor([epoch]),
+            torch.Tensor([scalar]),
+            y_label=y_label,
+            name=name,
+        )
