5287b4543afde5d78aef1cbacbf615df08bfa3e6
cyy <cyyever@outlook.com>
2020-07-19 12:53:21 +0800

    fix code

--- configuration.py
+++ configuration.py
@@ -23,20 +23,13 @@ def get_task_configuration(task_name, for_training):
     model = None
     loss_fun = None
     hyper_parameter = None
+    momentum = 0.9
+    dataset_name = task_name
     if task_name == "MNIST":
         model = LeNet5()
         if for_training:
             hyper_parameter = HyperParameter(
                 epochs=50, batch_size=64, learning_rate=0.01, weight_decay=1
-            )
-
-            hyper_parameter.set_optimizer_factory(
-                lambda params, learning_rate, weight_decay, dataset: optim.SGD(
-                    params,
-                    momentum=0.9,
-                    lr=learning_rate,
-                    weight_decay=(weight_decay / len(dataset)),
-                )
             )
 
             hyper_parameter.set_lr_scheduler_factory(
@@ -52,15 +45,6 @@ def get_task_configuration(task_name, for_training):
                 epochs=50, batch_size=64, learning_rate=0.01, weight_decay=1
             )
 
-            hyper_parameter.set_optimizer_factory(
-                lambda params, learning_rate, weight_decay, dataset: optim.SGD(
-                    params,
-                    momentum=0.9,
-                    lr=learning_rate,
-                    weight_decay=(weight_decay / len(dataset)),
-                )
-            )
-
             hyper_parameter.set_lr_scheduler_factory(
                 lambda optimizer: optim.lr_scheduler.ReduceLROnPlateau(
                     optimizer, verbose=True, factor=0.5, patience=2
@@ -74,15 +58,6 @@ def get_task_configuration(task_name, for_training):
                 epochs=350, batch_size=128, learning_rate=0.1, weight_decay=1
             )
 
-            hyper_parameter.set_optimizer_factory(
-                lambda params, learning_rate, weight_decay, dataset: optim.SGD(
-                    params,
-                    lr=learning_rate,
-                    momentum=0.9,
-                    weight_decay=(weight_decay / len(dataset)),
-                )
-            )
-
             hyper_parameter.set_lr_scheduler_factory(
                 lambda optimizer: optim.lr_scheduler.ReduceLROnPlateau(
                     optimizer, verbose=True, factor=0.1
@@ -91,21 +66,25 @@ def get_task_configuration(task_name, for_training):
             # hyper_parameter.set_lr_scheduler_factory(
             #     lambda optimizer: optim.lr_scheduler.StepLR(
             #         optimizer, step_size=10))
+    elif task_name == "CIFAR10_LENET":
+        dataset_name = "CIFAR10"
+        model = LeNet5(input_channels=3)
+        if for_training:
+            hyper_parameter = HyperParameter(
+                epochs=350, batch_size=128, learning_rate=0.1, weight_decay=1
+            )
+
+            hyper_parameter.set_lr_scheduler_factory(
+                lambda optimizer: optim.lr_scheduler.ReduceLROnPlateau(
+                    optimizer, verbose=True, factor=0.1
+                )
+            )
 
     elif task_name == "STL10":
         model = torchvision.models.densenet121(num_classes=10)
         if for_training:
             hyper_parameter = HyperParameter(
                 epochs=350, batch_size=32, learning_rate=0.1
-            )
-
-            hyper_parameter.set_optimizer_factory(
-                lambda params, learning_rate, weight_decay, dataset: optim.SGD(
-                    params,
-                    lr=learning_rate,
-                    momentum=0.9,
-                    weight_decay=(weight_decay / len(dataset)),
-                )
             )
 
             hyper_parameter.set_lr_scheduler_factory(
@@ -115,12 +94,22 @@ def get_task_configuration(task_name, for_training):
     else:
         raise NotImplementedError(task_name)
 
-    validation_dataset = get_dataset(task_name, False)
+    if hyper_parameter.optimizer_factory is None:
+        hyper_parameter.set_optimizer_factory(
+            lambda params, learning_rate, weight_decay, dataset: optim.SGD(
+                params,
+                momentum=momentum,
+                lr=learning_rate,
+                weight_decay=(weight_decay / len(dataset)),
+            )
+        )
+
+    validation_dataset = get_dataset(dataset_name, False)
 
     if loss_fun is None:
         loss_fun = choose_loss_function(model)
     if for_training:
-        training_dataset = get_dataset(task_name, True)
+        training_dataset = get_dataset(dataset_name, True)
         trainer = Trainer(model, loss_fun, training_dataset)
         trainer.set_hyper_parameter(hyper_parameter)
         trainer.validation_dataset = validation_dataset
