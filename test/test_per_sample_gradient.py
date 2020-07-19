1a10e694ddcec1676fd57d595aeb532df260174e
cyy <cyyever@outlook.com>
2020-07-20 00:24:06 +0800

    add code

---
+++ test/test_per_sample_gradient.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import torch
+from cyy_naive_lib.time_counter import TimeCounter
+
+from configuration import get_task_configuration
+from per_sample_gradient import get_per_sample_gradient
+
+trainer = get_task_configuration("MNIST", True)
+training_data_loader = torch.utils.data.DataLoader(
+    trainer.training_dataset, batch_size=64, shuffle=True,
+)
+for batch in training_data_loader:
+    with TimeCounter() as c:
+        get_per_sample_gradient(
+            trainer.model,
+            trainer.loss_fun,
+            batch[0],
+            batch[1])
