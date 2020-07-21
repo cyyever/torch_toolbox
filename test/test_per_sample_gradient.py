1438bde87125cd6601110af0bbca661cab655772
cyy <cyyever@outlook.com>
2020-07-21 21:40:31 +0800

    fix code

--- test/test_per_sample_gradient.py
+++ test/test_per_sample_gradient.py
@@ -6,19 +6,21 @@ from cyy_naive_lib.time_counter import TimeCounter
 from configuration import get_task_configuration
 from per_sample_gradient import get_per_sample_gradient
 
-trainer = get_task_configuration("MNIST", True)
-training_data_loader = torch.utils.data.DataLoader(
-    trainer.training_dataset, batch_size=64, shuffle=True,
-)
 
-cnt = 0
-for batch in training_data_loader:
-    with TimeCounter() as c:
-        get_per_sample_gradient(
-            trainer.model,
-            trainer.loss_fun,
-            batch[0],
-            batch[1])
-    cnt += 1
-    if cnt > 3:
-        break
+def test_get_per_sample_gradient():
+    trainer = get_task_configuration("MNIST", True)
+    training_data_loader = torch.utils.data.DataLoader(
+        trainer.training_dataset, batch_size=64, shuffle=True,
+    )
+
+    cnt = 0
+    for batch in training_data_loader:
+        with TimeCounter():
+            get_per_sample_gradient(
+                trainer.model,
+                trainer.loss_fun,
+                batch[0],
+                batch[1])
+        cnt += 1
+        if cnt > 3:
+            break
