84ff435e4ddbbb58993e49cb82377ae2de74dec6
cyy <cyyever@outlook.com>
2020-08-01 01:33:32 +0800

    fix  code

--- inverse_hessian_vector_product.py
+++ inverse_hessian_vector_product.py
@@ -5,9 +5,11 @@ from hessian_vector_product import get_hessian_vector_product_func
 
 
 def stochastic_inverse_hessian_vector_product(
-        model, dataset, loss_fun, v, loop=None):
+    model, dataset, loss_fun, v, loop=None, batch_size=1
+):
     data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=1, shuffle=True,)
+        dataset, batch_size=batch_size, shuffle=True,
+    )
     t = 0
     cur_product = v
 
@@ -19,7 +21,9 @@ def stochastic_inverse_hessian_vector_product(
             hvp_function = get_hessian_vector_product_func(
                 model, batch, loss_fun)
 
+            print("hvp_function(cur_product) is ", hvp_function(cur_product))
             cur_product = v + cur_product - hvp_function(cur_product)
+            print("cur_product is ", cur_product)
             t += 1
-        if t > loop:
-            return cur_product
+            if t > loop:
+                return cur_product
