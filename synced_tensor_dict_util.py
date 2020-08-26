c541615881738e13e8d2ddfdbf1285f884e1ff70
cyy <cyyever@outlook.com>
2020-08-26 22:44:21 +0800

    add api

--- synced_tensor_dict_util.py
+++ synced_tensor_dict_util.py
@@ -2,15 +2,14 @@ from typing import Callable
 from cyy_naive_lib.list_op import split_list_to_chunks
 
 
-def iterate_over_synced_tensor_dict(dict, cb: Callable):
-    keys = set(dict.keys())
-    in_memory_keys = set(dict.in_memory_keys())
+def iterate_over_synced_tensor_dict(tensor_dict, cb: Callable):
+    keys = set(tensor_dict.keys())
+    in_memory_keys = set(tensor_dict.in_memory_keys())
     for k in in_memory_keys:
-        cb(k, dict[k])
+        cb(k, tensor_dict[k])
     remain_keys = keys - in_memory_keys
-    for chunk in split_list_to_chunks(remain_keys, 100):
-        dict.prefetch(chunk)
+    cache_size = tensor_dict.get_in_memory_number()
+    for chunk in split_list_to_chunks(remain_keys, cache_size // 2):
+        tensor_dict.prefetch(chunk)
         for k in chunk:
-            cb(k, dict[k])
-
-        # self.cache_size // 3):
+            cb(k, tensor_dict[k])
