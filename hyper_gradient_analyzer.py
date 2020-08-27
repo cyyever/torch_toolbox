fa3a1681dcb68aaa58c76b74bb10dae5791bbbd1
cyy <cyyever@outlook.com>
2020-08-27 22:49:38 +0800

    fix code

--- hyper_gradient_analyzer.py
+++ hyper_gradient_analyzer.py
@@ -1,11 +1,11 @@
 import copy
 import tempfile
 
+from cyy_naive_lib.log import get_logger
 
 from .dataset import sub_dataset
 from .hyper_gradient_trainer import HyperGradientTrainer
 from .synced_tensor_dict_util import iterate_over_synced_tensor_dict
-from cyy_naive_lib.log import get_logger
 
 
 class HyperGradientAnalyzer:
@@ -55,14 +55,13 @@ class HyperGradientAnalyzer:
                     hyper_gradient_sum += hyper_gradient
             hyper_gradient_sum_dict[str(k)] = hyper_gradient_sum
         contribution_dict = dict()
-        for (test_key, test_subset_gradient) in self.get_test_gradients(
-            test_subset_dict
-        ):
-            for (training_key, hyper_gradient_sum) in iterate_over_synced_tensor_dict(
-                    hyper_gradient_sum_dict):
-                training_key = int(training_key)
-                if training_key not in contribution_dict:
-                    contribution_dict[training_key] = dict()
+        for (training_key, hyper_gradient_sum) in iterate_over_synced_tensor_dict(
+                hyper_gradient_sum_dict):
+            training_key = int(training_key)
+            contribution_dict[training_key] = dict()
+            for (test_key, test_subset_gradient) in self.get_test_gradients(
+                test_subset_dict
+            ):
                 contribution_dict[training_key][test_key] = (
                     -(test_subset_gradient @ hyper_gradient_sum) / training_set_size
                 ).data.item()
@@ -84,18 +83,17 @@ class HyperGradientAnalyzer:
         for (sample_index, hyper_gradient) in iterate_over_synced_tensor_dict(
             self.hyper_gradient_matrix, training_subset_indices
         ):
-            sample_index = int(sample_index)
             get_logger().info("use sample %s", sample_index)
+            sample_index = int(sample_index)
+            contribution_dict[sample_index] = dict()
             for (test_key, test_subset_gradient) in iterate_over_synced_tensor_dict(
                     test_subset_gradient_dict):
-                if sample_index not in contribution_dict:
-                    contribution_dict[sample_index] = dict()
                 contribution_dict[sample_index][test_key] = (
                     -(test_subset_gradient @ hyper_gradient) / training_set_size
                 ).data.item()
         return contribution_dict
 
-    def get_test_gradients(self, test_subset_dict):
+    def get_test_gradients(self, test_subset_dict: dict):
         tmp_validator = copy.deepcopy(self.validator)
         for test_key, indices in test_subset_dict.items():
             subset = sub_dataset(self.validator.dataset, indices)
@@ -103,11 +101,12 @@ class HyperGradientAnalyzer:
             tmp_validator.set_dataset(subset)
             yield (test_key, tmp_validator.get_gradient() * len(subset))
 
-    def get_test_gradient_dict(self, test_subset_dict):
+    def get_test_gradient_dict(self, test_subset_dict: dict):
         test_gredient_dict = HyperGradientTrainer.create_gradient_matrix(
             self.cache_size
         )
         test_gredient_dict.set_storage_dir(tempfile.gettempdir())
         for (test_key, gradient) in self.get_test_gradients(test_subset_dict):
+            assert test_key in test_gredient_dict
             test_gredient_dict[str(test_key)] = gradient
         return test_gredient_dict
