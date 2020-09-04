cabb5b092b474dc4d87c7f918b2f295151954259
cyy <cyyever@outlook.com>
2020-09-04 18:22:17 +0800

    fix code

--- gradient.py
+++ gradient.py
@@ -7,5 +7,5 @@ def get_dataset_gradients(dataset_dict: dict, validator, cache_size=512):
     for k, dataset in dataset_dict.items():
         tmp_validator = copy.deepcopy(validator)
         tmp_validator.set_dataset(dataset)
-        tensor_dict[k] = tmp_validator.get_gradient()
+        tensor_dict[str(k)] = tmp_validator.get_gradient()
     return tensor_dict
