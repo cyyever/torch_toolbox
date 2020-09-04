731a74933ba376b592759a04c43fda4d64c4be0a
cyy <cyyever@outlook.com>
2020-09-04 18:30:07 +0800

    fix code

--- gradient.py
+++ gradient.py
@@ -1,9 +1,11 @@
 import copy
+import tempfile
 from synced_tensor_dict_util import create_tensor_dict
 
 
 def get_dataset_gradients(dataset_dict: dict, validator, cache_size=512):
     tensor_dict = create_tensor_dict(cache_size)
+    tensor_dict.set_storage_dir(tempfile.gettempdir())
     for k, dataset in dataset_dict.items():
         tmp_validator = copy.deepcopy(validator)
         tmp_validator.set_dataset(dataset)
