76c163a96d06dee0fcca656551cddffad257ab89
cyy <cyyever@outlook.com>
2020-09-16 17:12:48 +0800

    add seed

--- util.py
+++ util.py
@@ -1,4 +1,19 @@
+import random
 import torch.nn as nn
+import torch
+import numpy as np
+
+from cyy_naive_lib.log import get_logger
+
+
+def set_reproducing_seed(reproducing_seed):
+    get_logger().warning("set reproducing seed")
+    assert isinstance(reproducing_seed, int)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(reproducing_seed)
+    random.seed(reproducing_seed)
+    np.random.seed(reproducing_seed)
 
 
 def cat_tensors_to_vector(tensors):
