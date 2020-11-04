7a9b7eb2be1e3a8e9432707b2b0564c3308c1ddf
cyy <cyyever@outlook.com>
2020-11-04 15:16:30 +0700

    fix code

--- util.py
+++ util.py
@@ -1,7 +1,5 @@
 import torch.nn as nn
 
-from cyy_naive_lib.log import get_logger
-
 
 def cat_tensors_to_vector(tensors):
     return nn.utils.parameters_to_vector([t.reshape(-1) for t in tensors])
