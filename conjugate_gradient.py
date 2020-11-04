267beebfedd9ac2750371446de92ed556b3c9e42
cyy <cyyever@outlook.com>
2020-11-04 15:15:33 +0700

    fix code

--- conjugate_gradient.py
+++ conjugate_gradient.py
@@ -39,13 +39,3 @@ def conjugate_gradient_general(
         if torch.sqrt(new_delta) < epsilon:
             break
     return x
-
-
-if __name__ == "__main__":
-    test_A = torch.Tensor([[1, 2], [2, 1]])
-    test_b = torch.Tensor([3, 4])
-    test_A = test_A.to(get_device())
-    test_b = test_b.to(get_device())
-    p = conjugate_gradient(test_A, test_b)
-    print(test_A @ p)
-    print(test_b)
---
+++ test/test_conjugate_gradient.py
@@ -0,0 +1,10 @@
+import torch
+from conjugate_gradient import conjugate_gradient
+from device import get_device
+
+
+def test_conjugate_gradient():
+    a = torch.Tensor([[1, 2], [2, 1]]).to(get_device())
+    b = torch.Tensor([3, 4]).to(get_device())
+    p = conjugate_gradient(a, b)
+    assert torch.all((a @ p).eq(b))
