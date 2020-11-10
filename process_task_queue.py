50606cb8dee2f5c5fa3a9e99f7961e2d9e7b657f
cyy <cyyever@outlook.com>
2020-11-10 19:20:24 +0700

    fix code

--- process_task_queue.py
+++ process_task_queue.py
@@ -1,44 +1,45 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import traceback
+from typing import Callable
 
 import torch.multiprocessing
+from cyy_naive_lib.log import default_logger
 
-from log import get_logger
 from device import get_cuda_devices
 
 
-class SentinelTask:
+class __SentinelTask:
     pass
 
 
 def worker(
         task_queue,
         result_queue,
-        processor_fun,
+        processor_fun: Callable,
         stop_event,
         extra_arguments: list):
     while not stop_event.is_set():
         task = task_queue.get()
-        if isinstance(task, SentinelTask):
+        if isinstance(task, __SentinelTask):
             break
         try:
             res = processor_fun(task, extra_arguments)
             if res is not None:
                 result_queue.put(res)
         except Exception as e:
-            get_logger().error("catch exception:%s", e)
-            get_logger().error("traceback:%s", traceback.format_exc())
+            default_logger.error("catch exception:%s", e)
+            default_logger.error("traceback:%s", traceback.format_exc())
 
 
 class ProcessTaskQueue:
-    def __init__(self, processor_fun, worker_num=1):
+    def __init__(self, processor_fun: Callable, worker_num: int = 1):
         self.ctx = torch.multiprocessing.get_context("spawn")
         self.task_queue = self.ctx.Queue()
         self.result_queue = self.ctx.Queue()
+        self.stop_event = self.ctx.Event()
         self.worker_num = worker_num
         self.processor_fun = processor_fun
-        self.processors = dict()
-        self.stop_event = self.ctx.Event()
+        self.processors: dict = dict()
         self.start()
 
     def start(self):
@@ -65,7 +66,7 @@ class ProcessTaskQueue:
             return
         # stop workers
         for _ in range(self.worker_num):
-            self.add_task(SentinelTask())
+            self.add_task(__SentinelTask())
         # block until all tasks are done
         self.join()
         self.processors = dict()
