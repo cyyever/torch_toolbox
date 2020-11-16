9c5706119463e9d6da66e6d9838026ca8ffbd55f
cyy <cyyever@outlook.com>
2020-11-16 15:32:02 +0800

    fix code

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import torch
 import dataset
 
 
 def test_dataset():
     mnist_dataset = dataset.get_dataset("MNIST", dataset.DatasetType.Training)
+    mnist_validation_dataset = dataset.get_dataset(
+        "MNIST", dataset.DatasetType.Validation
+    )
+    assert abs(
+        len(mnist_dataset) /
+        len(mnist_validation_dataset) -
+        5) < 0.01
     dataset.sub_dataset(mnist_dataset, [1])
     assert dataset.DatasetUtil(mnist_dataset).channel == 1
     cifar10_dataset = dataset.get_dataset(
