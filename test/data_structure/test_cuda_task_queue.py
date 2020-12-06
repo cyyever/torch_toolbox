190e41d9c3a5be3f953fdc2e60016fd0a6324c8e
cyy <cyyever@outlook.com>
2020-12-06 23:21:34 +0800

    fix code

--- test/data_structure/test_cuda_task_queue.py
+++ test/data_structure/test_cuda_task_queue.py
@@ -1,10 +1,12 @@
+from shutil import which
 from data_structure.cuda_task_queue import CUDATaskQueue
 
 
 def test_cuda_task_queue():
-    queue = CUDATaskQueue(
-        lambda task, device: print(
-            "hello world,device", device))
-    queue.start()
-    queue.add_task(())
-    queue.stop()
+    if which("nvcc"):
+        queue = CUDATaskQueue(
+            lambda task, device: print(
+                "hello world,device", device))
+        queue.start()
+        queue.add_task(())
+        queue.stop()
