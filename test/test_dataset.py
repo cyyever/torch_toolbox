d6b665f92660773273ad44ff3463918d892dbef3
cyy <cyyever@outlook.com>
2020-12-07 14:44:12 +0800

    fix code

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 
-import torch
 import dataset
 
 
 def test_dataset():
-    mnist_dataset = dataset.get_dataset("MNIST", dataset.MachineLearningPhase.Training)
+    mnist_dataset = dataset.get_dataset(
+        "MNIST", dataset.MachineLearningPhase.Training)
     mnist_validation_dataset = dataset.get_dataset(
         "MNIST", dataset.MachineLearningPhase.Validation
     )
-    assert abs(
-        len(mnist_dataset) /
-        len(mnist_validation_dataset) -
-        5) < 0.01
+    assert abs(len(mnist_dataset) / len(mnist_validation_dataset) - 5) < 0.01
     dataset.sub_dataset(mnist_dataset, [1])
     assert dataset.DatasetUtil(mnist_dataset).channel == 1
     cifar10_dataset = dataset.get_dataset(
-        "CIFAR10", dataset.MachineLearningPhase.Training)
+        "CIFAR10", dataset.MachineLearningPhase.Training
+    )
     assert dataset.DatasetUtil(cifar10_dataset).channel == 3
     assert dataset.DatasetUtil(mnist_dataset).get_label_number() == 10
     assert dataset.DatasetUtil(cifar10_dataset).get_label_number() == 10
