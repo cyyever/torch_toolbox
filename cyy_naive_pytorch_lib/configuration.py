9ed0ee1e8b57dd6468eb1a059ed368f01263e125
cyy <cyyever@outlook.com>
2021-01-29 14:34:14 +0700

    fix code

--- cyy_naive_pytorch_lib/configuration.py
+++ cyy_naive_pytorch_lib/configuration.py
@@ -1,35 +1,29 @@
-from hyper_parameter import HyperParameter
-from trainer import Trainer, ClassificationTrainer, DetectionTrainer
 from dataset import get_dataset
+from hyper_parameter import HyperParameter, get_recommended_hyper_parameter
 from ml_types import MachineLearningPhase
-from hyper_parameter import get_recommended_hyper_parameter
 from model_factory import get_model
-from ml_types import ModelType
+from trainer import Trainer
 
 
 def get_trainer_from_configuration(
     dataset_name: str, model_name: str, hyper_parameter: HyperParameter = None
 ) -> Trainer:
     if hyper_parameter is None:
-        hyper_parameter = get_recommended_hyper_parameter(
-            dataset_name, model_name)
+        hyper_parameter = get_recommended_hyper_parameter(dataset_name, model_name)
         assert hyper_parameter is not None
 
     training_dataset = get_dataset(dataset_name, MachineLearningPhase.Training)
-    validation_dataset = get_dataset(
-        dataset_name, MachineLearningPhase.Validation)
+    validation_dataset = get_dataset(dataset_name, MachineLearningPhase.Validation)
     test_dataset = get_dataset(dataset_name, MachineLearningPhase.Test)
     model_with_loss = get_model(model_name, training_dataset)
-    trainer: Trainer = None
-    if model_with_loss.model_type == ModelType.Classification:
-        trainer = ClassificationTrainer(
-            model_with_loss, training_dataset, hyper_parameter
-        )
-    elif model_with_loss.model_type == ModelType.Detection:
-        trainer = DetectionTrainer(
-            model_with_loss,
-            training_dataset,
-            hyper_parameter)
+    # trainer: Trainer = None
+    # if model_with_loss.model_type == ModelType.Classification:
+    trainer = Trainer(model_with_loss, training_dataset, hyper_parameter)
+    # elif model_with_loss.model_type == ModelType.Detection:
+    #     trainer = DetectionTrainer(
+    #         model_with_loss,
+    #         training_dataset,
+    #         hyper_parameter)
     trainer.set_validation_dataset(validation_dataset)
     trainer.set_test_dataset(test_dataset)
     return trainer
