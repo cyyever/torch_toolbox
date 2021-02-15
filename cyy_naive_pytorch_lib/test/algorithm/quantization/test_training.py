379c4f5e03acb4c550ab2aeb77b5e755a82fa873
cyy <cyyever@outlook.com>
2021-02-15 15:32:04 +0700

    fix code

--- cyy_naive_pytorch_lib/test/algorithm/quantization/test_training.py
+++ cyy_naive_pytorch_lib/test/algorithm/quantization/test_training.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 from algorithm.quantization.trainer import QuantizationTrainer
+from basic_trainer import StopTrainingException
 from configuration import get_trainer_from_configuration
+from model_executor import ModelExecutorCallbackPoint
+
+
+def stop_training(*args, **kwargs):
+    raise StopTrainingException()
 
 
 def test_training():
     trainer = get_trainer_from_configuration("MNIST", "LeNet5")
     trainer.hyper_parameter.set_epoch(1)
     trainer = QuantizationTrainer(trainer)
+    trainer.trainer.add_callback(ModelExecutorCallbackPoint.AFTER_BATCH, stop_training)
     trainer.train()
