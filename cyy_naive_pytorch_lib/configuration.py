3f986ea3bf2058b453388b46077d01d1ebc9faa1
cyy <cyyever@outlook.com>
2021-02-22 13:29:50 +0800

    fix code

--- cyy_naive_pytorch_lib/configuration.py
+++ cyy_naive_pytorch_lib/configuration.py
@@ -5,7 +5,10 @@ from trainer import Trainer
 
 
 def get_trainer_from_configuration(
-    dataset_name: str, model_name: str, hyper_parameter: HyperParameter = None
+    dataset_name: str,
+    model_name: str,
+    hyper_parameter: HyperParameter = None,
+    trainer_type=Trainer,
 ) -> Trainer:
     if hyper_parameter is None:
         hyper_parameter = get_recommended_hyper_parameter(dataset_name, model_name)
@@ -13,5 +16,5 @@ def get_trainer_from_configuration(
 
     dc = DatasetCollection.get_by_name(dataset_name)
     model_with_loss = get_model(model_name, dc)
-    trainer = Trainer(model_with_loss, dc, hyper_parameter)
+    trainer = trainer_type(model_with_loss, dc, hyper_parameter)
     return trainer
