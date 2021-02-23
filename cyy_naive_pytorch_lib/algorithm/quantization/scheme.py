0e9918c72e78409a2b05c31f202fc691cc175311
cyy <cyyever@outlook.com>
2021-02-23 10:53:55 +0700

    fix code

--- cyy_naive_pytorch_lib/algorithm/quantization/scheme.py
+++ cyy_naive_pytorch_lib/algorithm/quantization/scheme.py
@@ -3,32 +3,62 @@ from typing import Callable, Tuple
 import torch
 
 
-def stochastic_quantization(quantization_level: int) -> Tuple[Callable, Callable]:
+def stochastic_quantization(
+    quantization_level: int, use_l2_norm: bool = False
+) -> Tuple[Callable, Callable]:
     """Implement Stochastic Quantization as described in QSGD: Communication-Efficient SGDvia Gradient Quantization and Encoding (https://arxiv.org/pdf/1610.02132.pdf)"""
 
     def quant(tensor: torch.Tensor):
-        nonlocal quantization_level
+        nonlocal quantization_level, use_l2_norm
+        tensor_shape = tensor.shape
         tensor = tensor.reshape(-1)
         assert len(tensor.shape) == 1
-        l2_norm = torch.norm(tensor)
-        assert l2_norm != 0
+
+        norm = None
+        if use_l2_norm:
+            norm = torch.linalg.norm(tensor)
+        else:
+            norm = torch.linalg.norm(tensor, ord=float("inf"))
+        assert norm > 0
         sign_tensor = torch.sign(tensor)
-        normalized_tensor = tensor.abs() / l2_norm
-        for idx, element in enumerate(normalized_tensor):
+        normalized_abs_tensor = tensor.abs() / norm
+        for idx, element in enumerate(normalized_abs_tensor):
             assert element <= 1
             for slot in range(quantization_level):
-                if element <= ((slot + 1) / quantization_level):
-                    prob = element * quantization_level / l2_norm - slot
-                    assert prob >= 0 and prob <= 1
+                if (
+                    (slot / quantization_level)
+                    <= element
+                    <= ((slot + 1) / quantization_level)
+                ):
+                    prob = element * quantization_level - slot
+                    assert 0 <= prob <= 1
                     m = torch.distributions.Bernoulli(prob)
                     if m.sample() == 1:
-                        normalized_tensor[idx] = (slot + 1) / quantization_level
+                        normalized_abs_tensor[idx] = slot + 1
                     else:
-                        normalized_tensor[idx] = slot / quantization_level
+                        normalized_abs_tensor[idx] = slot
+                    break
 
-        return (l2_norm, sign_tensor, normalized_tensor)
+        return (
+            norm,
+            sign_tensor,
+            normalized_abs_tensor.int(),
+            quantization_level,
+            tensor_shape,
+        )
 
-    def dequant():
-        return 1
+    def dequant(quantized_pair):
+        (
+            norm,
+            sign_tensor,
+            quantized_tensor,
+            quantization_level,
+            tensor_shape,
+        ) = quantized_pair
+        quantized_tensor = quantized_tensor.float()
+        quantized_tensor *= norm
+        res = quantized_tensor * sign_tensor / quantization_level
+        res.reshape(tensor_shape)
+        return res
 
-    return (quant(), dequant())
+    return (quant, dequant)
