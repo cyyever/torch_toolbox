9ac23d16a294fde66ba4a7249909d0b1fa58b87b
cyy <cyyever@outlook.com>
2021-02-23 10:53:55 +0700

    fix code

--- cyy_naive_pytorch_lib/test/algorithm/quantization/test_scheme.py
+++ cyy_naive_pytorch_lib/test/algorithm/quantization/test_scheme.py
@@ -5,6 +5,12 @@ from algorithm.quantization.scheme import stochastic_quantization
 
 
 def test_stochastic_quantization():
-    a = torch.rand(3, 5)
+    a = torch.rand(2, 2)
     res = stochastic_quantization(256)
-    res[0](a)
+    pair = res[0](a)
+    recovered_tensor = res[1](pair)
+    print(recovered_tensor, a)
+    res = stochastic_quantization(256, use_l2_norm=True)
+    pair = res[0](a)
+    recovered_tensor = res[1](pair)
+    print(recovered_tensor, a)
