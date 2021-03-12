94bc9d252b3d9690d37320656c5a9ef690dada6d
cyy <cyyever@outlook.com>
2021-03-12 15:19:11 +0700

    fix code

--- cyy_naive_pytorch_lib/metrics/metric.py
+++ cyy_naive_pytorch_lib/metrics/metric.py
@@ -1,8 +1,4 @@
-from typing import Callable
-
 from callback import Callback
-from ml_type import ModelExecutorCallbackPoint
-from model_executor import ModelExecutor
 
 
 class Metric2(Callback):
@@ -15,40 +11,6 @@ class Metric2(Callback):
 
     def _before_execute(self, *args, **kwargs):
         self.clear()
-
-    def get_epoch_metric(self, epoch, name):
-        epoch_data = self.__epoch_metrics.get(epoch, None)
-        if epoch_data is None:
-            return None
-        return epoch_data.get(name, None)
-
-    def _set_epoch_metric(self, epoch, name, data):
-        if epoch not in self.__epoch_metrics:
-            self.__epoch_metrics[epoch] = dict()
-        self.__epoch_metrics[epoch][name] = data
-
-
-class Metric:
-    def __init__(self, model_exetutor: ModelExecutor):
-        self._model_executor = model_exetutor
-        self.__callback_names: list = []
-        self.__epoch_metrics: dict = dict()
-        self.add_callback(
-            ModelExecutorCallbackPoint.BEFORE_EXECUTE,
-            lambda *args, **kwargs: self.clear(),
-        )
-
-    def add_callback(self, cb_point: ModelExecutorCallbackPoint, cb: Callable):
-        name = self.__class__.__name__ + "." + str(cb)
-        self._model_executor.add_named_callback(cb_point, name, cb)
-        self.__callback_names.append(name)
-
-    def remove_callbacks(self):
-        for name in self.__callback_names:
-            self._model_executor.remove_callback(name)
-
-    def clear(self):
-        self.__epoch_metrics.clear()
 
     def get_epoch_metric(self, epoch, name):
         epoch_data = self.__epoch_metrics.get(epoch, None)
--- cyy_naive_pytorch_lib/metrics/prob_metric.py
+++ cyy_naive_pytorch_lib/metrics/prob_metric.py
@@ -1,36 +1,22 @@
 import torch
 import torch.nn as nn
 
-from ml_type import ModelExecutorCallbackPoint
-from model_executor import ModelExecutor
-
-from .metric import Metric
+from .metric import Metric2
 
 
-class ProbabilityMetric(Metric):
-    def __init__(self, model_exetutor: ModelExecutor):
-        super().__init__(model_exetutor=model_exetutor)
-        self.__probs: dict = dict()
-        self.__cur_probs: dict = dict()
-
-        self.add_callback(ModelExecutorCallbackPoint.BEFORE_EPOCH, self.__reset_prob)
-        self.add_callback(ModelExecutorCallbackPoint.AFTER_BATCH, self.__compute_prob)
-        self.add_callback(ModelExecutorCallbackPoint.AFTER_EPOCH, self.__save_prob)
-
+class ProbabilityMetric(Metric2):
     def get_prob(self, epoch):
-        return self.__probs.get(epoch)
+        return self.get_epoch_metric(epoch, "prob")
 
-    def clear(self):
-        self.__probs.clear()
-
-    def __reset_prob(self, *args, **kwargs):
-        self.__cur_probs.clear()
-
-    def __compute_prob(self, *args, **kwargs):
+    def _after_batch(self, *args, **kwargs):
         batch = kwargs["batch"]
+        epoch = kwargs["epoch"]
         result = kwargs["result"]
         output = result["output"]
         last_layer = list(self._model_executor.model.modules())[-1]
+        epoch_prob = self.get_prob(epoch)
+        if epoch_prob is None:
+            epoch_prob = dict()
         for i, sample_index in enumerate(batch[2]):
             sample_index = sample_index.data.item()
 
@@ -42,7 +28,5 @@ class ProbabilityMetric(Metric):
             else:
                 raise RuntimeError("unsupported layer", type(last_layer))
             max_prob_index = torch.argmax(probs).data.item()
-            self.__cur_probs[sample_index] = probs[max_prob_index].data.item()
-
-    def __save_prob(self, model_exetutor,epoch):
-        self.__probs[epoch] = self.__cur_probs
+            epoch_prob[sample_index] = probs[max_prob_index].data.item()
+        self._set_epoch_metric(epoch, "prob", epoch_prob)
