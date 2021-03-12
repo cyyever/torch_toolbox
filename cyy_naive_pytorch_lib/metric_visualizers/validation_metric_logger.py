11dcc338dd0e5f84a2732ad7f2ed15e95249b58c
cyy <cyyever@outlook.com>
2021-03-12 22:04:58 +0700

    fix code

---
+++ cyy_naive_pytorch_lib/metric_visualizers/validation_metric_logger.py
@@ -0,0 +1,28 @@
+from cyy_naive_lib.log import get_logger
+
+from ml_type import MachineLearningPhase
+
+from .metric_visualizer import MetricVisualizer
+
+
+class ValidationMetricLogger(MetricVisualizer):
+    def __init__(self):
+        super().__init__(metric=None)
+
+    def _after_epoch(self, *args, **kwargs):
+        epoch = kwargs["epoch"]
+        model_executor = kwargs.get("model_executor")
+        get_logger().info(
+            "epoch: %s, validation loss: %s, accuracy = %s",
+            epoch,
+            model_executor.get_validation_metric(MachineLearningPhase.Validation).loss,
+            model_executor.get_validation_metric(
+                MachineLearningPhase.Validation
+            ).accuracy,
+        )
+        get_logger().info(
+            "epoch: %s, test loss: %s, accuracy = %s",
+            epoch,
+            model_executor.get_validation_metric(MachineLearningPhase.Test).loss,
+            model_executor.get_validation_metric(MachineLearningPhase.Test).accuracy,
+        )
