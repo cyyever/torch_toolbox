cbb50eff8c5caba194ecedb975607bb8f7fc3a15
cyy <cyyever@outlook.com>
2021-03-13 14:36:59 +0700

    fix code

--- cyy_naive_pytorch_lib/algorithm/quantization/qat.py
+++ cyy_naive_pytorch_lib/algorithm/quantization/qat.py
@@ -143,6 +143,62 @@ class QuantizationAwareTraining(Callback):
 
         return parameter_dict
 
+    def load_quantized_parameters(self, parameter_dict: dict) -> dict:
+        model_util = ModelUtil(self.__original_model)
+        processed_modules = set()
+        state_dict = self.__quantized_model.state_dict()
+        quantized_model_util = ModelUtil(self.__quantized_model)
+        for name, module in self.__original_model.named_modules():
+            if isinstance(module, torch.nn.modules.BatchNorm2d):
+                get_logger().debug("ignore BatchNorm2d %s", name)
+                torch.nn.init.ones_(module.weight)
+                torch.nn.init.zeros_(module.bias)
+                torch.nn.init.zeros_(module.running_mean)
+                torch.nn.init.ones_(module.running_var)
+                # module.eps = 0
+
+        for k in state_dict:
+            if k in ("scale", "zero_point", "quant.scale", "quant.zero_point"):
+                continue
+            if "." not in k:
+                continue
+            module_name = ".".join(k.split(".")[:-1])
+            if module_name in processed_modules:
+                continue
+            if not quantized_model_util.has_attr(module_name):
+                continue
+            sub_module = quantized_model_util.get_attr(module_name)
+            if module_name.startswith("module."):
+                module_name = module_name[len("module."):]
+            if isinstance(
+                sub_module,
+                (
+                    torch.nn.intrinsic.quantized.modules.conv_relu.ConvReLU2d,
+                    torch.nn.quantized.modules.linear.Linear,
+                    torch.nn.quantized.modules.conv.Conv2d,
+                    torch.nn.quantized.modules.batchnorm.BatchNorm2d,
+                ),
+            ):
+                processed_modules.add(module_name)
+                weight = parameter_dict[module_name + ".weight"]
+                if isinstance(weight, tuple):
+                    (weight, scale, zero_point) = weight
+                    weight = weight.float()
+                    for idx, v in enumerate(weight):
+                        weight[idx] = (v - zero_point[idx]) * scale[idx]
+                model_util.set_attr(module_name + ".weight", weight)
+
+                for suffix in [".bias", ".running_mean", ".running_var"]:
+                    attr_name = module_name + suffix
+                    if attr_name in parameter_dict:
+                        model_util.set_attr(attr_name, parameter_dict[attr_name])
+                continue
+            if not isinstance(
+                sub_module, torch.nn.quantized.modules.linear.LinearPackedParams
+            ):
+                get_logger().warning("unsupported sub_module type %s", type(sub_module))
+        return parameter_dict
+
     @staticmethod
     def get_fused_modules(model):
         modules = list(model.named_modules())
