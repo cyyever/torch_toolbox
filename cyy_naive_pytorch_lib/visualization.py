8470d408094ce1e37a1fa0a7bfdf8f279e1a9c3b
cyy <cyyever@outlook.com>
2021-04-02 20:11:40 +0800

    add code

--- cyy_naive_pytorch_lib/visualization.py
+++ cyy_naive_pytorch_lib/visualization.py
@@ -4,7 +4,7 @@ import visdom
 
 class Window:
     __envs: dict = dict()
-    __sessions: dict = {}
+    __windows: dict = dict()
 
     @staticmethod
     def save_envs():
@@ -13,23 +13,29 @@ class Window:
     def __init__(self, title, env=None):
         if env is None:
             env = "main"
-        if env not in Window.__sessions:
-            Window.__sessions[env] = visdom.Visdom(env=env)
-
-        self.vis = Window.__sessions[env]
+        self.env = env
         self.title = title
-        self.win = None
-        if env in Window.__envs:
-            self.win = Window.__envs[env].get(title, None)
         self.x_label = None
         self.y_label = None
         self.extra_opts = dict(ytick=True)
         self.showlegend = True
 
+    @property
+    def vis(self):
+        if self.env not in Window.__envs:
+            Window.__envs[self.env] = visdom.Visdom(env=self.env)
+        return Window.__envs[self.env]
+
+    @property
+    def win(self):
+        if self.env not in Window.__windows:
+            Window.__windows[self.env] = dict()
+        return Window.__windows[self.env].get(self.title, None)
+
     def set_opt(self, k: str, v):
         self.extra_opts[k] = v
 
-    def get_opts(self):
+    def __get_opts(self):
         opts = dict(title=self.title, showlegend=self.showlegend)
         opts.update(self.extra_opts)
         return opts
@@ -51,8 +57,8 @@ class Window:
             xlabel=self.x_label,
             ylabel=self.y_label,
         )
-        opts.update(self.get_opts())
-        self.win = self.vis.line(
+        opts.update(self.__get_opts())
+        win = self.vis.line(
             Y=y,
             X=x,
             win=self.win,
@@ -60,29 +66,29 @@ class Window:
             update=update,
             opts=opts,
         )
-        self._add_window()
+        self.__add_window(win)
 
     def plot_histogram(self, tensor):
         opts = dict(numbins=1024)
-        opts.update(self.get_opts())
-        self.win = self.vis.histogram(tensor.view(-1), win=self.win, opts=opts)
+        opts.update(self.__get_opts())
+        win = self.vis.histogram(tensor.view(-1), win=self.win, opts=opts)
+        self.__add_window(win)
 
     def plot_scatter(self, x, y=None, name=None):
         update = None
         if self.win is not None:
             update = "replace"
-        self.win = self.vis.scatter(
-            X=x, Y=y, win=self.win, name=name, update=update, opts=self.get_opts()
+        win = self.vis.scatter(
+            X=x, Y=y, win=self.win, name=name, update=update, opts=self.__get_opts()
         )
-        self._add_window()
+        self.__add_window(win)
 
     def save(self):
         self.vis.save([self.vis.env])
 
-    def _add_window(self):
-        if self.vis.env not in Window.__envs:
-            Window.__envs[self.vis.env] = dict()
-        Window.__envs[self.vis.env][self.title] = self.win
+    def __add_window(self, win):
+        if self.win is None:
+            Window.__envs[self.env][self.title] = win
 
 
 class IterationWindow(Window):
