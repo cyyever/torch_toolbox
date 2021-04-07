dd9f0bd263dca65f750989d4daac496521c44f35
cyy <cyyever@outlook.com>
2021-04-07 17:28:59 +0800

    add code

--- cyy_naive_pytorch_lib/dataset_collection.py
+++ cyy_naive_pytorch_lib/dataset_collection.py
@@ -1,4 +1,5 @@
 # import multiprocessing
+import json
 import os
 from typing import Callable, Dict, List
 
@@ -7,7 +8,7 @@ import torchvision
 import torchvision.transforms as transforms
 from cyy_naive_lib.log import get_logger
 
-from dataset import DatasetUtil
+from dataset import DatasetUtil, replace_dataset_labels, sub_dataset
 from hyper_parameter import HyperParameter
 from ml_type import MachineLearningPhase
 
@@ -35,7 +36,7 @@ class DatasetCollection:
 
     def get_dataset(self, phase: MachineLearningPhase) -> torch.utils.data.Dataset:
         assert phase in self.__datasets
-        return self.__datasets.get(phase)
+        return self.__datasets[phase]
 
     def get_dataloader(
         self,
@@ -48,9 +49,6 @@ class DatasetCollection:
             batch_size=hyper_parameter.batch_size,
             shuffle=(phase == MachineLearningPhase.Training),
         )
-        # num_workers=multiprocessing.cpu_count(),
-
-        # collate_fn=hyper_parameter.__collate_fn,
 
     @property
     def name(self):
@@ -246,3 +244,100 @@ class DatasetCollection:
         dc = DatasetCollection(training_dataset, validation_dataset, test_dataset, name)
         DatasetCollection.__dataset_collections[name] = dc
         return dc
+
+
+class DatasetCollectionConfig:
+    def __init__(self, dataset_name):
+        self.dataset_name = dataset_name
+        self.dataset_args = dict()
+        self.training_dataset_percentage = None
+        self.training_dataset_indices_path = None
+        self.training_dataset_label_map_path = None
+        self.training_dataset_label_map = None
+        self.training_dataset_label_noise_percentage = None
+
+    def add_args(self, parser):
+        parser.add_argument("--dataset_name", type=str, required=True)
+        parser.add_argument("--training_dataset_percentage", type=float, default=None)
+        parser.add_argument("--training_dataset_indices_path", type=str, default=None)
+        parser.add_argument(
+            "--training_dataset_label_noise_percentage", type=float, default=None
+        )
+
+    def load_args(self, args):
+        for attr in dir(args):
+            if attr.startswith("_"):
+                continue
+            if not hasattr(self, attr):
+                continue
+            get_logger().debug("set dataset collection config attr %s", attr)
+            value = getattr(args, attr)
+            if value is not None:
+                setattr(self, attr, value)
+
+    def create_dataset_collection(self, save_dir):
+        if self.dataset_name is None:
+            raise RuntimeError("dataset_name is None")
+
+        dc = DatasetCollection.get_by_name(self.dataset_name, **self.dataset_args)
+        dc.transform_dataset(
+            MachineLearningPhase.Training,
+            lambda dataset: self.__transform_training_dataset(dataset, save_dir),
+        )
+        return dc
+
+    def __transform_training_dataset(
+        self, training_dataset, save_dir
+    ) -> torch.utils.data.Dataset:
+        subset_indices = None
+        if self.training_dataset_percentage is not None:
+            subset_dict = DatasetUtil(training_dataset).iid_sample(
+                self.training_dataset_percentage
+            )
+            subset_indices = sum(subset_dict.values(), [])
+            with open(
+                os.path.join(save_dir, "training_dataset_indices.json"),
+                mode="wt",
+            ) as f:
+                json.dump(subset_indices, f)
+
+        if self.training_dataset_indices_path is not None:
+            assert subset_indices is None
+            get_logger().info(
+                "use training_dataset_indices_path %s",
+                self.training_dataset_indices_path,
+            )
+            with open(self.training_dataset_indices_path, "r") as f:
+                subset_indices = json.load(f)
+        if subset_indices is not None:
+            training_dataset = sub_dataset(training_dataset, subset_indices)
+
+        label_map = None
+        if self.training_dataset_label_noise_percentage:
+            label_map = DatasetUtil(training_dataset).randomize_subset_label(
+                self.training_dataset_label_noise_percentage
+            )
+            with open(
+                os.path.join(
+                    save_dir,
+                    "training_dataset_label_map.json",
+                ),
+                mode="wt",
+            ) as f:
+                json.dump(label_map, f)
+
+        if self.training_dataset_label_map_path is not None:
+            assert label_map is not None
+            get_logger().info(
+                "use training_dataset_label_map_path %s",
+                self.training_dataset_label_map_path,
+            )
+            self.training_dataset_label_map = json.load(
+                open(self.training_dataset_label_map_path, "r")
+            )
+
+        if self.training_dataset_label_map is not None:
+            training_dataset = replace_dataset_labels(
+                training_dataset, self.training_dataset_label_map_path
+            )
+        return training_dataset
