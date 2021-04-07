62b87b0f810c7dc3fb6c55090c22b0af1c5e643f
cyy <cyyever@outlook.com>
2021-04-07 17:53:14 +0800

    add code

--- cyy_naive_pytorch_lib/test/test_inference.py
+++ cyy_naive_pytorch_lib/test/test_inference.py
@@ -5,7 +5,7 @@ from ml_type import MachineLearningPhase
 
 def test_training():
     config = DefaultConfig(dataset_name="MNIST", model_name="LeNet5")
-    config.epoch = 1
+    config.hyper_parameter_config.epoch = 1
     trainer = config.create_trainer()
     inferencer = trainer.get_inferencer(MachineLearningPhase.Test)
     inferencer.inference()
