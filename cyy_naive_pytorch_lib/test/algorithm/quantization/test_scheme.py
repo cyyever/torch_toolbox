adc1a6f7fe5e23bf425495113babba3a291ee8c8
cyy <cyyever@outlook.com>
2021-04-14 16:19:05 +0800

    fix code

--- cyy_naive_pytorch_lib/test/algorithm/quantization/test_scheme.py
+++ cyy_naive_pytorch_lib/test/algorithm/quantization/test_scheme.py
@@ -17,6 +17,17 @@ def test_stochastic_quantization():
         "relative diff",
         torch.linalg.norm(recovered_tensor - a) / torch.linalg.norm(a),
     )
+
+    b = torch.rand(2, 100000)
+    data = {"key1": a, "key2": b}
+    pair = quant(data)
+    recovered_data = dequant(pair)
+    print(
+        "use_l2_norm relative diff",
+        torch.linalg.norm(recovered_data["key1"] - a) / torch.linalg.norm(a),
+        torch.linalg.norm(recovered_data["key2"] - b) / torch.linalg.norm(b),
+    )
+
     quant, dequant = stochastic_quantization(256, use_l2_norm=True)
     pair = quant(a)
     recovered_tensor = dequant(pair)
@@ -27,14 +38,4 @@ def test_stochastic_quantization():
         a,
         "relative diff",
         torch.linalg.norm(recovered_tensor - a) / torch.linalg.norm(a),
-    )
-
-    b = torch.rand(2, 100000)
-    data = {"key1": a, "key2": b}
-    pair = quant(data)
-    recovered_data = dequant(pair)
-    print(
-        "use_l2_norm relative diff",
-        torch.linalg.norm(recovered_data["key1"] - a) / torch.linalg.norm(a),
-        torch.linalg.norm(recovered_data["key2"] - b) / torch.linalg.norm(b),
     )
