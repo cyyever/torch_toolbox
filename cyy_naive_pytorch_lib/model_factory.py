933e596e037d1a28e232a9ace87d8f9f96ab5d61
cyy <cyyever@outlook.com>
2021-04-20 19:49:21 +0800

    fix code

--- cyy_naive_pytorch_lib/model_factory.py
+++ cyy_naive_pytorch_lib/model_factory.py
@@ -6,7 +6,7 @@ import torchvision
 from cyy_naive_lib.log import get_logger
 
 from dataset_collection import DatasetCollection
-from ml_type import MachineLearningPhase, ModelType
+from ml_type import ModelType
 from model_with_loss import ModelWithLoss
 
 
@@ -91,10 +91,9 @@ def get_model(
             input_size = getattr(model_with_loss.model.__class__, "input_size", None)
             if input_size is not None:
                 get_logger().warning("use input_size %s", input_size)
-                for phase in MachineLearningPhase:
-                    dataset_collection.get_dataset_util(phase).append_transform(
-                        torchvision.transforms.Resize(input_size)
-                    )
+                dataset_collection.append_transform(
+                    torchvision.transforms.Resize(input_size)
+                )
             return model_with_loss
         except TypeError as e:
             retry = False
