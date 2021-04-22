343346d29f1487bd639f522fdf7ba42aa7c9837b
cyy <cyyever@outlook.com>
2021-04-22 14:06:28 +0800

    add code

---
+++ cyy_naive_pytorch_lib/hooks/model_executor_logger.py
@@ -0,0 +1,27 @@
+from cyy_naive_lib.log import get_logger
+
+from callback import Callback
+from model_util import ModelUtil
+
+
+class ModelExecutorLogger(Callback):
+    def _before_execute(self, **kwargs):
+        model_executor = kwargs["model_executor"]
+        model_util = ModelUtil(model_executor.model)
+        get_logger().info("dataset is %s", model_executor.dataset)
+        get_logger().info("model is %s", model_executor.model_with_loss.model)
+        get_logger().info("loss function is %s", model_executor.model_with_loss.loss)
+        get_logger().info(
+            "parameter number is %s",
+            len(model_util.get_parameter_list()),
+        )
+        get_logger().info("use device %s", model_executor.device)
+        get_logger().info("hyper_parameter is %s", model_executor.hyper_parameter)
+        if hasattr(model_executor, "get_optimizer"):
+            get_logger().info(
+                "optimizer is %s", getattr(model_executor, "get_optimizer")()
+            )
+        if hasattr(model_executor, "get_lr_scheduler "):
+            get_logger().info(
+                "lr_scheduler is %s", getattr(model_executor, "get_lr_scheduler ")()
+            )
--- cyy_naive_pytorch_lib/metric_visualizers/loss_metric_logger.py
+++ cyy_naive_pytorch_lib/metric_visualizers/loss_metric_logger.py
@@ -1,27 +1,9 @@
 from cyy_naive_lib.log import get_logger
-from model_util import ModelUtil
 
 from .metric_visualizer import MetricVisualizer
 
 
 class LossMetricLogger(MetricVisualizer):
-    def __init__(self):
-        super().__init__(metric=None)
-
-    def _before_execute(self, **kwargs):
-        trainer = kwargs["model_executor"]
-        model_util = ModelUtil(trainer.model)
-        get_logger().info("dataset size is %s", len(trainer.dataset))
-        get_logger().info("use device %s", trainer.device)
-        get_logger().info(
-            "hyper_parameter is %s, optimizer is %s, lr_scheduler is %s, %s, parameter number is %s",
-            trainer.hyper_parameter,
-            trainer.get_optimizer(),
-            trainer.get_lr_scheduler(),
-            trainer.model_with_loss,
-            len(model_util.get_parameter_list()),
-        )
-
     def _after_batch(self, **kwargs):
         model_executor = kwargs.get("model_executor")
         batch_size = kwargs["batch_size"]
