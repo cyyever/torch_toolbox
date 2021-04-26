b1ab492f994f869b3f4356c6d3b2260c4c7d4ca1
cyy <cyyever@outlook.com>
2021-04-26 18:13:10 +0800

    add code

--- cyy_naive_pytorch_lib/metric_visualizers/loss_metric_logger.py
+++ cyy_naive_pytorch_lib/metric_visualizers/loss_metric_logger.py
@@ -4,6 +4,13 @@ from .metric_visualizer import MetricVisualizer
 
 
 class LossMetricLogger(MetricVisualizer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.prefix = ""
+
+    def set_prefix(self, prefix: str):
+        self.prefix = prefix
+
     def _after_batch(self, **kwargs):
         model_executor = kwargs.get("model_executor")
         batch_size = kwargs["batch_size"]
@@ -11,7 +18,8 @@ class LossMetricLogger(MetricVisualizer):
         ten_batches = len(model_executor.dataset) // (10 * batch_size)
         if ten_batches == 0 or batch_index % ten_batches == 0:
             get_logger().info(
-                "epoch: %s, batch: %s, learning rate: %s, batch loss: %s",
+                "%s epoch: %s, batch: %s, learning rate: %s, batch loss: %s",
+                self.prefix,
                 kwargs["epoch"],
                 batch_index,
                 model_executor.get_data("cur_learning_rates", None),
