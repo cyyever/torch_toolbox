0c76e242fa3c2f0c9f5905a312389c7276841e3b
cyy <cyyever@outlook.com>
2021-04-28 14:13:43 +0800

    add code

--- cyy_naive_pytorch_lib/test/algorithm/test_sample_gradient.py
+++ cyy_naive_pytorch_lib/test/algorithm/test_sample_gradient.py
@@ -3,18 +3,18 @@
 import torch
 from cyy_naive_lib.profiling import Profile
 from cyy_naive_lib.time_counter import TimeCounter
-from default_config import DefaultConfig
-from device import get_device
 
 from algorithm.sample_gradient.sample_gradient import (get_sample_gradient,
                                                        stop_task_queue)
+from default_config import DefaultConfig
+from device import get_device
 
 
 def test_get_sample_gradient():
-    trainer = DefaultConfig("MNIST", "LeNet5").create_trainer()
+    trainer = DefaultConfig("CIFAR10", "resnet18").create_trainer()
     training_data_loader = torch.utils.data.DataLoader(
         trainer.dataset,
-        batch_size=64,
+        batch_size=8,
         shuffle=True,
     )
 
@@ -29,8 +29,8 @@ def test_get_sample_gradient():
         if cnt > 3:
             break
 
-    with Profile():
-        for batch in training_data_loader:
-            get_sample_gradient(trainer.model_with_loss, batch[0], batch[1])
-            break
+    # with Profile():
+    #     for batch in training_data_loader:
+    #         get_sample_gradient(trainer.model_with_loss, batch[0], batch[1])
+    #         break
     stop_task_queue()
