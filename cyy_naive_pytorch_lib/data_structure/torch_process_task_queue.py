33ad3b083a90c14d45a8a56a98058c134d1f0e44
cyy <cyyever@outlook.com>
2021-04-29 18:16:03 +0800

    fix code

--- cyy_naive_pytorch_lib/data_structure/torch_process_task_queue.py
+++ cyy_naive_pytorch_lib/data_structure/torch_process_task_queue.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
+import os
 from typing import Callable
 
 import torch.multiprocessing
 from cyy_naive_lib.data_structure.task_queue import TaskQueue
 
-from device import get_cuda_devices
+from device import get_devices
 
 
 class TorchProcessTaskQueue(TaskQueue):
-    def __init__(self, worker_fun: Callable):
-        self.cuda_devices = get_cuda_devices()
+    def __init__(self, worker_fun: Callable, worker_num=None):
+        self.devices = get_devices()
+        if worker_num is None:
+            worker_num = len(self.devices)
+            if worker_num == 1:
+                worker_num = os.cpu_count()
         super().__init__(
             worker_fun=worker_fun,
             ctx=torch.multiprocessing.get_context("spawn"),
-            worker_num=len(self.cuda_devices),
+            worker_num=worker_num,
         )
 
     def _get_extra_task_arguments(self, worker_id):
-        return [self.cuda_devices[worker_id % len(self.cuda_devices)]]
+        return [self.devices[worker_id % len(self.devices)]]
