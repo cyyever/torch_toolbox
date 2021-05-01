6ecd1aa89eb357dac43049b915f8778e03cf5229
cyy <cyyever@outlook.com>
2021-05-01 18:16:59 +0800

    fix code

--- cyy_naive_pytorch_lib/metrics/performance_metric.py
+++ cyy_naive_pytorch_lib/metrics/performance_metric.py
@@ -11,25 +11,22 @@ class PerformanceMetric(Metric):
         self.__loss_metric = LossMetric()
         self.__accuracy_metric = None
 
-    def _before_execute(self, **kwargs):
-        model_executor = kwargs.get("model_executor")
-        self.__loss_metric.prepend_to_model_executor_before_other_callback(
-            model_executor, self
-        )
+    def append_to_model_executor(self, model_executor):
+        self.__loss_metric.append_to_model_executor(model_executor)
         if model_executor.model_with_loss.model_type == ModelType.Classification:
             self.__accuracy_metric = AccuracyMetric()
-            self.__accuracy_metric.prepend_to_model_executor_before_other_callback(
-                model_executor, self
-            )
+            self.__accuracy_metric.append_to_model_executor(model_executor)
         else:
             self.__accuracy_metric = None
+        super().append_to_model_executor(model_executor)
 
     def _after_epoch(self, **kwargs):
         epoch = kwargs.get("epoch")
+        print("loss is", self.__loss_metric.get_loss(epoch))
         self._set_epoch_metric(epoch, "loss", self.__loss_metric.get_loss(epoch))
         self._set_epoch_metric(
             epoch, "accuracy", self.__accuracy_metric.get_accuracy(epoch)
         )
 
     def get_loss(self, epoch):
-        return self.__loss_metric(epoch)
+        return self.__loss_metric.get_loss(epoch)
