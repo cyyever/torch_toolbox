59a0d12d4ae1929af9f19c1d9612b27ae96dd3fe
cyy <cyyever@outlook.com>
2021-05-02 21:59:06 +0800

    fix code

--- cyy_naive_pytorch_lib/hooks/model_executor_logger.py
+++ cyy_naive_pytorch_lib/hooks/model_executor_logger.py
@@ -1,6 +1,5 @@
-from cyy_naive_lib.log import get_logger
-
 from callback import Callback
+from cyy_naive_lib.log import get_logger
 from model_util import ModelUtil
 
 
@@ -9,8 +8,10 @@ class ModelExecutorLogger(Callback):
         model_executor = kwargs["model_executor"]
         model_util = ModelUtil(model_executor.model)
         get_logger().info("dataset is %s", model_executor.dataset)
-        get_logger().info("model is %s", model_executor.model_with_loss.model)
-        get_logger().info("loss function is %s", model_executor.model_with_loss.loss_fun)
+        get_logger().info("model is %s", type(model_executor.model_with_loss.model))
+        get_logger().info(
+            "loss function is %s", model_executor.model_with_loss.loss_fun
+        )
         get_logger().info(
             "parameter number is %s",
             len(model_util.get_parameter_list()),
