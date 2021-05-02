33f0a996441c27d32da0a14c71f406f3b22d566c
cyy <cyyever@outlook.com>
2021-05-02 22:39:35 +0800

    fix code

--- cyy_naive_pytorch_lib/hooks/gradient_sanitizer.py
+++ cyy_naive_pytorch_lib/hooks/gradient_sanitizer.py
@@ -3,10 +3,14 @@ from callback import Callback
 
 class GradientSanitizer(Callback):
     def _before_batch(self, **kwargs):
+        batch_index = kwargs["batch_index"]
+        if batch_index % 100 != 0:
+            return
         # check parameters can be updated
         trainer = kwargs["model_executor"]
         optimizer = trainer.get_optimizer()
-        for group in optimizer.param_groups:
-            if group["params"] is trainer.model.parameters():
-                return
-        raise RuntimeError("can't find parameters in the optimizer")
+        for parameter in trainer.model.parameters():
+            for group in optimizer.param_groups:
+                if group["params"][0] is parameter:
+                    return
+            raise RuntimeError("can't find parameters in the optimizer")
