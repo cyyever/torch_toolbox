9539442089ad7cbad684cf3aaf11c6d9319688a9
cyy <cyyever@outlook.com>
2021-05-25 02:40:47 +0800

    add code

--- cyy_naive_pytorch_lib/algorithm/sample_gradient/sample_gradient_util.py
+++ cyy_naive_pytorch_lib/algorithm/sample_gradient/sample_gradient_util.py
@@ -6,9 +6,9 @@ from .sample_gradient_hook import SampleGradientHook
 
 def get_sample_gradient_dict(inferencer, computed_indices=None):
     tmp_inferencer = copy.deepcopy(inferencer)
-    callback = SampleGradientHook(storage_dir=tempfile.gettempdir())
+    hook = SampleGradientHook(storage_dir=tempfile.gettempdir())
     if computed_indices is not None:
-        callback.set_computed_indices(computed_indices)
-    callback.append_to_model_executor(tmp_inferencer)
+        hook.set_computed_indices(computed_indices)
+    hook.append_to_model_executor(tmp_inferencer)
     tmp_inferencer.inference(use_grad=True)
-    return callback.sample_gradient_dict
+    return hook.sample_gradient_dict
