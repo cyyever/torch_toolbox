44b161d5e5f11cbeabf0d8f274a3ef795479e05b
cyy <cyyever@outlook.com>
2021-05-27 00:22:59 +0800

    fix code

--- cyy_naive_pytorch_lib/hooks/model_executor_logger.py
+++ cyy_naive_pytorch_lib/hooks/model_executor_logger.py
@@ -1,3 +1,5 @@
+import os
+
 from cyy_naive_lib.log import get_logger
 from hook import Hook
 from model_util import ModelUtil
@@ -10,6 +12,8 @@ class ModelExecutorLogger(Hook):
     def _before_execute(self, **kwargs):
         model_executor = kwargs["model_executor"]
         model_util = ModelUtil(model_executor.model)
+        if os.getenv("draw_torch_model") is not None:
+            model_executor.model_with_loss.trace_input = True
         get_logger().info("dataset is %s", model_executor.dataset)
         get_logger().info(
             "model type is %s", model_executor.model_with_loss.model.__class__
@@ -32,3 +36,11 @@ class ModelExecutorLogger(Hook):
             get_logger().info(
                 "lr_scheduler is %s", getattr(model_executor, "get_lr_scheduler")()
             )
+
+    def _after_execute(self, **kwargs):
+        model_executor = kwargs["model_executor"]
+        if os.getenv("draw_torch_model") is not None:
+            model_executor.model_with_loss.trace_input = True
+        model_executor.visualizer.writer.add_graph(
+            model_executor.model, model_executor.model_with_loss.example_input
+        )
