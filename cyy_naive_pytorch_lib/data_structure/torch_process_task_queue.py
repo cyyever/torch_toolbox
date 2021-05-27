8c298debf9a28375838cfcfcfae1ac5d805ff012
cyy <cyyever@outlook.com>
2021-05-27 20:23:12 +0800

    add code

--- cyy_naive_pytorch_lib/data_structure/torch_process_task_queue.py
+++ cyy_naive_pytorch_lib/data_structure/torch_process_task_queue.py
@@ -4,22 +4,31 @@ from typing import Callable
 
 import torch.multiprocessing
 from cyy_naive_lib.data_structure.task_queue import TaskQueue
-from device import get_devices
+from device import get_cpu_device, get_devices
+from tensor import to_device
 
 
 class TorchProcessTaskQueue(TaskQueue):
-    def __init__(self, worker_fun: Callable = None, worker_num=None):
+    def __init__(
+        self, worker_fun: Callable = None, worker_num=None, use_cpu_tensor=False
+    ):
         self.devices = get_devices()
         if worker_num is None:
             if torch.cuda.is_available():
                 worker_num = len(self.devices)
             else:
                 worker_num = os.cpu_count()
+        self.__use_cpu_tensor = use_cpu_tensor
         super().__init__(
             worker_fun=worker_fun,
             ctx=torch.multiprocessing.get_context("spawn"),
             worker_num=worker_num,
         )
+
+    def put_result(self, result):
+        if self.__use_cpu_tensor:
+            result = to_device(result, get_cpu_device())
+        super().put_result(result)
 
     def __getstate__(self):
         state = super().__getstate__()
--- cyy_naive_pytorch_lib/test/data_structure/test_torch_process_task_queue.py
+++ cyy_naive_pytorch_lib/test/data_structure/test_torch_process_task_queue.py
@@ -1,4 +1,5 @@
 from shutil import which
+
 from data_structure.torch_process_task_queue import TorchProcessTaskQueue
 
 
@@ -10,6 +11,12 @@ def hello(task, args):
 def test_process_task_queue():
     if which("nvcc"):
         queue = TorchProcessTaskQueue(hello)
+        queue.start()
+        queue.add_task(())
+        devices = queue.get_result()
+        assert devices
+        queue.stop()
+        queue = TorchProcessTaskQueue(hello, use_cpu_tensor=True)
         queue.start()
         queue.add_task(())
         devices = queue.get_result()
