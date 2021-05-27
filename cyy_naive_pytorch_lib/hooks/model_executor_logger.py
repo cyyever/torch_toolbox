a9ca603c628e31d4fd81ece5dee56a0a726c1c30
cyy <cyyever@outlook.com>
2021-05-27 13:05:44 +0800

    fix code

--- cyy_naive_pytorch_lib/hooks/model_executor_logger.py
+++ cyy_naive_pytorch_lib/hooks/model_executor_logger.py
@@ -40,7 +40,6 @@ class ModelExecutorLogger(Hook):
     def _after_execute(self, **kwargs):
         model_executor = kwargs["model_executor"]
         if os.getenv("draw_torch_model") is not None:
-            model_executor.model_with_loss.trace_input = True
-        model_executor.visualizer.writer.add_graph(
-            model_executor.model, model_executor.model_with_loss.example_input
-        )
+            model_executor.visualizer.writer.add_graph(
+                model_executor.model, model_executor.model_with_loss.example_input
+            )
