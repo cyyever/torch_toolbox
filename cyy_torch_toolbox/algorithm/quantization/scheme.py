5b784f5b6ff53c7ecc761a1e9cbb92371a471b4d
cyy <cyyever@outlook.com>
2021-05-31 22:29:29 +0800

    fix code

--- cyy_torch_toolbox/algorithm/quantization/scheme.py
+++ cyy_torch_toolbox/algorithm/quantization/scheme.py
@@ -3,7 +3,6 @@ from typing import Callable, Tuple
 import cyy_naive_cpp_extension
 import torch
 from cyy_naive_lib.algorithm.mapping_op import get_mapping_values_by_key_order
-
 from device import put_data_to_device
 from tensor import cat_tensors_to_vector, split_tensor_to_dict
 
@@ -24,33 +23,34 @@ def stochastic_quantization(
         old_tensor_shape = tensor.shape
         old_device = tensor.device
         tensor = put_data_to_device(tensor)
-        tensor = tensor.reshape(-1)
-        assert len(tensor.shape) == 1
+        with torch.cuda.Stream(device=tensor.device):
+            tensor = tensor.reshape(-1)
+            assert len(tensor.shape) == 1
 
-        norm = None
-        if use_l2_norm:
-            norm = torch.linalg.norm(tensor)
-        else:
-            norm = torch.linalg.norm(tensor, ord=float("inf"))
-        assert norm > 0
-        sign_tensor = torch.sign(tensor)
-        normalized_abs_tensor = tensor.abs() / norm
-        slot_tensor = cyy_naive_cpp_extension.torch.stochastic_quantization(
-            normalized_abs_tensor, quantization_level
-        )
-        prob_tensor = normalized_abs_tensor * quantization_level - slot_tensor
-        random_vector = torch.distributions.Bernoulli(prob_tensor).sample()
-        slot_tensor += random_vector
+            norm = None
+            if use_l2_norm:
+                norm = torch.linalg.norm(tensor)
+            else:
+                norm = torch.linalg.norm(tensor, ord=float("inf"))
+            assert norm > 0
+            sign_tensor = torch.sign(tensor)
+            normalized_abs_tensor = tensor.abs() / norm
+            slot_tensor = cyy_naive_cpp_extension.torch.stochastic_quantization(
+                normalized_abs_tensor, quantization_level
+            )
+            prob_tensor = normalized_abs_tensor * quantization_level - slot_tensor
+            random_vector = torch.distributions.Bernoulli(prob_tensor).sample()
+            slot_tensor += random_vector
 
-        norm = put_data_to_device(norm, old_device)
-        sign_tensor = put_data_to_device(sign_tensor.char(), old_device).reshape(
-            old_tensor_shape
-        )
-        slot_tensor = put_data_to_device(slot_tensor.byte(), old_device).reshape(
-            old_tensor_shape
-        )
+            norm = put_data_to_device(norm, old_device)
+            sign_tensor = put_data_to_device(sign_tensor.char(), old_device).reshape(
+                old_tensor_shape
+            )
+            slot_tensor = put_data_to_device(slot_tensor.byte(), old_device).reshape(
+                old_tensor_shape
+            )
 
-        return (norm, sign_tensor, slot_tensor, quantization_level, name_and_shapes)
+            return (norm, sign_tensor, slot_tensor, quantization_level, name_and_shapes)
 
     def dequant(quantized_pair):
         (
