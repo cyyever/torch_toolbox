aff72bd9dbbb25543dd59b09b3d60d511e37650d
cyy <cyyever@outlook.com>
2021-05-31 23:14:55 +0800

    fix code

--- cyy_torch_toolbox/test/algorithm/test_conjugate_gradient.py
+++ cyy_torch_toolbox/test/algorithm/test_conjugate_gradient.py
@@ -1,10 +1,12 @@
 import torch
-from algorithm.conjugate_gradient import conjugate_gradient
 from device import get_device
+
+from algorithm.conjugate_gradient import conjugate_gradient
 
 
 def test_conjugate_gradient():
-    a = torch.tensor([[1.0, 2.0], [2.0, 1.0]]).to(get_device())
-    b = torch.tensor([3.0, 4.0]).to(get_device())
+    device = get_device()
+    a = torch.tensor([[1.0, 2.0], [2.0, 1.0]]).to(device)
+    b = torch.tensor([3.0, 4.0]).to(device)
     p = conjugate_gradient(a, b)
     assert torch.all((a @ p).eq(b))
