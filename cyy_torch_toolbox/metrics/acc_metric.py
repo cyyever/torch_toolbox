bee5323d2996b5c44932225b47c1c5df17376d2f
cyy <cyyever@outlook.com>
2021-06-25 22:33:18 +0800

    improve performance

--- cyy_torch_toolbox/metrics/acc_metric.py
+++ cyy_torch_toolbox/metrics/acc_metric.py
@@ -10,20 +10,18 @@ class AccuracyMetric(Metric):
         self.__classification_count_per_label: dict = dict()
         self.__classification_correct_count_per_label: dict = dict()
         self.__labels = None
+        self.__correct_count = None
+        self.__dataset_size = None
 
     def get_accuracy(self, epoch):
         return self.get_epoch_metric(epoch, "accuracy")
-
-    def get_class_accuracy(self, epoch):
-        return self.get_epoch_metric(epoch, "class_accuracy")
 
     def _before_epoch(self, **kwargs):
         if not self.__labels:
             model_executor = kwargs["model_executor"]
             self.__labels = model_executor.dataset_collection.get_labels()
-        for label in self.__labels:
-            self.__classification_correct_count_per_label[label] = 0
-            self.__classification_count_per_label[label] = 0
+        self.__dataset_size = 0
+        self.__correct_count = 0
 
     def _after_batch(self, **kwargs):
         batch = kwargs["batch"]
@@ -33,34 +31,14 @@ class AccuracyMetric(Metric):
         assert isinstance(targets, torch.Tensor)
         correct = torch.eq(torch.max(output, dim=1)[1].cpu(), targets.cpu()).view(-1)
         assert correct.shape[0] == targets.shape[0]
+        self.__correct_count += torch.sum(correct)
+        self.__dataset_size += targets.shape[0]
 
-        for label in self.__classification_correct_count_per_label:
-            self.__classification_count_per_label[label] += torch.sum(
-                targets == label
-            ).item()
-            self.__classification_correct_count_per_label[label] += torch.sum(
-                correct[targets == label]
-            ).item()
-            assert (
-                self.__classification_correct_count_per_label[label]
-                <= self.__classification_count_per_label[label]
-            )
+        assert self.__correct_count <= self.__dataset_size
 
     def _after_epoch(self, **kwargs):
         epoch = kwargs["epoch"]
         accuracy = sum(self.__classification_correct_count_per_label.values()) / sum(
             self.__classification_count_per_label.values()
         )
-
-        class_accuracy = dict()
-        for label in self.__classification_correct_count_per_label:
-            # non iid case
-            if self.__classification_count_per_label[label] == 0:
-                continue
-            class_accuracy[label] = (
-                self.__classification_correct_count_per_label[label]
-                / self.__classification_count_per_label[label]
-            )
-
         self._set_epoch_metric(epoch, "accuracy", accuracy)
-        self._set_epoch_metric(epoch, "class_accuracy", class_accuracy)
---
+++ cyy_torch_toolbox/metrics/class_acc_metric.py
@@ -0,0 +1,59 @@
+import torch
+from dataset import decode_batch
+
+from .metric import Metric
+
+
+class ClassAccuracyMetric(Metric):
+    def __init__(self):
+        super().__init__()
+        self.__classification_count_per_label: dict = dict()
+        self.__classification_correct_count_per_label: dict = dict()
+        self.__labels = None
+
+    def get_class_accuracy(self, epoch):
+        return self.get_epoch_metric(epoch, "class_accuracy")
+
+    def _before_epoch(self, **kwargs):
+        if not self.__labels:
+            model_executor = kwargs["model_executor"]
+            self.__labels = model_executor.dataset_collection.get_labels()
+        for label in self.__labels:
+            self.__classification_correct_count_per_label[label] = 0
+            self.__classification_count_per_label[label] = 0
+
+    def _after_batch(self, **kwargs):
+        batch = kwargs["batch"]
+        targets = decode_batch(batch)[1]
+        result = kwargs["result"]
+        output = result["output"]
+        assert isinstance(targets, torch.Tensor)
+        correct = torch.eq(torch.max(output, dim=1)[1].cpu(), targets.cpu()).view(-1)
+        assert correct.shape[0] == targets.shape[0]
+
+        for label in self.__classification_correct_count_per_label:
+            self.__classification_count_per_label[label] += torch.sum(
+                targets == label
+            ).item()
+            self.__classification_correct_count_per_label[label] += torch.sum(
+                correct[targets == label]
+            ).item()
+            assert (
+                self.__classification_correct_count_per_label[label]
+                <= self.__classification_count_per_label[label]
+            )
+
+    def _after_epoch(self, **kwargs):
+        epoch = kwargs["epoch"]
+
+        class_accuracy = dict()
+        for label in self.__classification_correct_count_per_label:
+            # non iid case
+            if self.__classification_count_per_label[label] == 0:
+                continue
+            class_accuracy[label] = (
+                self.__classification_correct_count_per_label[label]
+                / self.__classification_count_per_label[label]
+            )
+
+        self._set_epoch_metric(epoch, "class_accuracy", class_accuracy)
