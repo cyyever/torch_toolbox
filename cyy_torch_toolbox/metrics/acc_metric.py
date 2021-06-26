457322523e347442def1c72c71bab2fe96713a41
cyy <cyyever@outlook.com>
2021-06-26 17:02:38 +0800

    fix code

--- cyy_torch_toolbox/metrics/acc_metric.py
+++ cyy_torch_toolbox/metrics/acc_metric.py
@@ -8,6 +8,7 @@ class AccuracyMetric(Metric):
         super().__init__()
         self.__correct_count = None
         self.__dataset_size = None
+        self.__pending_count = None
 
     def get_accuracy(self, epoch):
         return self.get_epoch_metric(epoch, "accuracy").cpu()
@@ -15,18 +16,24 @@ class AccuracyMetric(Metric):
     def _before_epoch(self, **kwargs):
         self.__dataset_size = 0
         self.__correct_count = 0
+        self.__pending_count = None
 
     def _after_batch(self, **kwargs):
         batch = kwargs["batch"]
-        targets = batch[1]
-        result = kwargs["result"]
-        output = result["output"]
-        assert isinstance(targets, torch.Tensor)
-        correct = torch.eq(torch.max(output, dim=1)[1], targets).view(-1)
-        self.__correct_count += torch.sum(correct)
+        targets = batch[1].detach()
+        output = kwargs["result"]["output"].detach()
+        correct_count = torch.eq(torch.max(output, dim=1)[1], targets).view(-1).sum()
+        self.__process_pending_count()
+        self.__pending_count = correct_count
         self.__dataset_size += targets.shape[0]
+
+    def __process_pending_count(self):
+        if self.__pending_count is not None:
+            self.__correct_count += self.__pending_count.cpu()
+            self.__pending_count = None
 
     def _after_epoch(self, **kwargs):
         epoch = kwargs["epoch"]
+        self.__process_pending_count()
         accuracy = self.__correct_count / self.__dataset_size
         self._set_epoch_metric(epoch, "accuracy", accuracy)
