4b172b943ed946e65ab13647b3446300deb165b4
cyy <cyyever@outlook.com>
2021-08-27 18:53:42 +0800

    fix code

--- cyy_torch_toolbox/test/test_dataset.py
+++ cyy_torch_toolbox/test/test_dataset.py
@@ -33,4 +33,4 @@ def test_dataset():
 def test_dataset_labels():
     for name in ("MNIST", "CIFAR10"):
         dc = DatasetCollection.get_by_name(name)
-        assert dc.get_labels()
+        assert len(dc.get_labels()) == 10
