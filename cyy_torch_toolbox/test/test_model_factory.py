79c6393e9c91157255414acb0a5d5c28117f159f
cyy <cyyever@outlook.com>
2021-12-07 19:55:25 +0800

    fix torchtext

--- cyy_torch_toolbox/test/test_model_factory.py
+++ cyy_torch_toolbox/test/test_model_factory.py
@@ -6,9 +6,9 @@ from model_factory import get_model
 def test_model():
     # for dataset_name, model_name in [("IMDB", "aaa"), ("MNIST", "LeNet5")]:
     for dataset_name, model_name in [
-        # ("MNIST", "LeNet5"),
-        # ("MNIST", "efficientnet_b0"),
-        # ("IMDB", "bert-base-uncased"),
+        ("MNIST", "LeNet5"),
+        ("MNIST", "efficientnet_b0"),
+        ("IMDB", "simplelstm"),
     ]:
         dc = DatasetCollection.get_by_name(dataset_name)
         get_model(model_name, dc, pretrained=False)
