28471e1c755692db76d6dbfb64c98a55ce0dd381
cyy <cyyever@outlook.com>
2021-12-18 18:58:58 +0800

    fix code

--- cyy_torch_toolbox/data_structure/torch_process_task_queue.py
+++ cyy_torch_toolbox/data_structure/torch_process_task_queue.py
@@ -3,12 +3,18 @@ import os
 from typing import Callable
 
 import torch.multiprocessing
-from cyy_naive_lib.data_structure.task_queue import RepeatedResult, TaskQueue
+from cyy_naive_lib.data_structure.task_queue import TaskQueue
 from device import get_cpu_device, get_devices, put_data_to_device
 
 
 class TorchProcessTaskQueue(TaskQueue):
-    def __init__(self, worker_fun: Callable = None, worker_num=None, use_manager=False):
+    def __init__(
+        self,
+        worker_fun: Callable = None,
+        worker_num: int | None = None,
+        use_manager: bool = False,
+        move_data_in_cpu: bool = False,
+    ):
         self.__devices = get_devices()
         self.__use_manager = use_manager
         if worker_num is None:
@@ -23,19 +29,12 @@ class TorchProcessTaskQueue(TaskQueue):
         super().__init__(
             worker_fun=worker_fun, ctx=ctx, worker_num=worker_num, manager=manager
         )
+        self.__move_data_in_cpu = move_data_in_cpu
 
     def add_task(self, task):
-        if self.__use_manager:
+        if self.__move_data_in_cpu:
             task = put_data_to_device(task, get_cpu_device())
         super().add_task(task)
-
-    def put_result(self, result, queue_name: str = "default"):
-        if self.__use_manager:
-            if isinstance(result, RepeatedResult):
-                result.set_data(put_data_to_device(result.get_data(), get_cpu_device()))
-            else:
-                result = put_data_to_device(result, get_cpu_device())
-        super().put_result(result, queue_name=queue_name)
 
     def _get_extra_task_arguments(self, worker_id):
         return [self.__devices[worker_id % len(self.__devices)]]
