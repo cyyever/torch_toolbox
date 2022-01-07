0627af9c3220652d9276980aafcea68967cb52fb
cyy <cyyever@outlook.com>
2022-01-07 14:37:40 +0800

    fix error

--- cyy_torch_toolbox/test/test_model_util.py
+++ cyy_torch_toolbox/test/test_model_util.py
@@ -1,3 +1,4 @@
+import torch.nn as nn
 from dataset_collection import DatasetCollection
 from model_factory import get_model
 from model_util import ModelUtil
@@ -6,6 +7,9 @@ from model_util import ModelUtil
 def test_get_submodules():
     mnist = DatasetCollection.get_by_name("MNIST")
     model_util = ModelUtil(get_model("LeNet5", mnist).model)
-    model_util.get_sub_modules()
     model_util.get_sub_module_blocks()
     model_util.remove_statistical_variables()
+    num1 = len(model_util.get_sub_modules())
+    assert model_util.remove_sub_modules(module_classes={nn.Linear})
+    num2 = len(model_util.get_sub_modules())
+    assert num1 != num2
