b2493afe6f7e639738de3b2b4bc06ca34242399c
cyy <cyyever@outlook.com>
2022-01-21 17:09:28 +0800

    add code

--- cyy_torch_toolbox/test/test_training.py
+++ cyy_torch_toolbox/test/test_training.py
@@ -4,6 +4,6 @@ from default_config import DefaultConfig
 
 def test_training():
     config = DefaultConfig(dataset_name="MNIST", model_name="LeNet5")
-    config.hyper_parameter_config.set_epoch(1)
+    config.hyper_parameter_config.epoch = 1
     trainer = config.create_trainer()
     trainer.train()
