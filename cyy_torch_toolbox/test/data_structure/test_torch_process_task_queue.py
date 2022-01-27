a1ea3e39ce58dbbec42018f096fe9b04909a41cd
cyy <cyyever@outlook.com>
2022-01-27 18:00:27 +0800

    fix test

--- cyy_torch_toolbox/test/data_structure/test_torch_process_task_queue.py
+++ cyy_torch_toolbox/test/data_structure/test_torch_process_task_queue.py
@@ -4,21 +4,16 @@ from data_structure.torch_process_task_queue import TorchProcessTaskQueue
 
 
 def hello(task, args):
+    assert not task
     assert args
-    return args
+    return 1
 
 
 def test_process_task_queue():
     if which("nvcc"):
-        queue = TorchProcessTaskQueue(hello)
-        queue.start()
-        queue.add_task(())
-        devices = queue.get_result()
-        assert devices
-        queue.stop()
         queue = TorchProcessTaskQueue(hello, use_manager=True)
         queue.start()
         queue.add_task(())
-        devices = queue.get_result()
-        assert devices
+        res = queue.get_result()
+        assert res == 1
         queue.stop()
