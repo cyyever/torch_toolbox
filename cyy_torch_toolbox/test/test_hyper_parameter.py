a4a879fa553a9252abf31332ac74e8c55edf5493
cyy <cyyever@outlook.com>
2022-01-27 11:30:46 +0800

    add test

--- cyy_torch_toolbox/test/test_hyper_parameter.py
+++ cyy_torch_toolbox/test/test_hyper_parameter.py
@@ -6,6 +6,9 @@ import hyper_parameter
 def test_hyper_parameter():
     res = hyper_parameter.get_recommended_hyper_parameter("MNIST", "")
     assert res is not None
-    names = hyper_parameter.get_optimizer_names()
+    names = hyper_parameter.HyperParameter.get_optimizer_names()
+    assert names
+    print(names)
+    names = hyper_parameter.HyperParameter.get_lr_scheduler_names()
     assert names
     print(names)
