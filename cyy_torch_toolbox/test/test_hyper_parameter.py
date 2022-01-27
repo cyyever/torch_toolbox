07e64062aa28a658aa3bd6fa33e05a2ad673f3f6
cyy <cyyever@outlook.com>
2022-01-27 11:27:10 +0800

    add code

--- cyy_torch_toolbox/test/test_hyper_parameter.py
+++ cyy_torch_toolbox/test/test_hyper_parameter.py
@@ -6,3 +6,6 @@ import hyper_parameter
 def test_hyper_parameter():
     res = hyper_parameter.get_recommended_hyper_parameter("MNIST", "")
     assert res is not None
+    names = hyper_parameter.get_optimizer_names()
+    assert names
+    print(names)
