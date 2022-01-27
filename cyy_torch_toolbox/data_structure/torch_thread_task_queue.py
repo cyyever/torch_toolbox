c351c1b18a1001c464a5c40950c21efb91f18e53
cyy <cyyever@outlook.com>
2022-01-27 22:13:17 +0800

    fix code

---
+++ cyy_torch_toolbox/data_structure/torch_thread_task_queue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from cyy_naive_lib.data_structure.thread_task_queue import ThreadTaskQueue
+from device import get_devices
+
+
+class TorchProcessTaskQueue(ThreadTaskQueue):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__devices = get_devices()
+
+    def _get_extra_task_arguments(self, worker_id):
+        return super()._get_extra_task_arguments(worker_id) | {
+            "device": self.__devices[worker_id % len(self.__devices)]
+        }
