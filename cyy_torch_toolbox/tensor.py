9748af13280ec5a4dd7e5955a42b90332eb55a9d
cyy <cyyever@outlook.com>
2022-02-15 01:00:14 +0800

    fix code

--- cyy_torch_toolbox/tensor.py
+++ cyy_torch_toolbox/tensor.py
@@ -20,12 +20,23 @@ def load_tensor_dict(data: dict, tensor: torch.Tensor):
     return data
 
 
-def split_tensor_to_dict(name_and_shapes: list, tensor: torch.Tensor):
-    data = dict()
+def split_tensor_to_dict(name_and_shapes: list, tensor: torch.Tensor) -> dict:
+    data = {}
     bias = 0
     for (name, shape) in name_and_shapes:
         param_element_num = np.prod(shape)
         data[name] = tensor.narrow(0, bias, param_element_num).view(*shape)
+        bias += param_element_num
+    assert bias == tensor.shape[0]
+    return data
+
+
+def split_tensor_to_list(shapes: list, tensor: torch.Tensor) -> torch.Tensor:
+    data = []
+    bias = 0
+    for shape in shapes:
+        param_element_num = np.prod(shape)
+        data.append(tensor.narrow(0, bias, param_element_num).view(*shape))
         bias += param_element_num
     assert bias == tensor.shape[0]
     return data
