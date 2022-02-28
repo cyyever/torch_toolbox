2d2d04578437157d1dcbb9f5c66efddd219d7890
cyy <cyyever@outlook.com>
2022-02-28 15:05:30 +0800

    add code

--- cyy_torch_toolbox/hooks/profiler.py
+++ cyy_torch_toolbox/hooks/profiler.py
@@ -1,6 +1,7 @@
 from hook import Hook
 
 from .cuda_memory_profiler import CUDAMemoryProfiler
+from .cuda_stream_profiler import CUDAStreamProfiler
 from .dataloader_profiler import DataloaderProfiler
 
 
@@ -8,4 +9,5 @@ class Profiler(Hook):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.__cuda_memory_profiler = CUDAMemoryProfiler()
+        self.__cuda_stream_profiler = CUDAStreamProfiler()
         self.__dataloader_profiler = DataloaderProfiler()
