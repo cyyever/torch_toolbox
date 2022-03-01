e023f961727a15e237ec247c57596f2ddc5a4e35
cyy <cyyever@outlook.com>
2022-03-01 19:15:33 +0800

    add code

--- cyy_torch_toolbox/test/test_model_factory.py
+++ cyy_torch_toolbox/test/test_model_factory.py
@@ -8,7 +8,7 @@ def test_model():
     for dataset_name, model_name in [
         ("MNIST", "LeNet5"),
         ("MNIST", "efficientnet_b0"),
-        ("IMDB", "simplelstm"),
+        # ("IMDB", "simplelstm"),
     ]:
         dc = DatasetCollection.get_by_name(dataset_name)
         get_model(model_name, dc, pretrained=False)
