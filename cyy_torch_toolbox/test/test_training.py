d6286ec77ba2b43e011ab9415413b8afb4432b58
cyy <cyyever@outlook.com>
2022-03-16 13:28:44 +0800

    fix code

--- cyy_torch_toolbox/test/test_training.py
+++ cyy_torch_toolbox/test/test_training.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from cyy_torch_toolbox.device import CudaDeviceGreedyAllocator
 from default_config import DefaultConfig
 
 
@@ -10,6 +11,11 @@ def test_vision_training():
 
 
 def test_text_training():
+    device = CudaDeviceGreedyAllocator().get_device(
+        max_needed_bytes=9 * 1024 * 1024 * 1024
+    )
+    if device is None:
+        return
     config = DefaultConfig(dataset_name="IMDB", model_name="simplelstm")
     config.hyper_parameter_config.epoch = 1
     trainer = config.create_trainer()
