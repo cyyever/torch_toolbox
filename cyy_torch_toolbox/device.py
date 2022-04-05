894acabbf30d563d6504823fc091c886a6d9d9ee
cyy <cyyever@outlook.com>
2022-04-05 10:56:18 +0800

    add code

--- cyy_torch_toolbox/device.py
+++ cyy_torch_toolbox/device.py
@@ -89,9 +89,10 @@ class CudaDeviceGreedyAllocator:
         if not devices:
             return None
         cuda_device = torch.cuda.current_device()
+        get_logger().error("current_device %s devices %s", cuda_device, devices)
         if cuda_device >= 0:
-            if cuda_device in devices:
-                return cuda_device
+            if cuda_device in {d.index for d in devices}:
+                return torch.device(f"cuda:{cuda_device}")
         return devices[0]
 
     def __sort_devices(self) -> list:
