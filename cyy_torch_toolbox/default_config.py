7f94ed98e4ec01e0dd84196633ad5ab9f3629c26
cyy <cyyever@outlook.com>
2022-04-11 16:15:55 +0800

    add code

--- cyy_torch_toolbox/default_config.py
+++ cyy_torch_toolbox/default_config.py
@@ -1,6 +1,5 @@
 import argparse
 import datetime
-import json
 import os
 import uuid
 
@@ -10,7 +9,7 @@ from cyy_torch_toolbox.dataset_collection import DatasetCollectionConfig
 from cyy_torch_toolbox.hyper_parameter import HyperParameterConfig
 from cyy_torch_toolbox.inferencer import Inferencer
 from cyy_torch_toolbox.ml_type import MachineLearningPhase
-from cyy_torch_toolbox.model_factory import get_model
+from cyy_torch_toolbox.model_factory import ModelConfig
 from cyy_torch_toolbox.reproducible_env import global_reproducible_env
 from cyy_torch_toolbox.trainer import Trainer
 
@@ -21,40 +20,31 @@ class DefaultConfig:
         self.reproducible_env_load_path = None
         self.dc_config: DatasetCollectionConfig = DatasetCollectionConfig(dataset_name)
         self.hyper_parameter_config: HyperParameterConfig = HyperParameterConfig()
-        self.model_name = model_name
-        self.model_path = None
-        self.pretrained = False
+        self.model_config = ModelConfig(model_name=model_name)
         self.debug = False
         self.profile = False
-        self.use_checkpointing = False
         self.save_dir = None
-        self.model_kwargs = None
-        self.model_kwarg_json_path = None
         self.log_level = None
 
     def load_args(self, parser=None):
         if parser is None:
             parser = argparse.ArgumentParser()
 
-        if self.model_name is None:
-            parser.add_argument("--model_name", type=str, required=True)
-        parser.add_argument("--model_path", type=str, default=None)
-        parser.add_argument("--pretrained", action="store_true", default=False)
         parser.add_argument("--save_dir", type=str, default=None)
         parser.add_argument("--reproducible_env_load_path", type=str, default=None)
         parser.add_argument(
             "--make_reproducible_env", action="store_true", default=False
         )
-        parser.add_argument("--model_kwarg_json_path", type=str, default=None)
         self.dc_config.add_args(parser)
         self.hyper_parameter_config.add_args(parser)
+        self.model_config.add_args(parser)
         parser.add_argument("--log_level", type=str, default=None)
         parser.add_argument("--debug", action="store_true", default=False)
         parser.add_argument("--profile", action="store_true", default=False)
-        parser.add_argument("--use_checkpointing", action="store_true", default=False)
         args = parser.parse_args()
         self.dc_config.load_args(args)
         self.hyper_parameter_config.load_args(args)
+        self.model_config.load_args(args)
 
         for attr in dir(args):
             if attr.startswith("_"):
@@ -69,7 +59,7 @@ class DefaultConfig:
             self.save_dir = os.path.join(
                 "session",
                 self.dc_config.dataset_name,
-                self.model_name,
+                self.model_config.model_name,
                 "{date:%Y-%m-%d_%H_%M_%S}".format(date=datetime.datetime.now()),
                 str(uuid.uuid4()),
             )
@@ -82,25 +72,13 @@ class DefaultConfig:
 
     def create_trainer(self) -> Trainer:
         hyper_parameter = self.hyper_parameter_config.create_hyper_parameter(
-            self.dc_config.dataset_name, self.model_name
+            self.dc_config.dataset_name, self.model_config.model_name
         )
 
         dc = self.create_dataset_collection()
-        get_logger().info("use model %s", self.model_name)
-        model_kwargs = {}
-        if self.model_kwarg_json_path is not None:
-            with open(self.model_kwarg_json_path, "rt", encoding="utf8") as f:
-                model_kwargs = json.load(f)
-        if self.model_kwargs is not None:
-            model_kwargs |= self.model_kwargs
-        if self.pretrained:
-            if "pretrained" in model_kwargs:
-                raise RuntimeError("specify pretrained twice")
-            model_kwargs["pretrained"] = self.pretrained
 
-        model_with_loss = get_model(self.model_name, dc, **model_kwargs)
+        model_with_loss = self.model_config.get_model(dc)
         dc.adapt_to_model(model_with_loss.get_real_model())
-        model_with_loss.use_checkpointing = self.use_checkpointing
         trainer = Trainer(model_with_loss, dc, hyper_parameter)
         trainer.set_save_dir(self.get_save_dir())
         if self.debug:
@@ -109,9 +87,6 @@ class DefaultConfig:
         if self.profile:
             get_logger().warning("profile the trainer")
             trainer.profiling_mode = True
-        if self.model_path is not None:
-            trainer.load_model(self.model_path)
-
         return trainer
 
     def create_inferencer(self, phase=MachineLearningPhase.Test) -> Inferencer:
