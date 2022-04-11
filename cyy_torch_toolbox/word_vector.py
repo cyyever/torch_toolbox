13d4830e795477c0d2705dca10f0ed28819114d8
cyy <cyyever@outlook.com>
2022-04-11 15:22:29 +0800

    fix code

--- cyy_torch_toolbox/word_vector.py
+++ cyy_torch_toolbox/word_vector.py
@@ -2,7 +2,11 @@ import codecs
 import os
 
 import torch
+import torch.nn as nn
 from cyy_naive_lib.source_code.tarball_source import TarballSource
+from torchtext.vocab import Vocab
+
+from cyy_torch_toolbox.model_util import ModelUtil
 
 
 class PretrainedWordVector:
@@ -16,6 +20,25 @@ class PretrainedWordVector:
     @property
     def word_vector_dict(self):
         return self.__word_vector_dict
+
+    def load_to_model(self, model_util: ModelUtil, vocab: Vocab) -> None:
+        itos = vocab.get_itos()
+
+        def __load_embedding(_, layer):
+            embeddings = layer.weight.tolist()
+            for idx, token in enumerate(itos):
+                word_vector = self.__word_vector_dict.get(token, None)
+                if word_vector is None:
+                    word_vector = self.__word_vector_dict.get(token.lower(), None)
+                if word_vector is not None:
+                    embeddings[idx] = word_vector
+            assert list(layer.weight.shape) == [
+                len(itos),
+                len(next(iter(self.__word_vector_dict.values()))),
+            ], "Shape of weight does not match num_embeddings and embedding_dim"
+            layer.weight = nn.Parameter(embeddings)
+
+        model_util.change_sub_modules(nn.Embedding, __load_embedding)
 
     @classmethod
     def get_root_dir(cls) -> str:
