54d4e4f6e986a89935859297ce9d66c0c334d93c
cyy <cyyever@outlook.com>
2022-04-12 10:34:25 +0800

    fix code

--- cyy_torch_toolbox/word_vector.py
+++ cyy_torch_toolbox/word_vector.py
@@ -44,7 +44,7 @@ class PretrainedWordVector:
             layer.weight = nn.Parameter(torch.Tensor(embeddings))
             if unknown_token_cnt != 0:
                 get_logger().info(
-                    "there are %s tokens in word vectors for a total of %s",
+                    "there are %s unrecognized tokens in word vectors for a total of %s",
                     unknown_token_cnt,
                     len(itos),
                 )
@@ -59,20 +59,30 @@ class PretrainedWordVector:
     @classmethod
     def __download(cls, name: str) -> dict:
         word_vector_dict: dict = {}
-        tarball = None
-        if name == "glove.6B.300d":
-            tarball = TarballSource(
-                spec=name,
-                url="http://downloads.cs.stanford.edu/nlp/data/glove.6B.zip",
-                root_dir=cls.get_root_dir(),
-                checksum="sha256:617afb2fe6cbd085c235baf7a465b96f4112bd7f7ccb2b2cbd649fed9cbcf2fb",
-            )
-        if tarball is None:
+        urls = {
+            "glove.6B.300d": (
+                "http://downloads.cs.stanford.edu/nlp/data/glove.6B.zip",
+                "sha256:617afb2fe6cbd085c235baf7a465b96f4112bd7f7ccb2b2cbd649fed9cbcf2fb",
+            ),
+            "glove.840B.300d": (
+                "https://nlp.stanford.edu/data/glove.840B.300d.zip",
+                "sha256:c06db255e65095393609f19a4cfca20bf3a71e20cc53e892aafa490347e3849f",
+            ),
+        }
+        urls["glove.6B.100d"] = urls["glove.6B.300d"]
+        url, checksum = urls.get(name, (None, None))
+        if url is None:
             raise RuntimeError(f"unknown word vector {name}")
+        tarball = TarballSource(
+            spec=name, url=url, root_dir=cls.get_root_dir(), checksum=checksum
+        )
         with tarball:
-            if name == "glove.6B.300d":
-                with codecs.open("glove.6B.300d.txt", "r", encoding="utf-8") as f:
+            if name.startswith("glove"):
+                dim = int(name.split()[-1].replace("d", ""))
+                with codecs.open(f"{name}.txt", "r", encoding="utf-8") as f:
                     for line in f:
                         s = line.strip().split()
-                        word_vector_dict[s[0]] = torch.Tensor([float(i) for i in s[1:]])
+                        word_vector_dict[" ".join(s[:-dim])] = torch.Tensor(
+                            [float(i) for i in s[-dim:]]
+                        )
         return word_vector_dict
