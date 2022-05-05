1b19516d83087e482679c1616f18f595ac60ee39
cyy <cyyever@outlook.com>
2022-05-05 11:09:15 +0800

    fix code

--- cyy_torch_toolbox/dataset_collection.py
+++ cyy_torch_toolbox/dataset_collection.py
@@ -356,7 +356,9 @@ class DatasetCollection:
     @staticmethod
     def __split_for_validation(cache_dir, splitted_dataset):
         pickle_file = os.path.join(cache_dir, "split_index_lists.pk")
-        dataset_util = DatasetSplitter(splitted_dataset)
+        dataset_util = DatasetSplitter(
+            dataset=splitted_dataset, transforms=Transforms()
+        )
         split_index_lists = DatasetCollection.__read_data(pickle_file)
         if split_index_lists is not None:
             return dataset_util.split_by_indices(split_index_lists)
@@ -403,54 +405,6 @@ class ClassificationDatasetCollection(DatasetCollection):
         dc: ClassificationDatasetCollection = cls(*DatasetCollection.create(**kwargs))
         dc.__tokenizer_kwargs = tokenizer_kwargs
         add_transforms(dc, dataset_kwargs)
-        # if dc.dataset_type == DatasetType.Vision:
-        #     dc.append_transform(torchvision.transforms.ToTensor())
-        #     mean, std = dc.get_mean_and_std(
-        #         torch.utils.data.ConcatDataset(list(dc._datasets.values()))
-        #     )
-        #     dc.append_transform(torchvision.transforms.Normalize(mean=mean, std=std))
-        #     if dc.name not in ("SVHN", "MNIST"):
-        #         dc.append_transform(
-        #             torchvision.transforms.RandomHorizontalFlip(),
-        #             phases={MachineLearningPhase.Training},
-        #         )
-        #     # if name in ("CIFAR10", "CIFAR100"):
-        #     #     dc.append_transform(
-        #     #         # transforms.RandomCrop(32, padding=4),
-        #     #         phases={MachineLearningPhase.Training},
-        #     #     )
-        #     if dc.name.lower() == "imagenet":
-        #         dc.append_transform(torchvision.transforms.RandomResizedCrop(224))
-        # if dc.dataset_type == DatasetType.Text:
-        #     if dc.name == "IMDB":
-        #         dc.append_transform(
-        #             swap_input_and_target, key=TransformType.ExtractData
-        #         )
-
-        #         dc.append_transform(
-        #             lambda text: text.replace("<br />", ""), key=TransformType.InputText
-        #         )
-        #     text_transforms = dataset_kwargs.get("text_transforms", {})
-        #     for phase, transforms in text_transforms.items():
-        #         for f in transforms:
-        #             get_logger().info("add text_transform %s for phase %s", f, phase)
-        #             dc.append_transform(f, key=TransformType.InputText, phases=[phase])
-        #     dc.append_transform(dc.tokenizer)
-        #     dc.append_transform(torch.LongTensor)
-        #     dc.append_transform(
-        #         functools.partial(
-        #             pad_sequence, padding_value=dc.tokenizer.vocab["<pad>"]
-        #         ),
-        #         key=TransformType.InputBatch,
-        #     )
-        #     if isinstance(dc.get_dataset_util().get_sample_label(0), str):
-        #         label_names = dc.get_label_names()
-        #         dc.append_transform(
-        #             functools.partial(cls.get_label, label_names=label_names),
-        #             key=TransformType.Target,
-        #         )
-        #         get_logger().debug("covert string label to int")
-
         return dc
 
     def get_mean_and_std(self, dataset):
