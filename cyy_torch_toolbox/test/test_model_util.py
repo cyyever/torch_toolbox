8201243d4b25e993d43498ef4ae8d6bbf8c3d2d4
cyy <cyyever@outlook.com>
2022-05-14 12:06:03 +0800

    fix code

--- cyy_torch_toolbox/test/test_model_util.py
+++ cyy_torch_toolbox/test/test_model_util.py
@@ -4,11 +4,22 @@ from model_factory import get_model, get_model_info
 from model_util import ModelUtil
 
 
-def test_get_submodules():
-    mnist = create_dataset_collection(ClassificationDatasetCollection, "MNIST")
-    model_util = ModelUtil(get_model("LeNet5", mnist).model)
-    model_util.get_sub_module_blocks()
-    model_util.remove_statistical_variables()
+def test_model_info():
     models = get_model_info()
     assert models
     print(models.keys())
+
+
+def test_get_submodules():
+    mnist = create_dataset_collection(ClassificationDatasetCollection, "MNIST")
+    model_util = ModelUtil(get_model("LeNet5", mnist).model)
+    result = model_util.get_sub_modules()
+    assert result
+    print(result)
+
+
+def test_get_blocks():
+    mnist = create_dataset_collection(ClassificationDatasetCollection, "MNIST")
+    model_util = ModelUtil(get_model("LeNet5", mnist).model)
+    result = model_util.get_sub_module_blocks()
+    assert result
