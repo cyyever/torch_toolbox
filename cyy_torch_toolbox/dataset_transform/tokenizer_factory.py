50af8ab6e28b479a9feea36382670c50a0da7990
cyy <cyyever@outlook.com>
2022-05-15 15:19:52 +0800

    fix code

--- cyy_torch_toolbox/dataset_transform/tokenizer_factory.py
+++ cyy_torch_toolbox/dataset_transform/tokenizer_factory.py
@@ -4,7 +4,7 @@ from .tokenizer import SpacyTokenizer
 
 
 def get_tokenizer(tokenizer_kwargs, dc=None):
-    tokenizer_type = tokenizer_kwargs.pop("type", "spacy")
+    tokenizer_type = tokenizer_kwargs.get("type", "spacy")
     if tokenizer_type == "spacy":
         return SpacyTokenizer(dc, **tokenizer_kwargs)
     return AutoTokenizer.from_pretrained(tokenizer_type, **tokenizer_kwargs)
