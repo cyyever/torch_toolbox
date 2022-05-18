21e3cd51dd8bf4a7e39dd8541f5b859e6ee48538
cyy <cyyever@outlook.com>
2022-05-18 11:14:02 +0800

    disable log

--- cyy_torch_toolbox/model_factory.py
+++ cyy_torch_toolbox/model_factory.py
@@ -46,6 +46,7 @@ def get_model_info() -> dict:
                             force_reload=False,
                             trust_repo=True,
                             skip_validation=True,
+                            verbose=False,
                         ),
                         repo,
                     )
