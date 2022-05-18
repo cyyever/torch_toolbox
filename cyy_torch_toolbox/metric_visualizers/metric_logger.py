4a289a2bbfc51bf47aedfdf724df1c1dcfdda71e
cyy <cyyever@outlook.com>
2022-05-18 21:51:14 +0800

    change prefix

--- cyy_torch_toolbox/metric_visualizers/metric_logger.py
+++ cyy_torch_toolbox/metric_visualizers/metric_logger.py
@@ -2,9 +2,4 @@ from .metric_visualizer import MetricVisualizer
 
 
 class MetricLogger(MetricVisualizer):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.prefix = ""
-
-    def set_prefix(self, prefix: str):
-        self.prefix = prefix
+    prefix = ""
