7c5787bd67e316726cb8c8e0a6c3e8841f624f1c
cyy <cyyever@outlook.com>
2022-07-21 14:04:11 +0800

    add code

--- cyy_torch_toolbox/dataset_collection.py
+++ cyy_torch_toolbox/dataset_collection.py
@@ -44,6 +44,7 @@ class DatasetCollection:
         self.__transforms: dict[MachineLearningPhase, Transforms] = {}
         for phase in MachineLearningPhase:
             self.__transforms[phase] = Transforms()
+        self.__transforms_cached: bool = False
 
     @property
     def dataset_type(self):
@@ -127,7 +128,12 @@ class DatasetCollection:
             self._datasets[phase] = DictDataset(transformed_dataset)
             self.__transforms[phase] = new_transforms
             if phase == MachineLearningPhase.Training:
-                get_logger().debug("new training transforms are %s", new_transforms)
+                get_logger().error("new training transforms are %s", new_transforms)
+        self.__transforms_cached = True
+
+    @property
+    def transforms_cached(self) -> str:
+        return self.__transforms_cached
 
     @property
     def name(self) -> str:
