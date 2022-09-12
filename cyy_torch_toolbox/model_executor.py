f1855861c25df195c7461be28659c205b9e56384
cyy <cyyever@outlook.com>
2022-09-12 15:21:54 +0800

    fix code

--- cyy_torch_toolbox/model_executor.py
+++ cyy_torch_toolbox/model_executor.py
@@ -256,18 +256,20 @@ class ModelExecutor(ModelExecutorBase):
     def offload_from_memory(self):
         assert self.save_dir is not None
         self.offload_from_gpu()
-        with open(os.path.join(self.save_dir, "model_and_loss.pk"), "wb") as file:
-            pickle.dump(
-                self.model_with_loss,
-                file,
-            )
-            self._model_with_loss = None
-        with open(os.path.join(self.save_dir, "dc.pk"), "wb") as file:
-            pickle.dump(
-                self.__dataset_collection,
-                file,
-            )
-            self.__dataset_collection = None
+        if self._model_with_loss is not None:
+            with open(os.path.join(self.save_dir, "model_and_loss.pk"), "wb") as file:
+                pickle.dump(
+                    self.model_with_loss,
+                    file,
+                )
+                self._model_with_loss = None
+        if self.__dataset_collection is not None:
+            with open(os.path.join(self.save_dir, "dc.pk"), "wb") as file:
+                pickle.dump(
+                    self.__dataset_collection,
+                    file,
+                )
+                self.__dataset_collection = None
 
     def load_to_memory(self):
         assert self.save_dir is not None
