549f3cb545d50035465a9fd62a219ca1e93dad92
cyy <cyyever@outlook.com>
2022-09-12 15:17:45 +0800

    fix code

--- cyy_torch_toolbox/hooks/keep_model.py
+++ cyy_torch_toolbox/hooks/keep_model.py
@@ -1,59 +1,67 @@
 import os
-import pickle
-import shutil
 
+from cyy_naive_lib.storage import DataStorage
 from cyy_torch_toolbox.device import get_cpu_device
 from cyy_torch_toolbox.hook import Hook
 from cyy_torch_toolbox.ml_type import MachineLearningPhase
 
 
 class KeepModelHook(Hook):
-    __best_model: tuple | None = None
-    save_flag: bool = False
+    __best_model: DataStorage = DataStorage(data=None)
+    save_epoch_model: bool = False
+    save_best_model: bool = False
 
     @property
     def best_model(self):
-        return self.__best_model
+        return self.__best_model.data
 
-    def offload_from_memory(self, save_dir):
-        with open(os.path.join(save_dir, "best_model.pk"), "wb") as f:
-            pickle.dump(self.best_model, f)
+    def __get_model_dir(self, root_dir: str) -> str:
+        model_dir = os.path.join(root_dir, "model")
+        os.makedirs(model_dir, exist_ok=True)
+        return model_dir
 
-    def load_to_memory(self, save_dir):
-        with open(os.path.join(save_dir, "best_model.pk"), "rb") as f:
-            self.__best_model = pickle.load(f)
+    def offload_from_memory(self):
+        self.__best_model.save()
 
     def _before_execute(self, **kwargs):
-        self.__best_model = None
+        self.__best_model = DataStorage(data=None)
 
-    def _after_validation(self, **kwargs):
-        trainer = kwargs["model_executor"]
-        epoch = kwargs["epoch"]
-        if self.save_flag:
-            model_dir = os.path.join(trainer.save_dir, "model")
-            os.makedirs(model_dir, exist_ok=True)
-            model_path = os.path.join(model_dir, "epoch_" + str(epoch) + ".pt")
+    def _after_validation(self, model_executor, epoch, **kwargs):
+        trainer = model_executor
+        if self.save_epoch_model:
+            model_path = os.path.join(
+                self.__get_model_dir(trainer.save_dir), f"epoch_{epoch}.pt"
+            )
             trainer.save_model(model_path)
 
         acc = trainer.get_inferencer_performance_metric(
             MachineLearningPhase.Validation
         ).get_epoch_metric(epoch, "accuracy")
-        if self.__best_model is None or acc > self.__best_model[1]:
-            self.__best_model = (
-                trainer.copy_model_with_loss().model.to(
-                    get_cpu_device(), non_blocking=True
-                ),
-                acc,
+        if self.best_model is None or acc > self.best_model[1]:
+            self.__best_model.set_data(
+                (
+                    trainer.copy_model_with_loss().model.to(
+                        get_cpu_device(), non_blocking=True
+                    ),
+                    acc,
+                )
             )
+            if trainer.save_dir is not None:
+                self.__best_model.set_data_path(
+                    os.path.join(
+                        self.__get_model_dir(trainer.save_dir), "best_model.pk"
+                    )
+                )
+                self.__best_model.save()
 
-    def _after_execute(self, **kwargs):
-        if not self.save_flag:
-            return
-        trainer = kwargs["model_executor"]
-        model_dir = os.path.join(trainer.save_dir, "model")
-        os.makedirs(model_dir, exist_ok=True)
-        trainer.save_model(os.path.join(model_dir, "last.pt"))
-        shutil.copy(
-            self.__best_model[0],
-            os.path.join(model_dir, "best_acc.pt"),
-        )
+    def _after_execute(self, model_executor, **kwargs):
+        trainer = model_executor
+        if self.save_epoch_model:
+            trainer.save_model(
+                os.path.join(self.__get_model_dir(trainer.save_dir), "last.pt")
+            )
+        if self.save_best_model:
+            self.__best_model.set_data(self.best_model[0])
+            self.__best_model.save()
+        else:
+            self.__best_model.clear()
