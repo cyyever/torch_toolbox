de82b6e586d50bf36d0a32ca04a17d3ee2fea390
cyy <cyyever@outlook.com>
2022-09-27 17:06:09 +0800

    fix code

--- cyy_torch_toolbox/tensor.py
+++ cyy_torch_toolbox/tensor.py
@@ -36,26 +36,26 @@ def load_tensor_dict(shapes: dict, tensor: torch.Tensor) -> dict:
     return result
 
 
-def split_tensor_to_dict(name_and_shapes: list, tensor: torch.Tensor) -> dict:
-    data = {}
-    bias = 0
-    for (name, shape) in name_and_shapes:
-        param_element_num = numpy.prod(shape)
-        data[name] = tensor.narrow(0, bias, param_element_num).view(*shape)
-        bias += param_element_num
-    assert bias == tensor.shape[0]
-    return data
+# def split_tensor_to_dict(name_and_shapes: list, tensor: torch.Tensor) -> dict:
+#     data = {}
+#     bias = 0
+#     for (name, shape) in name_and_shapes:
+#         param_element_num = numpy.prod(shape)
+#         data[name] = tensor.narrow(0, bias, param_element_num).view(*shape)
+#         bias += param_element_num
+#     assert bias == tensor.shape[0]
+#     return data
 
 
-def split_tensor_to_list(shapes: list, tensor: torch.Tensor) -> list:
-    data = []
-    bias = 0
-    for shape in shapes:
-        param_element_num = numpy.prod(shape)
-        data.append(tensor.narrow(0, bias, param_element_num).view(*shape))
-        bias += param_element_num
-    assert bias == tensor.shape[0]
-    return data
+# def split_tensor_to_list(shapes: list, tensor: torch.Tensor) -> list:
+#     data = []
+#     bias = 0
+#     for shape in shapes:
+#         param_element_num = numpy.prod(shape)
+#         data.append(tensor.narrow(0, bias, param_element_num).view(*shape))
+#         bias += param_element_num
+#     assert bias == tensor.shape[0]
+#     return data
 
 
 def get_tensor_serialization_size(data):
