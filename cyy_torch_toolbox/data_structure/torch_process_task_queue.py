98e532cc6fb6b63b1b49fc1fa8a0c4f4d5bc70e1
cyy <cyyever@outlook.com>
2022-09-27 15:49:00 +0800

    fix code

--- cyy_torch_toolbox/data_structure/torch_process_task_queue.py
+++ cyy_torch_toolbox/data_structure/torch_process_task_queue.py
@@ -6,7 +6,8 @@ import torch.multiprocessing
 from cyy_naive_lib.data_structure.task_queue import TaskQueue
 from cyy_torch_toolbox.device import (CudaDeviceGreedyAllocator,
                                       get_cpu_device, get_devices)
-from cyy_torch_toolbox.tensor import tensor_to
+from cyy_torch_toolbox.tensor import (assemble_tensors, disassemble_tensor,
+                                      tensor_to)
 
 
 class TorchProcessTaskQueue(TaskQueue):
@@ -17,9 +18,10 @@ class TorchProcessTaskQueue(TaskQueue):
         self,
         worker_fun: Callable = None,
         worker_num: int | None = None,
-        move_data_in_cpu: bool = True,
+        send_tensor_in_cpu: bool = True,
         max_needed_cuda_bytes=None,
         use_manager: bool = True,
+        assemble_tensor: bool = False,
     ):
         self.__devices = get_devices()
         self.use_manager = use_manager
@@ -32,7 +34,10 @@ class TorchProcessTaskQueue(TaskQueue):
                 worker_num = len(self.__devices)
             else:
                 worker_num = os.cpu_count()
-        self.__move_data_in_cpu = move_data_in_cpu
+        self.__send_tensor_in_cpu = send_tensor_in_cpu
+        if assemble_tensor:
+            assert send_tensor_in_cpu
+        self.__assemble_tensor = assemble_tensor
         super().__init__(worker_fun=worker_fun, worker_num=worker_num)
 
     def get_ctx(self):
@@ -53,18 +58,32 @@ class TorchProcessTaskQueue(TaskQueue):
         return state
 
     def add_task(self, task, **kwargs):
-        super().add_task(self.__process_tensor(task), **kwargs)
+        super().add_task(task, **kwargs)
 
     def put_result(self, result, **kwargs):
         super().put_result(self.__process_tensor(result), **kwargs)
 
     def __process_tensor(self, data):
-        if self.__move_data_in_cpu:
+        if self.__send_tensor_in_cpu:
             data = tensor_to(data, device=get_cpu_device())
+            if self.__assemble_tensor:
+                data = assemble_tensors(data)
         return data
 
     def put_data(self, data, **kwargs):
-        super().put_data(self.__process_tensor(data), **kwargs)
+        return super().put_data(self.__process_tensor(data), **kwargs)
+
+    def get_result(self, **kwargs):
+        result = super().get_result(**kwargs)
+        if self.__assemble_tensor:
+            result = disassemble_tensor(*result)
+        return result
+
+    def get_data(self, **kwargs):
+        data = super().get_data(**kwargs)
+        if self.__assemble_tensor:
+            data = disassemble_tensor(*data)
+        return data
 
     def _get_extra_task_arguments(self, worker_id):
         return super()._get_extra_task_arguments(worker_id) | {
--- cyy_torch_toolbox/test/data_structure/test_torch_process_task_queue.py
+++ cyy_torch_toolbox/test/data_structure/test_torch_process_task_queue.py
@@ -1,5 +1,6 @@
 from shutil import which
 
+import torch
 from cyy_torch_toolbox.data_structure.torch_process_task_queue import \
     TorchProcessTaskQueue
 
@@ -7,14 +8,13 @@ from cyy_torch_toolbox.data_structure.torch_process_task_queue import \
 def hello(task, args):
     assert not task
     assert args
-    return 1
+    return {"1": torch.Tensor([1, 2, 3])}
 
 
 def test_process_task_queue():
-    if which("nvcc"):
-        queue = TorchProcessTaskQueue(hello)
-        queue.start()
-        queue.add_task(())
-        res = queue.get_result()
-        assert res == 1
-        queue.stop()
+    queue = TorchProcessTaskQueue(hello, send_tensor_in_cpu=True, assemble_tensor=True)
+    queue.start()
+    queue.add_task(())
+    res = queue.get_result()
+    assert res == {"1": torch.Tensor[1, 2, 3]}
+    queue.stop()
