8b468aaff7c98597718b584af56966d4ad3283b6
cyy <cyyever@outlook.com>
2022-10-01 21:32:38 +0800

    add code

--- cyy_torch_toolbox/inferencer.py
+++ cyy_torch_toolbox/inferencer.py
@@ -9,12 +9,13 @@ from cyy_torch_toolbox.model_executor import ModelExecutor
 class Inferencer(ModelExecutor):
     _use_grad = False
 
-    def inference(self, use_grad=False, epoch=None, **kwargs):
+    def inference(self, use_grad=False, epoch=None, **kwargs) -> bool:
         self._use_grad = use_grad
         self._prepare_execution(**kwargs)
         if epoch is None:
             epoch = 1
         self.exec_hooks(ModelExecutorHookPoint.BEFORE_EXECUTE)
+        early_stop: bool = False
         with (torch.set_grad_enabled(use_grad), torch.cuda.stream(self.cuda_stream)):
             self.model.zero_grad(set_to_none=True)
             try:
@@ -23,9 +24,11 @@ class Inferencer(ModelExecutor):
                 )
             except StopExecutingException:
                 get_logger().warning("stop inference")
+                early_stop = True
             finally:
                 self._wait_stream()
             self.exec_hooks(ModelExecutorHookPoint.AFTER_EXECUTE)
+        return not early_stop
 
     def _get_backward_loss(self, result):
         if self._use_grad:
@@ -33,7 +36,8 @@ class Inferencer(ModelExecutor):
         return None
 
     def get_gradient(self):
-        self.inference(use_grad=True)
+        normal_stop = self.inference(use_grad=True)
+        assert normal_stop
         return self.model_util.get_gradient_list()
 
     def get_optimizer(self):
