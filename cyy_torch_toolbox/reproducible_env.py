a236304b75471df20b887649dd251e1626b7ae36
cyy <cyyever@outlook.com>
2022-12-10 15:09:41 +0800

    fix code

--- cyy_torch_toolbox/reproducible_env.py
+++ cyy_torch_toolbox/reproducible_env.py
@@ -1,44 +1,22 @@
-import copy
 import os
-import pickle
-import random
-import threading
-from typing import Any
 
-import numpy
 import torch
 from cyy_naive_lib.log import get_logger
+from cyy_naive_lib.reproducible_random_env import ReproducibleRandomEnv
 
 
-class ReproducibleEnv:
-    lock = threading.RLock()
-
+class ReproducibleEnv(ReproducibleRandomEnv):
     def __init__(self):
+        super().__init__()
         self.__torch_seed = None
         self.__torch_rng_state = None
         self.__torch_cuda_rng_state = None
-        self.__randomlib_state = None
-        self.__numpy_state = None
-        self.__enabled = False
-        self.__last_seed_path = None
-
-    @property
-    def enabled(self):
-        return self.__enabled
-
-    @property
-    def last_seed_path(self):
-        return self.__last_seed_path
 
     def enable(self):
         """
         https://pytorch.org/docs/stable/notes/randomness.html
         """
-        with ReproducibleEnv.lock:
-            if self.__enabled:
-                get_logger().warning("%s use reproducible env", id(self))
-            else:
-                get_logger().warning("%s initialize and use reproducible env", id(self))
+        with self.lock:
             os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
             torch.backends.cudnn.deterministic = True
             torch.backends.cudnn.benchmark = False
@@ -60,76 +38,31 @@ class ReproducibleEnv:
                 self.__torch_cuda_rng_state = torch.cuda.get_rng_state_all()
 
             if self.__torch_rng_state is not None:
-                get_logger().debug("overwrite torch cuda rng state")
+                get_logger().debug("overwrite torch rng state")
                 torch.set_rng_state(self.__torch_rng_state)
             else:
                 get_logger().debug("collect torch rng state")
                 self.__torch_rng_state = torch.get_rng_state()
-
-            if self.__randomlib_state is not None:
-                get_logger().debug("overwrite random lib state")
-                random.setstate(self.__randomlib_state)
-            else:
-                get_logger().debug("get random lib state")
-                self.__randomlib_state = random.getstate()
-
-            if self.__numpy_state is not None:
-                get_logger().debug("overwrite numpy random lib state")
-                numpy.random.set_state(copy.deepcopy(self.__numpy_state))
-            else:
-                get_logger().debug("get numpy random lib state")
-                self.__numpy_state = numpy.random.get_state()
-            self.__enabled = True
+            super().enable()
 
     def disable(self):
-        get_logger().warning("disable reproducible env")
         with ReproducibleEnv.lock:
             torch.use_deterministic_algorithms(False)
             torch.set_deterministic_debug_mode(0)
-            self.__enabled = False
+            super().disable()
 
-    def __enter__(self):
-        self.enable()
-        return self
+    def get_state(self) -> dict:
+        return super().get_state() | {
+            "torch_seed": self.__torch_seed,
+            "torch_cuda_rng_state": self.__torch_cuda_rng_state,
+            "torch_rng_state": self.__torch_rng_state,
+        }
 
-    def __exit__(self, exc_type, exc_value, real_traceback):
-        if real_traceback:
-            return
-        self.disable()
-
-    def save(self, save_dir: str) -> Any:
-        seed_path = os.path.join(save_dir, "random_seed")
-        get_logger().warning("%s save reproducible env to %s", id(self), seed_path)
-        with ReproducibleEnv.lock:
-            assert self.__enabled
-            os.makedirs(save_dir, exist_ok=True)
-            self.__last_seed_path = seed_path
-            with open(seed_path, "wb") as f:
-                return pickle.dump(
-                    {
-                        "torch_seed": self.__torch_seed,
-                        "torch_cuda_rng_state": self.__torch_cuda_rng_state,
-                        "torch_rng_state": self.__torch_rng_state,
-                        "randomlib_state": self.__randomlib_state,
-                        "numpy_state": self.__numpy_state,
-                    },
-                    f,
-                )
-
-    def load(self, path: str) -> None:
-        with ReproducibleEnv.lock:
-            assert not self.__enabled
-            with open(path, "rb") as f:
-                get_logger().warning("%s load reproducible env from %s", id(self), path)
-                obj: dict = pickle.load(f)
-                self.__torch_seed = obj["torch_seed"]
-                self.__torch_cuda_rng_state = obj["torch_cuda_rng_state"]
-                self.__torch_rng_state = obj["torch_rng_state"]
-                self.__randomlib_state = obj["randomlib_state"]
-                self.__numpy_state = obj["numpy_state"]
-
-    def load_last_seed(self):
-        self.load(self.last_seed_path)
+    def load_state(self, state: dict) -> None:
+        super().load_state(state)
+        self.__torch_seed = state["torch_seed"]
+        self.__torch_cuda_rng_state = state["torch_cuda_rng_state"]
+        self.__torch_rng_state = state["torch_rng_state"]
 
 
 global_reproducible_env: ReproducibleEnv = ReproducibleEnv()
