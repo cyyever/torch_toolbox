d1a76b335f62e870ef592eb3bba3e5f425d29144
cyy <cyyever@outlook.com>
2022-12-28 11:27:01 +0800

    fix code

--- cyy_torch_toolbox/metric_visualizers/batch_loss_logger.py
+++ cyy_torch_toolbox/metric_visualizers/batch_loss_logger.py
@@ -4,10 +4,14 @@ from .metric_logger import MetricLogger
 
 
 class BatchLossLogger(MetricLogger):
+    log_times = 5
+
     def _after_batch(self, epoch, batch_index, batch_size, result, **kwargs):
+        if self.log_times == 0:
+            return
         model_executor = kwargs.get("model_executor")
-        five_batches = model_executor.dataset_size // (5 * batch_size)
-        if five_batches == 0 or batch_index % five_batches == 0:
+        interval = model_executor.dataset_size // (self.log_times * batch_size)
+        if interval == 0 or batch_index % interval == 0:
             get_logger().info(
                 "%s epoch: %s, batch: %s, learning rate: %e, batch loss: %e",
                 self.prefix,
