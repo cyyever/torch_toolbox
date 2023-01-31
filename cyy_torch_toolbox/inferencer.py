bbafee600bdb164ef6553a0e90157cd34daaa622
cyy <cyyever@outlook.com>
2023-01-31 21:17:03 +0800

    fix code

--- cyy_torch_toolbox/inferencer.py
+++ cyy_torch_toolbox/inferencer.py
@@ -1,3 +1,5 @@
+import contextlib
+
 import torch
 from cyy_naive_lib.log import get_logger
 
@@ -7,23 +9,19 @@ from cyy_torch_toolbox.model_executor import ModelExecutor
 
 
 class Inferencer(ModelExecutor):
-    # def __init__(self, *args, batch_size=8, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.__batch_size = batch_size
-
-    # def _get_batch_size(self) -> int:
-    #     return self.__batch_size
-
-    def inference(self, use_grad: bool = False, epoch: int = 1, **kwargs: dict) -> bool:
+    def inference(self, use_grad: bool = False, **kwargs: dict) -> bool:
         error_return: bool = False
         try:
             self._prepare_execution(**kwargs)
             with (
                 torch.set_grad_enabled(use_grad),
+                torch.cuda.device(self.device)
+                if self.cuda_stream is not None
+                else contextlib.nullcontext(),
                 torch.cuda.stream(self.cuda_stream),
             ):
                 self.model.zero_grad(set_to_none=True)
-                self._execute_epoch(epoch=epoch, need_backward=use_grad)
+                self._execute_epoch(epoch=1, need_backward=use_grad)
             self.exec_hooks(ModelExecutorHookPoint.AFTER_EXECUTE)
         except StopExecutingException:
             get_logger().warning("stop inference")
@@ -39,9 +37,3 @@ class Inferencer(ModelExecutor):
         normal_stop: bool = self.inference(use_grad=True)
         assert normal_stop
         return self.model_util.get_gradient_list()
-
-    def get_optimizer(self):
-        return None
-
-    def get_lr_scheduler(self):
-        return None
