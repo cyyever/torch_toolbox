810487033ed462dccb78f4c6a22ec91b28860227
cyy <cyyevwer@outlook.com>
2023-02-25 16:05:40 +0000

    fix code

--- cyy_torch_toolbox/data_structure/torch_process_pool.py
+++ cyy_torch_toolbox/data_structure/torch_process_pool.py
@@ -6,7 +6,5 @@ from cyy_naive_lib.data_structure.process_pool import ProcessPool
 
 
 class TorchProcessPool(ProcessPool):
-    def __init__(self, **kwargs):
-        super().__init__(
-            mp_context=torch.multiprocessing.get_context("spawn"), **kwargs
-        )
+    def __init__(self, method="spawn", **kwargs) -> None:
+        super().__init__(mp_context=torch.multiprocessing.get_context(method), **kwargs)
