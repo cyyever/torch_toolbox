67bdd6c77336f4e12a561555c2b4e58a0c1f9623
cyy <cyyever@outlook.com>
2023-03-17 23:19:58 +0800

    fix API

--- cyy_torch_toolbox/model_factory.py
+++ cyy_torch_toolbox/model_factory.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 import torch
+import torch.nn
 
 try:
     import transformers
@@ -24,80 +25,77 @@ __model_info: dict = {}
 
 
 def get_model_info() -> dict:
+    if __model_info:
+        return __model_info
     github_repos = [
         "pytorch/vision:main",
         "cyyever/torch_models:main",
         "huggingface/pytorch-image-models:main",
     ]
 
-    if not __model_info:
-        for repo in github_repos:
-            try:
-                entrypoints = torch.hub.list(
-                    repo, force_reload=False, trust_repo=True, skip_validation=True
+    for repo in github_repos:
+        try:
+            entrypoints = torch.hub.list(
+                repo, force_reload=False, trust_repo=True, skip_validation=True
+            )
+        except BaseException:
+            entrypoints = torch.hub.list(repo, force_reload=False, skip_validation=True)
+        for model_name in entrypoints:
+            if model_name.lower() not in __model_info:
+                __model_info[model_name.lower()] = (
+                    model_name,
+                    functools.partial(
+                        torch.hub.load,
+                        repo_or_dir=repo,
+                        model=model_name,
+                        force_reload=False,
+                        trust_repo=True,
+                        skip_validation=True,
+                        verbose=False,
+                    ),
+                    repo,
                 )
-            except BaseException:
-                entrypoints = torch.hub.list(
-                    repo, force_reload=False, skip_validation=True
-                )
-            for model_name in entrypoints:
-                if model_name.lower() not in __model_info:
-                    __model_info[model_name.lower()] = (
-                        model_name,
-                        functools.partial(
-                            torch.hub.load,
-                            repo_or_dir=repo,
-                            model=model_name,
-                            force_reload=False,
-                            trust_repo=True,
-                            skip_validation=True,
-                            verbose=False,
-                        ),
-                        repo,
-                    )
-                else:
-                    get_logger().debug("ignore model_name %s", model_name)
+            else:
+                get_logger().debug("ignore model_name %s", model_name)
 
-        if has_hugging_face:
-            for model_name in huggingface_models:
-                full_model_name = "sequence_classification_" + model_name
+    if has_hugging_face:
+        for model_name in huggingface_models:
+            full_model_name = "sequence_classification_" + model_name
 
-                def create_model(model_name, pretrained, **model_kwargs):
-                    pretrained_model = (
-                        transformers.AutoModelForSequenceClassification.from_pretrained(
-                            model_name, **model_kwargs
-                        )
-                    )
-                    if pretrained:
-                        return pretrained_model
-                    get_logger().warning(
-                        "use huggingface without pretrained parameters"
-                    )
-                    old_embedding = pretrained_model.get_input_embeddings()
-                    config = transformers.AutoConfig.from_pretrained(
+            def create_model(model_name, pretrained, **model_kwargs):
+                pretrained_model = (
+                    transformers.AutoModelForSequenceClassification.from_pretrained(
                         model_name, **model_kwargs
                     )
-                    model = transformers.AutoModelForSequenceClassification.from_config(
-                        config
-                    )
-                    model.set_input_embeddings(old_embedding)
-                    return model
+                )
+                if pretrained:
+                    return pretrained_model
+                get_logger().warning("use huggingface without pretrained parameters")
+                old_embedding = pretrained_model.get_input_embeddings()
+                config = transformers.AutoConfig.from_pretrained(
+                    model_name, **model_kwargs
+                )
+                model = transformers.AutoModelForSequenceClassification.from_config(
+                    config
+                )
+                model.set_input_embeddings(old_embedding)
+                return model
 
-                if full_model_name.lower() not in __model_info:
-                    __model_info[full_model_name.lower()] = (
-                        full_model_name,
-                        functools.partial(
-                            create_model,
-                            model_name,
-                        ),
-                        None,
-                    )
+            if full_model_name.lower() not in __model_info:
+                __model_info[full_model_name.lower()] = (
+                    full_model_name,
+                    functools.partial(
+                        create_model,
+                        model_name,
+                    ),
+                    None,
+                )
     return __model_info
 
 
 def get_model(
     name: str, dataset_collection: DatasetCollection, model_kwargs: dict = {}
-) -> ModelWithLoss:
+) -> torch.nn.Module:
     model_info = get_model_info()
 
     added_kwargs: dict = {}
@@ -120,22 +118,16 @@ def get_model(
     model_type = ModelType.Classification
     if "rcnn" in name.lower():
         model_type = ModelType.Detection
-    try:
-        added_kwargs["num_classes"] = model_kwargs.get("num_classes", None)
-        if added_kwargs["num_classes"] is None:
-            added_kwargs["num_classes"] = len(
-                dataset_collection.get_labels(use_cache=True)
-            )
-            if model_type == ModelType.Detection:
-                added_kwargs["num_classes"] += 1
-        added_kwargs["num_labels"] = added_kwargs["num_classes"]
-    except Exception:
-        pass
-    loss_fun_name = model_kwargs.pop("loss_fun_name", None)
+    added_kwargs["num_classes"] = model_kwargs.get("num_classes", None)
+    if added_kwargs["num_classes"] is None:
+        added_kwargs["num_classes"] = len(dataset_collection.get_labels(use_cache=True))
+        if model_type == ModelType.Detection:
+            added_kwargs["num_classes"] += 1
+    added_kwargs["num_labels"] = added_kwargs["num_classes"]
     # use_checkpointing = model_kwargs.pop("use_checkpointing", False)
     while True:
         try:
-            _, model_constructor, repo = model_info.get(
+            model_name, model_constructor, repo = model_info.get(
                 name.lower(), (None, None, None)
             )
             if model_constructor is None:
@@ -143,18 +135,9 @@ def get_model(
                     f"unsupported model {name}, supported models are "
                     + str(model_info.keys())
                 )
+            get_logger().info("use model %s", model_name)
             model = model_constructor(**(added_kwargs | model_kwargs))
             get_logger().warning("use model arguments %s", model_kwargs | added_kwargs)
-            model_with_loss_fun = ModelWithLoss
-            if dataset_collection.dataset_type == DatasetType.Vision:
-                model_with_loss_fun = VisionModelWithLoss
-            elif dataset_collection.dataset_type == DatasetType.Text:
-                model_with_loss_fun = TextModelWithLoss
-            model_with_loss = model_with_loss_fun(
-                model=model,
-                loss_fun=loss_fun_name,
-                model_type=model_type,
-            )
             if repo is not None:
                 # we need the model path to pickle models
                 hub_dir = torch.hub.get_dir()
@@ -172,7 +155,7 @@ def get_model(
                 owner_name_branch = "_".join([repo_owner, repo_name, normalized_br])
                 repo_dir = os.path.join(hub_dir, owner_name_branch)
                 sys.path.append(repo_dir)
-            return model_with_loss
+            return model
         except TypeError as e:
             retry = False
             for k in copy.copy(added_kwargs):
@@ -189,6 +172,37 @@ def get_model(
                 raise e
 
 
+def get_model_with_loss(
+    model: torch.nn.Module,
+    dataset_collection: DatasetCollection,
+    model_type: None | ModelType = None,
+    model_kwargs: dict = {},
+) -> ModelWithLoss:
+    model_with_loss_fun = ModelWithLoss
+    if dataset_collection.dataset_type == DatasetType.Vision:
+        model_with_loss_fun = VisionModelWithLoss
+    elif dataset_collection.dataset_type == DatasetType.Text:
+        model_with_loss_fun = TextModelWithLoss
+    loss_fun_name = model_kwargs.get("loss_fun_name", None)
+    model_type = ModelType.Classification
+    model_with_loss = model_with_loss_fun(
+        model=model,
+        loss_fun=loss_fun_name,
+        model_type=model_type,
+    )
+    model_path = model_kwargs.get("model_path", None)
+    if model_path is not None:
+        model_with_loss.model.load_state_dict(torch.load(model_path))
+    word_vector_name = model_kwargs.get("word_vector_name", None)
+    if word_vector_name is not None:
+        from cyy_torch_toolbox.word_vector import PretrainedWordVector
+
+        PretrainedWordVector(word_vector_name).load_to_model(
+            model_with_loss=model_with_loss, tokenizer=dataset_collection.tokenizer
+        )
+    return model_with_loss
+
+
 class ModelConfig:
     def __init__(self, model_name: str):
         self.model_name: str = model_name
@@ -199,16 +213,14 @@ class ModelConfig:
     def get_model(self, dc: DatasetCollection) -> ModelWithLoss:
         assert not (self.pretrained and self.model_path)
         model_kwargs = copy.deepcopy(self.model_kwargs)
-        model_kwargs["pretrained"] = self.pretrained
-        get_logger().info("use model %s", self.model_name)
-        word_vector_name = model_kwargs.pop("word_vector_name", None)
-        model_with_loss = get_model(self.model_name, dc, model_kwargs)
+        if "pretrained" not in model_kwargs:
+            model_kwargs["pretrained"] = self.pretrained
         if self.model_path is not None:
-            model_with_loss.model.load_state_dict(torch.load(self.model_path))
-        if word_vector_name is not None:
-            from cyy_torch_toolbox.word_vector import PretrainedWordVector
-
-            PretrainedWordVector(word_vector_name).load_to_model(
-                model_with_loss=model_with_loss, tokenizer=dc.tokenizer
-            )
-        return model_with_loss
+            assert "model_path" not in model_kwargs
+            model_kwargs["model_path"] = self.model_path
+        model = get_model(
+            name=self.model_name, dataset_collection=dc, model_kwargs=model_kwargs
+        )
+        return get_model_with_loss(
+            model=model, dataset_collection=dc, model_kwargs=model_kwargs
+        )
--- cyy_torch_toolbox/test/test_model_util.py
+++ cyy_torch_toolbox/test/test_model_util.py
@@ -1,7 +1,6 @@
 from cyy_torch_toolbox.dataset_collection import (
     ClassificationDatasetCollection, create_dataset_collection)
 from cyy_torch_toolbox.model_factory import get_model, get_model_info
-from cyy_torch_toolbox.model_util import ModelUtil
 
 
 def test_model_info():
@@ -9,8 +8,6 @@ def test_model_info():
     assert models
 
 
-def test_get_modules():
+def test_get_module():
     mnist = create_dataset_collection(ClassificationDatasetCollection, "MNIST")
-    model_util = ModelUtil(get_model("LeNet5", mnist).model)
-    result = model_util.get_modules()
-    assert result
+    get_model("LeNet5", mnist)
