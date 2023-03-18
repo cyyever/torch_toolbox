686328ccdf316d57bc2719516bc6142211b5dcd5
cyy <cyyever@outlook.com>
2023-03-18 12:36:26 +0800

    fix code

--- cyy_torch_toolbox/test/test_inference.py
+++ cyy_torch_toolbox/test/test_inference.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 from cyy_torch_toolbox.default_config import DefaultConfig
 from cyy_torch_toolbox.ml_type import MachineLearningPhase
+from cyy_torch_toolbox.dependency import has_torchvision,has_torchtext
 
 
 def test_inference():
+    if not has_torchvision:
+        return
     config = DefaultConfig(dataset_name="MNIST", model_name="LeNet5")
     config.hyper_parameter_config.epoch = 1
     trainer = config.create_trainer()
@@ -12,6 +15,8 @@ def test_inference():
 
 
 def test_gradient():
+    if not has_torchtext:
+        return
     config = DefaultConfig(dataset_name="IMDB", model_name="simplelstm")
     config.hyper_parameter_config.epoch = 1
     trainer = config.create_trainer()
