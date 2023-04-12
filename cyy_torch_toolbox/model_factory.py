d77b6bf38e14db4e663bb53cb2dbfe9c2e171941
cyy <cyyever@outlook.com>
2023-04-12 11:29:01 +0800

    fix code

--- cyy_torch_toolbox/model_factory.py
+++ cyy_torch_toolbox/model_factory.py
@@ -24,39 +24,43 @@ from cyy_torch_toolbox.model_with_loss import (GraphModelWithLoss,
 __model_info: dict = {}
 
 
-def get_model_info() -> dict:
+def _get_model_info() -> dict:
     if __model_info:
         return __model_info
-    github_repos = ["pytorch/vision:main", "cyyever/torch_models:main"]
-    if has_hugging_face:
-        github_repos += [
-            "huggingface/pytorch-image-models:main",
-        ]
+    github_repos: dict[DatasetType, list] = {}
+    github_repos[DatasetType.Vision] = [
+        "pytorch/vision:main",
+        "huggingface/pytorch-image-models:main",
+    ]
+    for dataset_type in DatasetType:
+        if dataset_type not in github_repos:
+            github_repos[dataset_type] = []
+        github_repos[dataset_type].append("cyyever/torch_models:main")
 
-    for repo in github_repos:
-        try:
+    for dataset_type, repos in github_repos.items():
+        for repo in repos:
             entrypoints = torch.hub.list(
                 repo, force_reload=False, trust_repo=True, skip_validation=True
             )
-        except BaseException:
-            entrypoints = torch.hub.list(repo, force_reload=False, skip_validation=True)
-        for model_name in entrypoints:
-            if model_name.lower() not in __model_info:
-                __model_info[model_name.lower()] = (
-                    model_name,
-                    functools.partial(
-                        torch.hub.load,
-                        repo_or_dir=repo,
-                        model=model_name,
-                        force_reload=False,
-                        trust_repo=True,
-                        skip_validation=True,
-                        verbose=False,
-                    ),
-                    repo,
-                )
-            else:
-                get_logger().debug("ignore model_name %s", model_name)
+            for model_name in entrypoints:
+                if dataset_type not in __model_info:
+                    __model_info[dataset_type] = {}
+                if model_name.lower() not in __model_info[dataset_type]:
+                    __model_info[dataset_type][model_name.lower()] = (
+                        model_name,
+                        functools.partial(
+                            torch.hub.load,
+                            repo_or_dir=repo,
+                            model=model_name,
+                            force_reload=False,
+                            trust_repo=True,
+                            skip_validation=True,
+                            verbose=False,
+                        ),
+                        repo,
+                    )
+                else:
+                    get_logger().debug("ignore model_name %s", model_name)
 
     if has_hugging_face:
         for model_name in huggingface_models:
@@ -81,8 +85,8 @@ def get_model_info() -> dict:
                 model.set_input_embeddings(old_embedding)
                 return model
 
-            if full_model_name.lower() not in __model_info:
-                __model_info[full_model_name.lower()] = (
+            if full_model_name.lower() not in __model_info[DatasetType.Text]:
+                __model_info[DatasetType.Text][full_model_name.lower()] = (
                     full_model_name,
                     functools.partial(
                         create_model,
@@ -96,26 +100,27 @@ def get_model_info() -> dict:
 def get_model(
     name: str, dataset_collection: DatasetCollection, model_kwargs: dict = {}
 ) -> torch.nn.Module:
-    model_info = get_model_info()
+    model_info = _get_model_info()
 
     final_model_kwargs: dict = {}
-    if dataset_collection.dataset_type == DatasetType.Vision:
-        dataset_util = dataset_collection.get_dataset_util()
-        for k in ("input_channels", "channels"):
-            if k not in model_kwargs:
-                final_model_kwargs |= {
-                    k: dataset_util.channel,
-                }
-    if dataset_collection.dataset_type == DatasetType.Text:
-        for k in ("num_embeddings", "token_num"):
-            if k not in model_kwargs:
-                if dataset_collection.tokenizer is not None:
-                    final_model_kwargs[k] = len(dataset_collection.tokenizer.vocab)
-    if dataset_collection.dataset_type == DatasetType.Graph:
-        if "num_features" not in model_kwargs:
-            final_model_kwargs[
-                "num_features"
-            ] = dataset_collection.get_training_dataset().num_features
+    match dataset_collection.dataset_type:
+        case DatasetType.Vision:
+            dataset_util = dataset_collection.get_dataset_util()
+            for k in ("input_channels", "channels"):
+                if k not in model_kwargs:
+                    final_model_kwargs |= {
+                        k: dataset_util.channel,
+                    }
+        case DatasetType.Text:
+            for k in ("num_embeddings", "token_num"):
+                if k not in model_kwargs:
+                    if dataset_collection.tokenizer is not None:
+                        final_model_kwargs[k] = len(dataset_collection.tokenizer.vocab)
+        case DatasetType.Graph:
+            if "num_features" not in model_kwargs:
+                final_model_kwargs[
+                    "num_features"
+                ] = dataset_collection.get_training_dataset().num_features
 
     model_type = ModelType.Classification
     if "rcnn" in name.lower():
@@ -132,8 +137,8 @@ def get_model(
     while True:
         try:
             model_name, model_constructor, repo = model_info.get(
-                name.lower(), (None, None, None)
-            )
+                dataset_collection.dataset_type, {}
+            ).get(name.lower(), (None, None, None))
             if model_constructor is None:
                 raise NotImplementedError(
                     f"unsupported model {name}, supported models are "
