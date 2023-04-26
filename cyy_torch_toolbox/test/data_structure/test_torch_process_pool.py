47b70bd7a5c2b49d8c4306384ca5c8d8dd9aa0a2
cyy <cyyever@outlook.com>
2023-04-26 12:15:07 +0800

    fix test

--- cyy_torch_toolbox/test/data_structure/test_torch_process_pool.py
+++ cyy_torch_toolbox/test/data_structure/test_torch_process_pool.py
@@ -2,6 +2,7 @@ from cyy_naive_lib.log import get_logger
 from cyy_torch_toolbox.data_structure.torch_process_pool import \
     TorchProcessPool
 from cyy_torch_toolbox.default_config import DefaultConfig
+from cyy_torch_toolbox.dependency import has_torchvision
 from cyy_torch_toolbox.ml_type import ExecutorHookPoint, StopExecutingException
 
 
@@ -10,14 +11,15 @@ def stop_training(*args, **kwargs):
 
 
 def train(worker_id):
-    get_logger().info("worker_id is %s", worker_id)
-    trainer = DefaultConfig("MNIST", "LeNet5").create_trainer()
-    trainer.hyper_parameter.set_epoch(1)
-    trainer.hyper_parameter.set_learning_rate(0.01)
-    trainer.append_named_hook(
-        ExecutorHookPoint.AFTER_BATCH, "stop_training", stop_training
-    )
-    trainer.train()
+    if has_torchvision:
+        get_logger().info("worker_id is %s", worker_id)
+        trainer = DefaultConfig("MNIST", "LeNet5").create_trainer()
+        trainer.hyper_parameter.set_epoch(1)
+        trainer.hyper_parameter.set_learning_rate(0.01)
+        trainer.append_named_hook(
+            ExecutorHookPoint.AFTER_BATCH, "stop_training", stop_training
+        )
+        trainer.train()
 
 
 def test_process_pool():
