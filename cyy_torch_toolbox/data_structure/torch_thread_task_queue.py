ed83d570f597fccbea505abdcd938e1f84049499
cyy <cyyever@outlook.com>
2023-05-09 12:13:40 +0800

    fix code

--- cyy_torch_toolbox/data_structure/torch_thread_task_queue.py
+++ cyy_torch_toolbox/data_structure/torch_thread_task_queue.py
@@ -1,8 +1,7 @@
-import threading
-
+from cyy_naive_lib.data_structure.thread_context import ThreadContext
 from cyy_torch_toolbox.data_structure.torch_task_queue import TorchTaskQueue
 
 
 class TorchThreadTaskQueue(TorchTaskQueue):
-    def get_ctx(self):
-        return threading
+    def __init__(self, **kwargs):
+        super().__init__(mp_ctx=ThreadContext(), **kwargs)
