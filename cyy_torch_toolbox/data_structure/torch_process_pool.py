ae6d95f69753f8964e7aa5405770e3b883006ddd
cyy <cyyever@outlook.com>
2023-05-16 12:19:31 +0800

    fix code

--- cyy_torch_toolbox/data_structure/torch_process_pool.py
+++ cyy_torch_toolbox/data_structure/torch_process_pool.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-
-
 import torch
 from cyy_naive_lib.data_structure.process_pool import ProcessPool
 from cyy_naive_lib.system_info import get_operating_system
 
+from .torch_process_context import TorchProcessContext
+
 
 class TorchProcessPool(ProcessPool):
-    def __init__(self, method: str | None = None, **kwargs) -> None:
-        if method is None:
-            method = "spawn" if get_operating_system() != "freebsd" else "fork"
-        super().__init__(mp_context=torch.multiprocessing.get_context(method), **kwargs)
+    def __init__(self, **kwargs) -> None:
+        super().__init__(mp_context=TorchProcessContext().get_ctx(), **kwargs)
