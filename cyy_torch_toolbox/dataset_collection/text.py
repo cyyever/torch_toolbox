3c7456fa0116ec766660847509ffc944b7ff0f66
cyy <cyyever@outlook.com>
2023-05-30 18:04:19 +0800

    fix code

--- cyy_torch_toolbox/dataset_collection/text.py
+++ cyy_torch_toolbox/dataset_collection/text.py
@@ -11,13 +11,16 @@ class TextDatasetCollection(DatasetCollection):
         super().__init__(*args, **kwargs)
         assert self.dataset_type == DatasetType.Text
         self.__tokenizer: Any | None = None
+        self.__model_kwargs: dict | None = None
+
+    def set_model_kwargs(self, model_kwargs: dict) -> None:
+        self.__model_kwargs = model_kwargs
 
     @property
     def tokenizer(self) -> Any | None:
         if self.__tokenizer is None:
-            self.__tokenizer = get_tokenizer(
-                self, self.__dataset_kwargs.get("tokenizer", {})
-            )
+            tokenizer_kwargs = self.__dataset_kwargs.get("tokenizer", {})
+            self.__tokenizer = get_tokenizer(self, tokenizer_kwargs)
         return self.__tokenizer
 
     def __copy__(self):
