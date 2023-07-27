0bb8bbb58315259b45a88f23a772a59d0f93dc55
cyy <cyyever@outlook.com>
2023-07-27 16:26:02 +0800

    fix code

--- cyy_torch_toolbox/hook/config.py
+++ cyy_torch_toolbox/hook/config.py
@@ -1,6 +1,5 @@
 import torch
 import torch.cuda
-
 from cyy_torch_toolbox.hook.amp import AMP
 from cyy_torch_toolbox.hook.cudnn import CUDNNHook
 from cyy_torch_toolbox.hook.debugger import Debugger
@@ -20,11 +19,11 @@ class HookConfig:
 
     def append_hooks(self, executor) -> None:
         if executor.phase == MachineLearningPhase.Training:
-            if torch.cuda.torch.cuda.is_available():
+            if torch.cuda.is_available():
                 executor.enable_or_disable_hook("AMP", self.use_amp, AMP())
         executor.enable_or_disable_hook("debugger", self.debug, Debugger())
         executor.enable_or_disable_hook("profiler", self.profile, Profiler())
-        if torch.cuda.torch.cuda.is_available():
+        if torch.cuda.is_available():
             executor.enable_or_disable_hook("cudnn", self.benchmark_cudnn, CUDNNHook())
         executor.enable_or_disable_hook(
             "performance_metric_recorder",
