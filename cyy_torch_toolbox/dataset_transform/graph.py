08c9d915929253aa04f5881d6228d687e4c23c38
cyy <cyyever@outlook.com>
2023-08-02 16:36:23 +0800

    fix code

--- cyy_torch_toolbox/dataset_transform/graph.py
+++ cyy_torch_toolbox/dataset_transform/graph.py
@@ -1,19 +1,21 @@
 from typing import Any
 
 
-def pyg_data_extraction(data: Any, extract_index: bool = True) -> dict | None:
+def pyg_data_extraction(data: Any, extract_index: bool = True) -> dict:
     match data:
         case {
             "mask": mask,
             "graph": graph,
-            "graph_index": _,
-            "original_dataset": __,
+            "graph_index": graph_index,
+            "original_dataset": original_dataset,
         }:
             return {
                 "input": {"x": graph.x, "edge_index": graph.edge_index},
                 "target": graph.y,
                 "mask": mask,
+                "graph_index": graph_index,
+                "original_dataset": original_dataset,
             }
         case {"input": {"x": _, "edge_index": __}, "target": ___, "mask": ____}:
             return data
-    return None
+    return NotImplementedError()
