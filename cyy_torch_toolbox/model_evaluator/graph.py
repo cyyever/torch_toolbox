edcd1ed529feac15b7a4327fbc8a282a7d640e98
cyy <cyyever@outlook.com>
2023-08-04 09:58:03 +0800

    fix code

--- cyy_torch_toolbox/model_evaluator/graph.py
+++ cyy_torch_toolbox/model_evaluator/graph.py
@@ -15,7 +15,6 @@ if has_torch_geometric:
 class GraphModelEvaluator(ModelEvaluator):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        self.edge_dict: dict = {}
         self.node_and_neighbour_index_map: dict = {}
         self.edge_index_map: dict = {}
         self.node_and_neighbour_mask: dict = {}
@@ -29,12 +28,13 @@ class GraphModelEvaluator(ModelEvaluator):
         inputs = kwargs["inputs"]
         mask = kwargs["mask"]
         phase = kwargs["phase"]
-        if not self.edge_dict:
-            self.edge_dict = kwargs["edge_dict"]
         # get_logger().error("old edge shape is %s", inputs["edge_index"].shape)
         if phase not in self.edge_index_map:
             self.__narrow_graph(
-                mask=mask, phase=phase, edge_dtype=inputs["edge_index"].dtype
+                mask=mask,
+                phase=phase,
+                edge_dict=kwargs["edge_dict"],
+                edge_dtype=inputs["edge_index"].dtype,
             )
         masked = inputs["x"].shape[0] < mask.shape[0]
         if not masked:
@@ -64,28 +64,29 @@ class GraphModelEvaluator(ModelEvaluator):
         output = output[batch_mask]
         return super()._compute_loss(output=output, **kwargs)
 
-    def __narrow_graph(self, mask, phase, edge_dtype) -> None:
-        if phase not in self.edge_index_map:
-            node_indices = set(torch_geometric.utils.mask_to_index(mask).tolist())
-            node_and_neighbours, neighbour_edges = GraphDatasetUtil.get_neighbors(
-                node_indices=node_indices,
-                edge_dict=self.edge_dict,
-                hop=self.neighbour_hop,
-            )
-            node_and_neighbour_index_map = {
-                node_index: idx
-                for idx, node_index in enumerate(sorted(node_and_neighbours))
-            }
-            self.node_and_neighbour_index_map[phase] = node_and_neighbour_index_map
-            new_source_list = []
-            new_target_list = []
-            for source, target in neighbour_edges:
-                new_source_list.append(node_and_neighbour_index_map[source])
-                new_target_list.append(node_and_neighbour_index_map[target])
-            self.edge_index_map[phase] = torch.tensor(
-                data=[new_source_list, new_target_list], dtype=edge_dtype
-            )
-            node_and_neighbour_mask = torch.zeros_like(mask)
-            for idx in node_and_neighbours:
-                node_and_neighbour_mask[idx] = True
-            self.node_and_neighbour_mask[phase] = node_and_neighbour_mask
+    def __narrow_graph(self, mask, phase, edge_dict, edge_dtype) -> None:
+        if phase in self.edge_index_map:
+            return
+        node_indices = set(torch_geometric.utils.mask_to_index(mask).tolist())
+        node_and_neighbours, neighbour_edges = GraphDatasetUtil.get_neighbors(
+            node_indices=node_indices,
+            edge_dict=edge_dict,
+            hop=self.neighbour_hop,
+        )
+        node_and_neighbour_index_map = {
+            node_index: idx
+            for idx, node_index in enumerate(sorted(node_and_neighbours))
+        }
+        self.node_and_neighbour_index_map[phase] = node_and_neighbour_index_map
+        new_source_list = []
+        new_target_list = []
+        for source, target in neighbour_edges:
+            new_source_list.append(node_and_neighbour_index_map[source])
+            new_target_list.append(node_and_neighbour_index_map[target])
+        self.edge_index_map[phase] = torch.tensor(
+            data=[new_source_list, new_target_list], dtype=edge_dtype
+        )
+        node_and_neighbour_mask = torch.zeros_like(mask)
+        for idx in node_and_neighbours:
+            node_and_neighbour_mask[idx] = True
+        self.node_and_neighbour_mask[phase] = node_and_neighbour_mask
