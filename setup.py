b8057f2eeb4cd3167452ad2d0da0068b44fcd4fb
cyy <cyyever@outlook.com>
2023-09-06 12:06:38 +0800

    fix code

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="cyy_torch_toolbox",
     author="cyy",
-    version="0.1",
+    version="0.2",
     author_email="cyyever@outlook.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -33,5 +33,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.11",
 )
