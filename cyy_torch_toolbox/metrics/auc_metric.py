346e5dae5306bb6474fe9bf0061790449bf5ff5a
cyy <cyyever@outlook.com>
2023-10-02 21:33:57 +0800

    fix code

--- cyy_torch_toolbox/metrics/auc_metric.py
+++ cyy_torch_toolbox/metrics/auc_metric.py
@@ -1,29 +1,29 @@
 from typing import Any
 
-from torchmetrics.classification import MulticlassF1Score
+from torchmetrics.classification import MulticlassAUROC
 
 from .metric import Metric
 
 
 class F1Metric(Metric):
-    __f1: None | MulticlassF1Score = None
+    __auroc: None | MulticlassAUROC = None
 
     def _before_epoch(self, **kwargs) -> None:
         executor = kwargs["executor"]
         assert executor.dataset_collection.label_number > 0
-        self.__f1 = MulticlassF1Score(
+        self.__auroc = MulticlassAUROC(
             num_classes=executor.dataset_collection.label_number
         )
 
     def _after_batch(self, result: dict, **kwargs: Any) -> None:
+        assert self.__auroc is not None
         targets = result["targets"]
-        assert self.__f1 is not None
-        self.__f1.update(
+        self.__auroc.update(
             self._get_output(result).clone().detach().cpu(),
             targets.clone().detach().cpu(),
         )
 
     def _after_epoch(self, **kwargs) -> None:
         epoch = kwargs["epoch"]
-        assert self.__f1 is not None
-        self._set_epoch_metric(epoch, "F1", self.__f1.compute())
+        assert self.__auroc is not None
+        self._set_epoch_metric(epoch, "AUROC", self.__auroc.compute())
