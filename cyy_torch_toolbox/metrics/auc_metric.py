93abd5dbfc7f4cf78490149933a7bfd10263e2ca
cyy <cyyever@outlook.com>
2023-10-03 14:04:12 +0800

    fix code

--- cyy_torch_toolbox/metrics/auc_metric.py
+++ cyy_torch_toolbox/metrics/auc_metric.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from torchmetrics.classification import MulticlassAUROC
 
+from ..ml_type import ModelType
 from .classification_metric import ClassificationMetric
 
 
@@ -10,13 +11,17 @@ class AUROCMetric(ClassificationMetric):
 
     def _before_epoch(self, **kwargs) -> None:
         executor = kwargs["executor"]
+        if executor.running_model_evaluator.model_type != ModelType.Classification:
+            self.__auroc = None
+            return
         assert executor.dataset_collection.label_number > 0
         self.__auroc = MulticlassAUROC(
             num_classes=executor.dataset_collection.label_number
         )
 
     def _after_batch(self, result: dict, **kwargs: Any) -> None:
-        assert self.__auroc is not None
+        if self.__auroc is None:
+            return
         targets = result["targets"]
         self.__auroc.update(
             self._get_output(result).clone().detach().cpu(),
@@ -24,6 +29,7 @@ class AUROCMetric(ClassificationMetric):
         )
 
     def _after_epoch(self, **kwargs) -> None:
+        if self.__auroc is None:
+            return
         epoch = kwargs["epoch"]
-        assert self.__auroc is not None
         self._set_epoch_metric(epoch, "AUROC", self.__auroc.compute())
