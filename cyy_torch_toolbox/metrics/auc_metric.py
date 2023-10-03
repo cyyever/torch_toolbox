54361763f4bdf5f0790fe8984c6e01131cc33921
cyy <cyyever@outlook.com>
2023-10-03 15:37:08 +0800

    fix code

--- cyy_torch_toolbox/metrics/auc_metric.py
+++ cyy_torch_toolbox/metrics/auc_metric.py
@@ -1,35 +1,32 @@
 from typing import Any
 
-from torchmetrics.classification import MulticlassAUROC
+from torchmetrics.classification import MulticlassAUROC, MultilabelAUROC
 
-from ..ml_type import ModelType
 from .classification_metric import ClassificationMetric
 
 
 class AUROCMetric(ClassificationMetric):
-    __auroc: None | MulticlassAUROC = None
+    __auroc: None | MulticlassAUROC | MultilabelAUROC = None
 
     def _before_epoch(self, **kwargs) -> None:
         executor = kwargs["executor"]
-        if executor.running_model_evaluator.model_type != ModelType.Classification:
-            self.__auroc = None
-            return
         assert executor.dataset_collection.label_number > 0
-        self.__auroc = MulticlassAUROC(
-            num_classes=executor.dataset_collection.label_number
-        )
+        if executor.dataset_collection.is_mutilabel():
+            self.__auroc = MultilabelAUROC(
+                num_labels=executor.dataset_collection.label_number
+            )
+        else:
+            self.__auroc = MulticlassAUROC(
+                num_classes=executor.dataset_collection.label_number
+            )
 
     def _after_batch(self, result: dict, **kwargs: Any) -> None:
-        if self.__auroc is None:
-            return
-        targets = result["targets"]
+        targets = result["original_targets"]
         self.__auroc.update(
             self._get_output(result).clone().detach().cpu(),
-            targets.clone().detach().cpu(),
+            targets.clone().detach().cpu().long(),
         )
 
     def _after_epoch(self, **kwargs) -> None:
-        if self.__auroc is None:
-            return
         epoch = kwargs["epoch"]
         self._set_epoch_metric(epoch, "AUROC", self.__auroc.compute())
