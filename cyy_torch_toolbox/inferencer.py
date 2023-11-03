ee9b7bafebac8500d571372bfae3c509b62699cd
cyy <cyyever@outlook.com>
2023-11-03 11:46:12 +0800

    Fix code

--- cyy_torch_toolbox/inferencer.py
+++ cyy_torch_toolbox/inferencer.py
@@ -9,28 +9,22 @@ from cyy_torch_toolbox.ml_type import ExecutorHookPoint, StopExecutingException
 
 class Inferencer(Executor):
     def inference(self, use_grad: bool = False, **kwargs: Any) -> bool:
-        has_failure: bool = False
-        try:
-            self._prepare_execution(**kwargs)
-            with (
-                torch.set_grad_enabled(use_grad),
-                self.device_context,
-                self.device_stream_context,
-            ):
+        succ_flag: bool = False
+        with (
+            torch.set_grad_enabled(use_grad),
+            self.device_context,
+            self.device_stream_context,
+        ):
+            try:
+                self._prepare_execution(**kwargs)
                 self._execute_epoch(epoch=1, need_backward=use_grad, in_training=False)
-            self.exec_hooks(hook_point=ExecutorHookPoint.AFTER_EXECUTE)
-        except StopExecutingException:
-            get_logger().warning("stop inference")
-            has_failure = True
-        finally:
-            self.wait_stream()
-        return not has_failure
-
-    def get_optimizer(self) -> Any:
-        return None
-
-    def get_lr_scheduler(self) -> Any:
-        return None
+                self.exec_hooks(hook_point=ExecutorHookPoint.AFTER_EXECUTE)
+                succ_flag = True
+            except StopExecutingException:
+                get_logger().warning("stop inference")
+            finally:
+                self.wait_stream()
+            return succ_flag
 
     def _get_backward_loss(self, result: dict) -> Any:
         return result["normalized_batch_loss"]
