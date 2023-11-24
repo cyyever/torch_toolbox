566aabd306306d4fc13145fcb972f18118937de2
cyy <cyyever@outlook.com>
2023-11-24 17:38:45 +0800

    Fix code

---
+++ cyy_torch_toolbox/tokenizer/util.py
@@ -0,0 +1,34 @@
+from collections import Counter
+from typing import Callable
+
+from ..dataset.util import TextDatasetUtil
+from ..dataset_collection.text import TextDatasetCollection
+from ..ml_type import MachineLearningPhase
+
+
+def collect_tokens(
+    dc: TextDatasetCollection,
+    tokenizer: Callable,
+    phase: MachineLearningPhase | None = None,
+) -> Counter:
+    counter: Counter = Counter()
+
+    if phase is None:
+        util_list = [dc.get_dataset_util(phase=phase) for phase in MachineLearningPhase]
+    else:
+        util_list = [dc.get_dataset_util(phase=phase)]
+    for util in util_list:
+        assert isinstance(util, TextDatasetUtil)
+        for index in range(len(util)):
+            input_text = util.get_sample_text(index)
+            match input_text:
+                case str():
+                    input_text = [input_text]
+                # case [*elements]:
+                #     pass
+                case _:
+                    raise NotImplementedError(type(input_text))
+            for text in input_text:
+                tokens = tokenizer(text)
+                counter.update(tokens)
+    return counter
