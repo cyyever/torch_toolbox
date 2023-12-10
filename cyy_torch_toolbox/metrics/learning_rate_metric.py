e791ee36b53b5bcbb121ac29dca34d3f65a11556
cyy <cyyever@outlook.com>
2023-12-10 12:13:59 +0800

    Fix code

--- cyy_torch_toolbox/metrics/learning_rate_metric.py
+++ cyy_torch_toolbox/metrics/learning_rate_metric.py
@@ -3,10 +3,12 @@ from .metric import Metric
 
 class LearningRateMetric(Metric):
     def _before_batch(self, executor, batch_index, **kwargs) -> None:
-        if executor.has_optimizer():
+        if batch_index == 0:
             optimizer = executor.get_optimizer()
-            self._set_batch_metric(
-                batch_index=batch_index,
-                name="learning_rate",
-                data=[group["lr"] for group in optimizer.param_groups],
-            )
+        elif executor.has_optimizer():
+            optimizer = executor.get_optimizer()
+        self._set_batch_metric(
+            batch_index=batch_index,
+            name="learning_rate",
+            data=[group["lr"] for group in optimizer.param_groups],
+        )
