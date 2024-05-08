ed8cca8ec1c0d8ae7786415164c1d74653499dc7
cyy <cyyever@outlook.com>
2024-05-08 10:55:11 +0800

    Fix code

--- cyy_torch_toolbox/typing.py
+++ cyy_torch_toolbox/typing.py
@@ -1,6 +1,10 @@
-from typing import TypeAlias
+from typing import Iterable, TypeAlias
 
 import torch
 
+OptionalTensor: TypeAlias = torch.Tensor | None
 TensorDict: TypeAlias = dict[str, torch.Tensor]
+OptionalTensorDict: TypeAlias = TensorDict | None
 BlockType: TypeAlias = list[tuple[str, torch.nn.Module]]
+IndicesType: TypeAlias = Iterable[int]
+OptionalIndicesType: TypeAlias = IndicesType | None
