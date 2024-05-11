9b2129729d1cd068959983b80ff805e365df0d77
cyy <cyyever@outlook.com>
2024-05-11 14:09:09 +0800

    Fix code

--- cyy_torch_toolbox/__init__.py
+++ cyy_torch_toolbox/__init__.py
@@ -7,7 +7,8 @@ from .hyper_parameter import *
 from .inferencer import *
 from .ml_type import *
 from .model import *
-from .tensor import cat_tensor_dict, cat_tensors_to_vector
+from .tensor import (cat_tensor_dict, cat_tensors_to_vector, tensor_clone,
+                     tensor_to)
 from .trainer import *
 from .typing import (BlockType, IndicesType, OptionalIndicesType,
                      OptionalTensor, OptionalTensorDict, TensorDict)
