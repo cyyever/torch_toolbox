4993a3625d60ccb81ea435fd2499c5109b4c0fc0
cyy <cyyever@outlook.com>
2024-07-20 22:10:02 +0800

    Fix code

--- cyy_torch_toolbox/dataset/classification_collection.py
+++ cyy_torch_toolbox/dataset/classification_collection.py
@@ -1,33 +1,28 @@
+import copy
 import functools
-from typing import Any, Callable, Protocol
+from typing import Self
 
 from ..ml_type import MachineLearningPhase
 from .collection import DatasetCollection
-from .util import DatasetUtil
 
 
-class DatasetCollectionProtocol(Protocol):
-    @property
-    def name(self) -> str: ...
+class ClassificationDatasetCollection:
+    def __init__(self, dc: DatasetCollection) -> None:
+        self.__dc = dc
 
-    def get_dataset_util(
-        self, phase: MachineLearningPhase = MachineLearningPhase.Test
-    ) -> DatasetUtil: ...
+    def __copy__(self) -> Self:
+        return type(self)(dc=copy.copy(self.__dc))
 
-    def has_dataset(self, phase: MachineLearningPhase) -> bool: ...
-
-    def get_cached_data(self, file: str, computation_fun: Callable) -> Any: ...
-
-
-class ClassificationDatasetCollection(DatasetCollectionProtocol):
+    def __getattr__(self, name):
+        if "dc" in name:
+            raise AttributeError()
+        return getattr(self.__dc, name)
 
     @functools.cached_property
     def label_number(self) -> int:
         return len(self.get_labels())
 
     def get_labels(self, use_cache: bool = True) -> set:
-        assert isinstance(self, DatasetCollection)
-
         def computation_fun() -> set:
             if self.name.lower() == "imagenet":
                 return set(range(1000))
@@ -47,8 +42,6 @@ class ClassificationDatasetCollection(DatasetCollectionProtocol):
         return self.get_cached_data("labels.pk", computation_fun)
 
     def is_mutilabel(self) -> bool:
-        assert isinstance(self, DatasetCollection)
-
         def computation_fun() -> bool:
             if self.name.lower() == "imagenet":
                 return False
@@ -63,8 +56,6 @@ class ClassificationDatasetCollection(DatasetCollectionProtocol):
         return self.get_cached_data("is_mutilabel.pk", computation_fun)
 
     def get_label_names(self) -> dict:
-        assert isinstance(self, DatasetCollection)
-
         def computation_fun():
             label_names = self.get_dataset_util(
                 phase=MachineLearningPhase.Training
@@ -76,7 +67,6 @@ class ClassificationDatasetCollection(DatasetCollectionProtocol):
         return self.get_cached_data("label_names.pk", computation_fun)
 
     def __get_first_dataset_util(self):
-        assert isinstance(self, DatasetCollection)
         for phase in (
             MachineLearningPhase.Training,
             MachineLearningPhase.Validation,
