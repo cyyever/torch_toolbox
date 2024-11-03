590d5e688ecf712c94ea2f857de84b64d445927c
cyy <cyyever@outlook.com>
2024-11-03 18:13:43 +0800

    Fix code

--- cyy_torch_toolbox/__init__.py
+++ cyy_torch_toolbox/__init__.py
@@ -9,7 +9,7 @@ from .inferencer import *
 from .ml_type import *
 from .model import *
 from .tensor import cat_tensor_dict, cat_tensors_to_vector, tensor_clone, tensor_to
-from .trainer import *
 from .tokenizer import *
+from .trainer import *
 
 # __all__ = ["cat_tensor_dict", "cat_tensors_to_vector", "tensor_clone", "tensor_to"]
