984d3a08d0b174a6587f3874d5c8437c0edc83ba
cyy <cyyever@outlook.com>
2024-11-07 10:14:49 +0800

    Fix code

--- cyy_torch_toolbox/data_pipeline/__init__.py
+++ cyy_torch_toolbox/data_pipeline/__init__.py
@@ -1,11 +1,13 @@
+from typing import Any
+
 from ..ml_type import Factory
-from .dataset import *
-from .transform import *
+# from .dataset import *
+# from .transform import *
 
 global_data_transform_factory = Factory()
 
 
-def append_transforms_to_dc(dc, model_evaluator=None) -> None:
+def append_transforms_to_dc(dc: Any, model_evaluator: Any = None) -> None:
     constructor = global_data_transform_factory.get(dc.dataset_type)
     assert constructor is not None
     return constructor(dc=dc, model_evaluator=model_evaluator)
