7181fadf688c049cf09cc0edc84adc067dd9c337
cyy <cyyever@outlook.com>
2024-11-13 17:09:53 +0800

    Fix code

--- cyy_torch_toolbox/executor.py
+++ cyy_torch_toolbox/executor.py
@@ -258,9 +258,7 @@ class Executor(HookCollection, abc.ABC):
         self.__model_evaluator = model_evaluator
 
     def load_model(self, model_path: str) -> None:
-        self.model.load_state_dict(
-            torch.load(model_path, map_location=self.device, weights_only=True)
-        )
+        self.model.load_state_dict(torch.load(model_path, map_location=self.device))
 
     def _foreach_sub_executor(self) -> Generator:
         yield from []
