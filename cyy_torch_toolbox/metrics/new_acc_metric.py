d4999e303f1c23558e675076ac0351d7b5d95a9a
cyy <cyyever@outlook.com>
2024-11-15 10:02:51 +0800

    Fix code

--- cyy_torch_toolbox/metrics/new_acc_metric.py
+++ cyy_torch_toolbox/metrics/new_acc_metric.py
@@ -9,17 +9,22 @@ from .metric import Metric
 class NewAccuracyMetric(Metric):
     __acc: None | Accuracy = None
 
-    def _before_epoch(self, **kwargs: Any) -> None:
-        executor = kwargs["executor"]
-        if executor.dataset_collection.label_number <= 2:
-            self.__acc = Accuracy(task="binary")
-        else:
-            self.__acc = Accuracy(
-                task="multiclass", num_classes=executor.dataset_collection.label_number
-            )
+    def _before_execute(self, **kwargs: Any) -> None:
+        self.__acc = None
 
     @torch.no_grad()
     def _after_batch(self, result: dict, **kwargs: Any) -> None:
+        if self.__acc is None:
+            executor = kwargs["executor"]
+            with executor.device:
+                if executor.dataset_collection.label_number <= 2:
+                    self.__acc = Accuracy(task="binary")
+                else:
+                    self.__acc = Accuracy(
+                        task="multiclass",
+                        num_classes=executor.dataset_collection.label_number,
+                    )
+
         targets = result["targets"]
         output = result.get("model_output")
         if output is None:
