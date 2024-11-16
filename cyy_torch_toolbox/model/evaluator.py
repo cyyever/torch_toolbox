4f76961779d602b66fe7d8892f713aac5170f1a7
cyy <cyyever@outlook.com>
2024-11-16 18:08:07 +0800

    Fix code

--- cyy_torch_toolbox/model/evaluator.py
+++ cyy_torch_toolbox/model/evaluator.py
@@ -228,9 +228,9 @@ class ModelEvaluator:
             convert_kwargs["dtype"] = torch.float
         match loss_fun:
             case nn.CrossEntropyLoss():
-                # if len(targets.shape) == 2 and targets.shape[-1] == 1:
-                #     targets = targets.view(-1)
-                #     res["targets"] = targets
+                if len(targets.shape) == 2 and targets.shape[-1] == 1:
+                    targets = targets.view(-1)
+                    res["targets"] = targets
                 if len(targets.shape) <= 1:
                     convert_kwargs.pop("dtype")
             case nn.BCEWithLogitsLoss():
