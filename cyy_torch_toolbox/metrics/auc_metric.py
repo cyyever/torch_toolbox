551d593fff9bfcb7154efc79c1d1740979120ff2
cyy <cyyever@outlook.com>
2024-11-16 16:45:50 +0800

    Fix code

--- cyy_torch_toolbox/metrics/auc_metric.py
+++ cyy_torch_toolbox/metrics/auc_metric.py
@@ -7,16 +7,13 @@ from .classification_metric import ClassificationMetric
 
 class AUROCMetric(ClassificationMetric):
     def _after_batch(self, result: dict, **kwargs: Any) -> None:
+        executor = kwargs["executor"]
         if self._metric is None:
-            executor = kwargs["executor"]
             with executor.device:
-                self._metric = AUROC(
-                    task=self._get_task(executor),
-                    num_labels=executor.dataset_collection.label_number,
-                )
-        targets = result["targets"]
-        output = self._get_output(result).detach()
-        self.metric.update(output, targets.detach().long())
+                self._metric = AUROC(**self._get_metric_kwargs(executor))
+        output, targets = self._get_new_output(executor, result)
+        self.metric.update(output, targets)
+        # .detach().long())
 
     def _after_epoch(self, **kwargs) -> None:
         epoch = kwargs["epoch"]
