be1a21e5367aee8b17a79c57aed86ab366def4f6
cyy <cyyever@outlook.com>
2024-11-16 17:36:37 +0800

    Fix code

--- cyy_torch_toolbox/metrics/f1_metric.py
+++ cyy_torch_toolbox/metrics/f1_metric.py
@@ -1,34 +1,20 @@
 from typing import Any
 
-from torchmetrics.classification import MulticlassF1Score, MultilabelF1Score
+from torchmetrics.classification import F1Score
 
 from .classification_metric import ClassificationMetric
 
 
 class F1Metric(ClassificationMetric):
-    __f1: None | MulticlassF1Score | MultilabelF1Score = None
-
-    def _before_epoch(self, **kwargs) -> None:
-        self.__f1 = None
-
     def _after_batch(self, result: dict, **kwargs: Any) -> None:
-        targets = result["targets"]
-        output = self._get_output(result).detach()
-        if self.__f1 is None:
-            executor = kwargs["executor"]
+        executor = kwargs["executor"]
+        if self._metric is None:
             assert executor.dataset_collection.label_number > 0
             with executor.device:
-                if executor.dataset_collection.is_mutilabel():
-                    self.__f1 = MultilabelF1Score(
-                        num_labels=executor.dataset_collection.label_number
-                    )
-                else:
-                    self.__f1 = MulticlassF1Score(
-                        num_classes=executor.dataset_collection.label_number
-                    )
-        self.__f1.update(output, targets.detach())
+                self._metric = F1Score(**self._get_metric_kwargs(executor))
+        output, targets = self._get_new_output(executor, result)
+        self.metric.update(output, targets.detach())
 
     def _after_epoch(self, **kwargs) -> None:
         epoch = kwargs["epoch"]
-        assert self.__f1 is not None
-        self._set_epoch_metric(epoch, "F1", self.__f1.compute())
+        self._set_epoch_metric(epoch, "F1", self.metric.compute())
