4c81a17b2e4531275e56e47fd0164460ad4816cb
cyy <cyyever@outlook.com>
2024-12-04 10:25:33 +0800

    Fix code

--- cyy_torch_toolbox/metric_visualizers/batch_loss_logger.py
+++ cyy_torch_toolbox/metric_visualizers/batch_loss_logger.py
@@ -4,16 +4,16 @@ from .metric_visualizer import MetricVisualizer
 
 
 class BatchLossLogger(MetricVisualizer):
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.log_times = 5
+    log_times = 5
 
     def _after_batch(self, executor, epoch, batch_index, result, **kwargs) -> None:
         if self.log_times == 0:
             return
         if not executor.has_hook_obj("performance_metric"):
             return
-        interval: int = executor.dataset_size
+        interval: int = executor.hyper_parameter.get_iterations_per_epoch(
+            executor.dataset_size
+        )
         if interval != 0 and batch_index % interval != 0:
             return
         performance_metric = executor.get_hook("performance_metric")
