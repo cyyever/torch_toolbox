1dea75045fe2b2d03d963ae74c4e07c61a155672
cyy <cyyever@outlook.com>
2024-12-08 09:03:07 +0800

    Fix code

--- cyy_torch_toolbox/data_pipeline/transform.py
+++ cyy_torch_toolbox/data_pipeline/transform.py
@@ -8,18 +8,24 @@ class Transform:
     fun: Callable
     name: str = ""
     cacheable: bool = False
-    component: str | None = None
-    for_batch: bool = False
 
     def __str__(self) -> str:
         fun_name = self.name if self.name else str(self.fun)
         return f"name:{fun_name} cacheable:{self.cacheable}"
 
     def __call__(self, data: Any) -> Any:
+        return self.fun(data)
+
+
+@dataclass(kw_only=True)
+class SampleTransform(Transform):
+    component: str | None = None
+
+    def __call__(self, data: Any) -> Any:
         if self.component is not None:
             data[self.component] = self.fun(data[self.component])
             return data
-        return self.fun(data)
+        return super().__call__(data)
 
 
 class DatasetTransform(Transform):
