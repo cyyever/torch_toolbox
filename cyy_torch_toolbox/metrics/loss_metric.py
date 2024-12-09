7f551cab84132d53220768fe3b4fcf0944e31e6f
cyy <cyyever@outlook.com>
2024-12-09 18:44:06 +0800

    Fix code

--- cyy_torch_toolbox/metrics/loss_metric.py
+++ cyy_torch_toolbox/metrics/loss_metric.py
@@ -20,7 +20,5 @@ class LossMetric(Metric):
             return
         total_size = sum(item[1] for item in self.__batch_losses)
         total_loss = sum((item[1] * item[0]).item() for item in self.__batch_losses)
-        print("total loss is", total_loss)
-        print("total size is", total_size)
         self.__batch_losses = []
         self._set_epoch_metric(epoch, "loss", float(total_loss) / float(total_size))
