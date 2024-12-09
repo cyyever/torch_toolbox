5a1d956c0676e76d38006b4abe13d7e5a9a59808
cyy <cyyever@outlook.com>
2024-12-09 12:23:34 +0800

    Fix code

--- cyy_torch_toolbox/model/__init__.py
+++ cyy_torch_toolbox/model/__init__.py
@@ -22,12 +22,14 @@ def get_model_evaluator(
     dataset_collection: DatasetCollection | None = None,
     **model_kwargs,
 ) -> ModelEvaluator:
-    model_evaluator_funs: type | list[type] = ModelEvaluator
+    model_evaluator_funs: type | list[type] = [ModelEvaluator]
     dataset_type: DatasetType | None = model_kwargs.get("dataset_type")
     if dataset_type is None and dataset_collection is not None:
         dataset_type = dataset_collection.dataset_type
     if dataset_type is not None:
-        model_evaluator_funs = global_model_evaluator_factory.get(dataset_type)
+        model_evaluator_funs = global_model_evaluator_factory.get(
+            dataset_type, default=[ModelEvaluator]
+        )
     if not isinstance(model_evaluator_funs, list):
         model_evaluator_funs = [model_evaluator_funs]
     for model_evaluator_fun in model_evaluator_funs:
