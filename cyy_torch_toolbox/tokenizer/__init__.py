7a9fd48c5ae6abaca6ba1534899d37227aa6f1ca
cyy <cyyever@outlook.com>
2025-01-04 15:27:36 +0800

    Fix code

--- cyy_torch_toolbox/tokenizer/__init__.py
+++ cyy_torch_toolbox/tokenizer/__init__.py
@@ -1,12 +1,15 @@
-from collections.abc import Mapping
+from collections import Counter
+from collections.abc import Mapping, Sequence
 from typing import Any
 
 import torch
 from cyy_naive_lib.log import log_error
 
+from ..dataset import DatasetCollection
 from ..executor import Executor
+from ..ml_type import MachineLearningPhase
 
-type TokenIDType = int | tuple[int] | list[int] | torch.Tensor
+type TokenIDType = int | Sequence[int] | torch.Tensor
 type TokenIDsType = torch.Tensor
 
 
@@ -67,3 +70,27 @@ def convert_phrase_to_token_ids(
             log_error("decoded phrase is: %s", decoded_phrase)
             raise RuntimeError("failed to recover phrase")
     return token_ids
+
+
+def collect_tokens(
+    tokenizer: Tokenizer,
+    dc: DatasetCollection,
+    phase: MachineLearningPhase | None = None,
+) -> Counter:
+    counter: Counter = Counter()
+    if phase is None:
+        util_list = [
+            dc.get_dataset_util(phase=phase)
+            for phase in MachineLearningPhase
+            if dc.has_dataset(phase)
+        ]
+    else:
+        util_list = [dc.get_dataset_util(phase=phase)]
+    for util in util_list:
+        for index in range(len(util)):
+            transformed_token_results = util._get_sample_input(index)
+            tokens = tokenizer.get_tokens_from_transformed_result(
+                transformed_token_results
+            )
+            counter.update(tokens)
+    return counter
