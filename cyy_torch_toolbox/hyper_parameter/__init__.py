3574f422dfb947938ad4a8a84ff41fc0aef9d5ff
cyy <cyyever@outlook.com>
2025-02-14 09:41:52 +0800

    Fix code

--- cyy_torch_toolbox/hyper_parameter/__init__.py
+++ cyy_torch_toolbox/hyper_parameter/__init__.py
@@ -29,7 +29,10 @@ def _determine_learning_rate(task: Any, **kwargs: Any) -> float:
 def lr_scheduler_step_after_batch(
     lr_scheduler: torch.optim.lr_scheduler.LRScheduler,
 ) -> bool:
-    return isinstance(lr_scheduler, torch.optim.lr_scheduler.OneCycleLR)
+    return isinstance(
+        lr_scheduler,
+        torch.optim.lr_scheduler.OneCycleLR | torch.optim.lr_scheduler.CyclicLR,
+    )
 
 
 class HyperParameterAction(StrEnum):
