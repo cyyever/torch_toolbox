bd425c2eb5f207a2ca2c4a95ada53e329962ae45
cyy <cyyever@outlook.com>
2025-06-18 10:02:36 +0800

    Fix code

--- cyy_torch_toolbox/hook/executor_logger.py
+++ cyy_torch_toolbox/hook/executor_logger.py
@@ -14,14 +14,12 @@ class ExecutorLogger(Hook):
         log_info("model type is %s", executor.model.__class__)
         log_debug("model is %s", executor.model)
         # log_debug("loss function is %s", executor.loss_fun)
-        log_info(
-            "parameter number is %s",
-            sum(a.numel() for a in executor.model_util.get_parameter_seq(detach=False)),
-        )
+        total_parameter_number = 0
         trainable_parameter_number = 0
         dtype_stat: dict = {}
         device_stat: dict = {}
         for name, parameter in executor.model.named_parameters():
+            total_parameter_number += parameter.numel()
             if parameter.requires_grad:
                 trainable_parameter_number += parameter.numel()
             if parameter.device not in device_stat:
@@ -30,6 +28,7 @@ class ExecutorLogger(Hook):
             if parameter.dtype not in dtype_stat:
                 dtype_stat[parameter.dtype] = []
             dtype_stat[parameter.dtype].append(name)
+        log_info("total parameter number is %s", total_parameter_number)
         log_info("trainable parameter number is %s", trainable_parameter_number)
         if len(device_stat) == 1:
             log_info("model use device %s", list(device_stat.keys())[0])
